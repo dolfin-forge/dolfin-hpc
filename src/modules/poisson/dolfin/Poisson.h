a3ad940d2242b2ef4a4c395a0d4a1ac99a139369
fenics <devnull@localhost>
2005-03-16 23:33:26 +0000

    Tailorized "2005-03-16 17:33:25 by fenics" Added support for mixing multiple different finite elements in the same variational formulation. (Updates for latest CVS version of FFC.)

--- src/modules/poisson/dolfin/Poisson.h
+++ src/modules/poisson/dolfin/Poisson.h
@@ -10,62 +10,6 @@
 
 namespace dolfin { namespace Poisson {
 
-/// This is the finite element for which the form is generated,
-/// providing the information neccessary to do assembly.
-
-class FiniteElement : public dolfin::NewFiniteElement
-{
-public:
-
-  FiniteElement() : dolfin::NewFiniteElement(), tensordims(0)
-  {
-    // Do nothing
-  }
-
-  ~FiniteElement()
-  {
-    if ( tensordims ) delete [] tensordims;
-  }
-
-  inline unsigned int spacedim() const
-  {
-    return 3;
-  }
-
-  inline unsigned int shapedim() const
-  {
-    return 2;
-  }
-
-  inline unsigned int tensordim(unsigned int i) const
-  {
-    dolfin_error("Element is scalar.");
-    return 0;
-  }
-
-  inline unsigned int rank() const
-  {
-    return 0;
-  }
-
-  // FIXME: Only works for nodal basis
-  inline unsigned int dof(unsigned int i, const Cell& cell, const Mesh& mesh) const
-  {
-    return cell.nodeID(i);
-  }
-
-  // FIXME: Only works for nodal basis
-  inline const Point coord(unsigned int i, const Cell& cell, const Mesh& mesh) const
-  {
-    return cell.node(i).coord();
-  }
-
-private:
-
-  unsigned int* tensordims;
-
-};
-
 /// This class contains the form to be evaluated, including
 /// contributions from the interior and boundary of the domain.
 
@@ -73,28 +17,139 @@ class BilinearForm : public dolfin::BilinearForm
 {
 public:
 
-  BilinearForm() : dolfin::BilinearForm()
+  class TestElement : public dolfin::NewFiniteElement
   {
+  public:
+
+    TestElement() : dolfin::NewFiniteElement(), tensordims(0)
+    {
+      // Do nothing
+    }
+
+    ~TestElement()
+    {
+      if ( tensordims ) delete [] tensordims;
+    }
+
+    inline unsigned int spacedim() const
+    {
+      return 3;
+    }
+
+    inline unsigned int shapedim() const
+    {
+      return 2;
+    }
+
+    inline unsigned int tensordim(unsigned int i) const
+    {
+      dolfin_error("Element is scalar.");
+      return 0;
+    }
+
+    inline unsigned int rank() const
+    {
+      return 0;
+    }
+
+    // FIXME: Only works for nodal basis
+    inline unsigned int dof(unsigned int i, const Cell& cell, const Mesh& mesh) const
+    {
+      return cell.nodeID(i);
+    }
+
+    // FIXME: Only works for nodal basis
+    inline const Point coord(unsigned int i, const Cell& cell, const Mesh& mesh) const
+    {
+      return cell.node(i).coord();
+    }
+
+  private:
+
+    unsigned int* tensordims;
+
+  };
+
+  class TrialElement : public dolfin::NewFiniteElement
+  {
+  public:
+
+    TrialElement() : dolfin::NewFiniteElement(), tensordims(0)
+    {
+      // Do nothing
+    }
+
+    ~TrialElement()
+    {
+      if ( tensordims ) delete [] tensordims;
+    }
+
+    inline unsigned int spacedim() const
+    {
+      return 3;
+    }
+
+    inline unsigned int shapedim() const
+    {
+      return 2;
+    }
+
+    inline unsigned int tensordim(unsigned int i) const
+    {
+      dolfin_error("Element is scalar.");
+      return 0;
+    }
+
+    inline unsigned int rank() const
+    {
+      return 0;
+    }
+
+    // FIXME: Only works for nodal basis
+    inline unsigned int dof(unsigned int i, const Cell& cell, const Mesh& mesh) const
+    {
+      return cell.nodeID(i);
+    }
+
+    // FIXME: Only works for nodal basis
+    inline const Point coord(unsigned int i, const Cell& cell, const Mesh& mesh) const
+    {
+      return cell.node(i).coord();
+    }
+
+  private:
+
+    unsigned int* tensordims;
+
+  };
+
+  BilinearForm() : dolfin::BilinearForm(0)
+  {
+    // Create finite element for test space
+    _test = new TestElement();
+
+    // Create finite element for trial space
+    _trial = new TrialElement();
   }
 
   bool interior(real* block) const
   {
     // Compute geometry tensors
-    real G0_00 = det*(g00*g00 + g01*g01);
-    real G0_01 = det*(g00*g10 + g01*g11);
-    real G0_10 = det*(g10*g00 + g11*g01);
-    real G0_11 = det*(g10*g10 + g11*g11);
+    real G0_0_0 = det*(g00*g00 + g01*g01);
+    real G0_0_1 = det*(g00*g10 + g01*g11);
+    real G0_1_0 = det*(g10*g00 + g11*g01);
+    real G0_1_1 = det*(g10*g10 + g11*g11);
 
     // Compute element tensor
-    block[0] = 0.5*G0_00 + 0.5*G0_01 + 0.5*G0_10 + 0.5*G0_11;
-    block[1] = -0.5*G0_00 - 0.5*G0_10;
-    block[2] = -0.5*G0_01 - 0.5*G0_11;
-    block[3] = -0.5*G0_00 - 0.5*G0_01;
-    block[4] = 0.5*G0_00;
-    block[5] = 0.5*G0_01;
-    block[6] = -0.5*G0_10 - 0.5*G0_11;
-    block[7] = 0.5*G0_10;
-    block[8] = 0.5*G0_11;
+    block[0] = 0.5*G0_0_0 + 0.5*G0_0_1 + 0.5*G0_1_0 + 0.5*G0_1_1;
+    block[1] = -0.5*G0_0_0 - 0.5*G0_1_0;
+    block[2] = -0.5*G0_0_1 - 0.5*G0_1_1;
+    block[3] = -0.5*G0_0_0 - 0.5*G0_0_1;
+    block[4] = 0.5*G0_0_0;
+    block[5] = 0.5*G0_0_1;
+    block[6] = -0.5*G0_1_0 - 0.5*G0_1_1;
+    block[7] = 0.5*G0_1_0;
+    block[8] = 0.5*G0_1_1;
 
     return true;
   }
@@ -108,11 +163,119 @@ class LinearForm : public dolfin::LinearForm
 {
 public:
 
-  LinearForm(const NewFunction& w0) : dolfin::LinearForm()
+  class TestElement : public dolfin::NewFiniteElement
   {
+  public:
+
+    TestElement() : dolfin::NewFiniteElement(), tensordims(0)
+    {
+      // Do nothing
+    }
+
+    ~TestElement()
+    {
+      if ( tensordims ) delete [] tensordims;
+    }
+
+    inline unsigned int spacedim() const
+    {
+      return 3;
+    }
+
+    inline unsigned int shapedim() const
+    {
+      return 2;
+    }
+
+    inline unsigned int tensordim(unsigned int i) const
+    {
+      dolfin_error("Element is scalar.");
+      return 0;
+    }
+
+    inline unsigned int rank() const
+    {
+      return 0;
+    }
+
+    // FIXME: Only works for nodal basis
+    inline unsigned int dof(unsigned int i, const Cell& cell, const Mesh& mesh) const
+    {
+      return cell.nodeID(i);
+    }
+
+    // FIXME: Only works for nodal basis
+    inline const Point coord(unsigned int i, const Cell& cell, const Mesh& mesh) const
+    {
+      return cell.node(i).coord();
+    }
+
+  private:
+
+    unsigned int* tensordims;
+
+  };
+
+  class FunctionElement_0 : public dolfin::NewFiniteElement
+  {
+  public:
+
+    FunctionElement_0() : dolfin::NewFiniteElement(), tensordims(0)
+    {
+      // Do nothing
+    }
+
+    ~FunctionElement_0()
+    {
+      if ( tensordims ) delete [] tensordims;
+    }
+
+    inline unsigned int spacedim() const
+    {
+      return 3;
+    }
+
+    inline unsigned int shapedim() const
+    {
+      return 2;
+    }
+
+    inline unsigned int tensordim(unsigned int i) const
+    {
+      dolfin_error("Element is scalar.");
+      return 0;
+    }
+
+    inline unsigned int rank() const
+    {
+      return 0;
+    }
+
+    // FIXME: Only works for nodal basis
+    inline unsigned int dof(unsigned int i, const Cell& cell, const Mesh& mesh) const
+    {
+      return cell.nodeID(i);
+    }
+
+    // FIXME: Only works for nodal basis
+    inline const Point coord(unsigned int i, const Cell& cell, const Mesh& mesh) const
+    {
+      return cell.node(i).coord();
+    }
+
+  private:
+
+    unsigned int* tensordims;
+
+  };
+
+  LinearForm(const NewFunction& w0) : dolfin::LinearForm(1)
+  {
+    // Create finite element for test space
+    _test = new TestElement();
+        
     // Add functions
-    init(1, 3);
-    add(w0);
+    add(w0, new FunctionElement_0());
   }
 
   bool interior(real* block) const
@@ -131,7 +294,6 @@ public:
   }
 
 };
-
 
 } }
 

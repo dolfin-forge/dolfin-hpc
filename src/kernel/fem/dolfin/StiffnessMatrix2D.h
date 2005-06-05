3357558708175ae02f47b99420b990c1c1194b29
logg <devnull@localhost>
2005-06-05 22:06:37 +0000

    Tailorized "2005-06-05 17:06:36 by logg" Updated MassMatrix and StiffnessMatrix for new system.
    MassMatrix and StiffnessMatrix are easy-to-use shortcuts for linear
    elements. Normally, assembly is through FFC, but sometimes it may
    be convenient to just declare a mass or stiffness matrix for linear
    elements.
    
    For example, to create the standard stiffness matrix on the unit
    square with homogeneous Neumann boundary conditions, just do
    
      UnitSquare mesh(16, 16); // Total number of triangles is 2*16*16
      StiffnessMatrix A(mesh); // A is a (PETSc) matrix

--- src/kernel/fem/dolfin/StiffnessMatrix2D.h
+++ src/kernel/fem/dolfin/StiffnessMatrix2D.h
@@ -2,14 +2,14 @@
 // For further information, go to http://www/fenics.org/ffc/.
 // Licensed under the GNU GPL Version 2.
 
-#ifndef __POISSON_H
-#define __POISSON_H
+#ifndef __STIFFNESSMATRIX2D_H
+#define __STIFFNESSMATRIX2D_H
 
 #include <dolfin/FiniteElement.h>
 #include <dolfin/LinearForm.h>
 #include <dolfin/BilinearForm.h>
 
-namespace dolfin { namespace Poisson {
+namespace dolfin { namespace StiffnessMatrix2D {
 
 /// This class contains the form to be evaluated, including
 /// contributions from the interior and boundary of the domain.
@@ -161,153 +161,6 @@ public:
     block[6] = -4.999999999999997e-01*G0_1_0 - 4.999999999999996e-01*G0_1_1;
     block[7] = 4.999999999999997e-01*G0_1_0;
     block[8] = 4.999999999999996e-01*G0_1_1;
-  }
-
-};
-
-/// This class contains the form to be evaluated, including
-/// contributions from the interior and boundary of the domain.
-
-class LinearForm : public dolfin::LinearForm
-{
-public:
-    
-  class TestElement : public dolfin::FiniteElement
-  {
-  public:
-
-    TestElement() : dolfin::FiniteElement(), tensordims(0)
-    {
-      // Do nothing
-    }
-
-    ~TestElement()
-    {
-      if ( tensordims ) delete [] tensordims;
-    }
-
-    inline unsigned int spacedim() const
-    {
-      return 3;
-    }
-
-    inline unsigned int shapedim() const
-    {
-      return 2;
-    }
-
-    inline unsigned int tensordim(unsigned int i) const
-    {
-      dolfin_error("Element is scalar.");
-      return 0;
-    }
-
-    inline unsigned int rank() const
-    {
-      return 0;
-    }
-
-    void dofmap(int dofs[], const Cell& cell, const Mesh& mesh) const
-    {
-      dofs[0] = cell.nodeID(0);
-      dofs[1] = cell.nodeID(1);
-      dofs[2] = cell.nodeID(2);
-    }
-
-    void pointmap(Point points[], unsigned int components[], const AffineMap& map) const
-    {
-      points[0] = map(0.000000000000000e+00, 0.000000000000000e+00);
-      points[1] = map(1.000000000000000e+00, 0.000000000000000e+00);
-      points[2] = map(0.000000000000000e+00, 1.000000000000000e+00);
-      components[0] = 0;
-      components[1] = 0;
-      components[2] = 0;
-    }
-
-  private:
-
-    unsigned int* tensordims;
-
-  };
-    
-  class FunctionElement_0 : public dolfin::FiniteElement
-  {
-  public:
-
-    FunctionElement_0() : dolfin::FiniteElement(), tensordims(0)
-    {
-      // Do nothing
-    }
-
-    ~FunctionElement_0()
-    {
-      if ( tensordims ) delete [] tensordims;
-    }
-
-    inline unsigned int spacedim() const
-    {
-      return 3;
-    }
-
-    inline unsigned int shapedim() const
-    {
-      return 2;
-    }
-
-    inline unsigned int tensordim(unsigned int i) const
-    {
-      dolfin_error("Element is scalar.");
-      return 0;
-    }
-
-    inline unsigned int rank() const
-    {
-      return 0;
-    }
-
-    void dofmap(int dofs[], const Cell& cell, const Mesh& mesh) const
-    {
-      dofs[0] = cell.nodeID(0);
-      dofs[1] = cell.nodeID(1);
-      dofs[2] = cell.nodeID(2);
-    }
-
-    void pointmap(Point points[], unsigned int components[], const AffineMap& map) const
-    {
-      points[0] = map(0.000000000000000e+00, 0.000000000000000e+00);
-      points[1] = map(1.000000000000000e+00, 0.000000000000000e+00);
-      points[2] = map(0.000000000000000e+00, 1.000000000000000e+00);
-      components[0] = 0;
-      components[1] = 0;
-      components[2] = 0;
-    }
-
-  private:
-
-    unsigned int* tensordims;
-
-  };
-
-  LinearForm(Function& w0) : dolfin::LinearForm(1)
-  {
-    // Create finite element for test space
-    _test = new TestElement();
-        
-    // Add functions
-    add(w0, new FunctionElement_0());
-  }
-
-  void eval(real block[], const AffineMap& map) const
-  {
-    // Compute geometry tensors
-    real G0_0 = map.det*c[0][0];
-    real G0_1 = map.det*c[0][1];
-    real G0_2 = map.det*c[0][2];
-
-    // Compute element tensor
-    block[0] = 8.333333333333318e-02*G0_0 + 4.166666666666659e-02*G0_1 + 4.166666666666658e-02*G0_2;
-    block[1] = 4.166666666666659e-02*G0_0 + 8.333333333333318e-02*G0_1 + 4.166666666666659e-02*G0_2;
-    block[2] = 4.166666666666658e-02*G0_0 + 4.166666666666659e-02*G0_1 + 8.333333333333316e-02*G0_2;
   }
 
 };

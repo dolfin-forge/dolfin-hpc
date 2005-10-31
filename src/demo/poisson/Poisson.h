05d3ee2db3cf0c68160b8fd9039cb7a8b3793bd9
logg <devnull@localhost>
2005-10-31 10:43:46 +0000

    Tailorized "2005-10-31 04:43:45 by logg" Make FiniteElement::vertexeval() pure virtual and recompile all forms (except Navier-Stokes which is already current) to match new interface.

--- src/demo/poisson/Poisson.h
+++ src/demo/poisson/Poisson.h
@@ -22,49 +22,49 @@ namespace dolfin { namespace Poisson {
 class BilinearForm : public dolfin::BilinearForm
 {
 public:
-    
+  
   class TestElement : public dolfin::FiniteElement
   {
   public:
-
+  
     TestElement() : dolfin::FiniteElement(), tensordims(0)
     {
       // Do nothing
     }
-
+  
     ~TestElement()
     {
       if ( tensordims ) delete [] tensordims;
     }
-
+  
     inline unsigned int spacedim() const
     {
       return 3;
     }
-
+  
     inline unsigned int shapedim() const
     {
       return 2;
     }
-
+  
     inline unsigned int tensordim(unsigned int i) const
     {
       dolfin_error("Element is scalar.");
       return 0;
     }
-
+  
     inline unsigned int rank() const
     {
       return 0;
     }
-
+  
     void dofmap(int dofs[], const Cell& cell, const Mesh& mesh) const
     {
       dofs[0] = cell.nodeID(0);
       dofs[1] = cell.nodeID(1);
       dofs[2] = cell.nodeID(2);
     }
-
+  
     void pointmap(Point points[], unsigned int components[], const AffineMap& map) const
     {
       points[0] = map(0.000000000000000e+00, 0.000000000000000e+00);
@@ -74,61 +74,61 @@ public:
       components[1] = 0;
       components[2] = 0;
     }
-
+  
     void vertexeval(real values[], unsigned int vertex, const Vector& x, const Mesh& mesh) const
     {
       // FIXME: Temporary fix for Lagrange elements
       values[0] = x(vertex);
     }
-
+  
   private:
-
+  
     unsigned int* tensordims;
-
+  
   };
     
   class TrialElement : public dolfin::FiniteElement
   {
   public:
-
+  
     TrialElement() : dolfin::FiniteElement(), tensordims(0)
     {
       // Do nothing
     }
-
+  
     ~TrialElement()
     {
       if ( tensordims ) delete [] tensordims;
     }
-
+  
     inline unsigned int spacedim() const
     {
       return 3;
     }
-
+  
     inline unsigned int shapedim() const
     {
       return 2;
     }
-
+  
     inline unsigned int tensordim(unsigned int i) const
     {
       dolfin_error("Element is scalar.");
       return 0;
     }
-
+  
     inline unsigned int rank() const
     {
       return 0;
     }
-
+  
     void dofmap(int dofs[], const Cell& cell, const Mesh& mesh) const
     {
       dofs[0] = cell.nodeID(0);
       dofs[1] = cell.nodeID(1);
       dofs[2] = cell.nodeID(2);
     }
-
+  
     void pointmap(Point points[], unsigned int components[], const AffineMap& map) const
     {
       points[0] = map(0.000000000000000e+00, 0.000000000000000e+00);
@@ -138,19 +138,19 @@ public:
       components[1] = 0;
       components[2] = 0;
     }
-
+  
     void vertexeval(real values[], unsigned int vertex, const Vector& x, const Mesh& mesh) const
     {
       // FIXME: Temporary fix for Lagrange elements
       values[0] = x(vertex);
     }
-
+  
   private:
-
+  
     unsigned int* tensordims;
-
+  
   };
-
+  
   BilinearForm() : dolfin::BilinearForm(0)
   {
     // Create finite element for test space
@@ -188,49 +188,49 @@ public:
 class LinearForm : public dolfin::LinearForm
 {
 public:
-    
+  
   class TestElement : public dolfin::FiniteElement
   {
   public:
-
+  
     TestElement() : dolfin::FiniteElement(), tensordims(0)
     {
       // Do nothing
     }
-
+  
     ~TestElement()
     {
       if ( tensordims ) delete [] tensordims;
     }
-
+  
     inline unsigned int spacedim() const
     {
       return 3;
     }
-
+  
     inline unsigned int shapedim() const
     {
       return 2;
     }
-
+  
     inline unsigned int tensordim(unsigned int i) const
     {
       dolfin_error("Element is scalar.");
       return 0;
     }
-
+  
     inline unsigned int rank() const
     {
       return 0;
     }
-
+  
     void dofmap(int dofs[], const Cell& cell, const Mesh& mesh) const
     {
       dofs[0] = cell.nodeID(0);
       dofs[1] = cell.nodeID(1);
       dofs[2] = cell.nodeID(2);
     }
-
+  
     void pointmap(Point points[], unsigned int components[], const AffineMap& map) const
     {
       points[0] = map(0.000000000000000e+00, 0.000000000000000e+00);
@@ -240,61 +240,61 @@ public:
       components[1] = 0;
       components[2] = 0;
     }
-
+  
     void vertexeval(real values[], unsigned int vertex, const Vector& x, const Mesh& mesh) const
     {
       // FIXME: Temporary fix for Lagrange elements
       values[0] = x(vertex);
     }
-
+  
   private:
-
+  
     unsigned int* tensordims;
-
+  
   };
     
   class FunctionElement_0 : public dolfin::FiniteElement
   {
   public:
-
+  
     FunctionElement_0() : dolfin::FiniteElement(), tensordims(0)
     {
       // Do nothing
     }
-
+  
     ~FunctionElement_0()
     {
       if ( tensordims ) delete [] tensordims;
     }
-
+  
     inline unsigned int spacedim() const
     {
       return 3;
     }
-
+  
     inline unsigned int shapedim() const
     {
       return 2;
     }
-
+  
     inline unsigned int tensordim(unsigned int i) const
     {
       dolfin_error("Element is scalar.");
       return 0;
     }
-
+  
     inline unsigned int rank() const
     {
       return 0;
     }
-
+  
     void dofmap(int dofs[], const Cell& cell, const Mesh& mesh) const
     {
       dofs[0] = cell.nodeID(0);
       dofs[1] = cell.nodeID(1);
       dofs[2] = cell.nodeID(2);
     }
-
+  
     void pointmap(Point points[], unsigned int components[], const AffineMap& map) const
     {
       points[0] = map(0.000000000000000e+00, 0.000000000000000e+00);
@@ -304,19 +304,19 @@ public:
       components[1] = 0;
       components[2] = 0;
     }
-
+  
     void vertexeval(real values[], unsigned int vertex, const Vector& x, const Mesh& mesh) const
     {
       // FIXME: Temporary fix for Lagrange elements
       values[0] = x(vertex);
     }
-
+  
   private:
-
+  
     unsigned int* tensordims;
-
+  
   };
-
+  
   LinearForm(Function& w0) : dolfin::LinearForm(1)
   {
     // Create finite element for test space
@@ -334,9 +334,9 @@ public:
     real G0_2 = map.det*c[0][2];
 
     // Compute element tensor
-    block[0] = 8.333333333333318e-02*G0_0 + 4.166666666666659e-02*G0_1 + 4.166666666666657e-02*G0_2;
+    block[0] = 8.333333333333318e-02*G0_0 + 4.166666666666659e-02*G0_1 + 4.166666666666658e-02*G0_2;
     block[1] = 4.166666666666659e-02*G0_0 + 8.333333333333318e-02*G0_1 + 4.166666666666659e-02*G0_2;
-    block[2] = 4.166666666666657e-02*G0_0 + 4.166666666666659e-02*G0_1 + 8.333333333333316e-02*G0_2;
+    block[2] = 4.166666666666658e-02*G0_0 + 4.166666666666659e-02*G0_1 + 8.333333333333316e-02*G0_2;
   }
 
 };

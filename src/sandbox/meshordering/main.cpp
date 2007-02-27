719613d52ae754e5a722b8305b6613c6f09dde29
Anders Logg <logg@simula.no>
2007-02-27 16:05:35 +0100

    Minor fixes for orderEntities() in Triangle.cpp

--- src/sandbox/meshordering/main.cpp
+++ src/sandbox/meshordering/main.cpp
@@ -2,27 +2,20 @@
 // Licensed under the GNU GPL Version 2.
 //
 // First added:  2007-01-30
-// Last changed: 2007-01-30
+// Last changed: 2007-02-27
 //
 // Testing experimental code for ordering of mesh entities.
 
 #include <dolfin.h>
-#include "test.h"
 
 using namespace dolfin;
 
 int main()
 {
-  UnitSquare mesh(2, 2);
-//  UnitCube mesh(1, 1, 1);
-
-//  Mesh mesh;
-
-  Function f;
-
-  test::LinearForm L(f);
-//  FEM::disp(mesh, L.test());
-
+  UnitSquare mesh(1, 1);
+  mesh.disp();
+  
   MeshOrdering::order(mesh);
-  FEM::disp(mesh, L.test());
+
+  mesh.disp();
 }

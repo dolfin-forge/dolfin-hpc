b212e7ed5b4ce8a460a872bbba907cbd9f1f5510
Anders Logg <logg@simula.no>
2007-03-08 09:59:16 +0100

    Add DG sandbox, code compiles now, but assembly over facets has not been implemented.

--- src/sandbox/assembly/main.cpp
+++ src/sandbox/assembly/main.cpp
@@ -2,14 +2,14 @@
 // Licensed under the GNU GPL Version 2.
 //
 // First added:  2007-01-17
-// Last changed: 2007-03-01
+// Last changed: 2007-03-06
 //
 // Testing experimental code for new assembly.
 
 #include <dolfin.h>
 
+#include "PoissonOld.h"
 #include "Poisson.h"
-#include "poisson_ufc.h"
 #include "AssemblyMatrix.h"
 
 using namespace dolfin;
@@ -19,16 +19,18 @@ int main()
   UnitSquare mesh(2, 2);
 
   // Old assembly
+  /*
   cout << "---------- Old assembly ----------" << endl;
   Matrix A;
-  Poisson::BilinearForm a;
+  PoissonOld::BilinearForm a;
   FEM::assemble(a, A, mesh);
   A.disp();
+  */
 
   // New assembly
   cout << "---------- New assembly ----------" << endl;
   AssemblyMatrix B;
-  poisson b;
+  Poisson b;
   assemble(B, b, mesh);
   B.disp();
 }

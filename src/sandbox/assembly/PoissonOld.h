a3518b68b7e85ae632c5fb0466b9e59b8b4c7ee2
Anders Logg <logg@simula.no>
2007-04-04 19:32:30 +0200

    Fixes for ConstantFunction and UserFunction which now use ufc::finite_element::evaluate_dof() for interpolation on each element.

--- src/sandbox/assembly/PoissonOld.h
+++ src/sandbox/assembly/PoissonOld.h
@@ -1074,6 +1074,262 @@ void BilinearForm::eval(real block[], const AffineMap& map0, const AffineMap& ma
   }
 }
 
+/// This class contains the form to be evaluated, including
+/// contributions from the interior and boundary of the domain.
+
+class LinearForm : public dolfin::LinearForm
+{
+public:
+
+  class TestElement;
+
+  class FunctionElement_0;
+
+  LinearForm(Function& w0);
+  
+
+  bool interior_contribution() const;
+
+  void eval(real block[], const AffineMap& map, real det) const;
+
+  bool boundary_contribution() const;
+
+  void eval(real block[], const AffineMap& map, real det, unsigned int facet) const;
+
+  bool interior_boundary_contribution() const;
+
+  void eval(real block[], const AffineMap& map0, const AffineMap& map1, real det, unsigned int facet0, unsigned int facet1, unsigned int alignment) const;
+
+};
+
+class LinearForm::TestElement : public dolfin::FiniteElement
+{
+public:
+
+  TestElement() : dolfin::FiniteElement(), tensordims(0), subelements(0)
+  {
+    // Element is scalar, don't need to initialize tensordims
+
+    // Element is simple, don't need to initialize subelements
+  }
+
+  ~TestElement()
+  {
+    if ( tensordims ) delete [] tensordims;
+    if ( subelements )
+    {
+      for (unsigned int i = 0; i < elementdim(); i++)
+        delete subelements[i];
+      delete [] subelements;
+    }
+  }
+
+  inline unsigned int spacedim() const
+  {
+    return 3;
+  }
+
+  inline unsigned int shapedim() const
+  {
+    return 2;
+  }
+
+  inline unsigned int tensordim(unsigned int i) const
+  {
+    dolfin_error("Element is scalar.");
+    return 0;
+  }
+
+  inline unsigned int elementdim() const
+  {
+    return 1;
+  }
+
+  inline unsigned int rank() const
+  {
+    return 0;
+  }
+
+  void nodemap(int nodes[], const Cell& cell, const Mesh& mesh) const
+  {
+    nodes[0] = cell.entities(0)[0];
+    nodes[1] = cell.entities(0)[1];
+    nodes[2] = cell.entities(0)[2];
+  }
+
+  void pointmap(Point points[], unsigned int components[], const AffineMap& map) const
+  {
+    points[0] = map(0.000000000000000e+00, 0.000000000000000e+00);
+    points[1] = map(1.000000000000000e+00, 0.000000000000000e+00);
+    points[2] = map(0.000000000000000e+00, 1.000000000000000e+00);
+    components[0] = 0;
+    components[1] = 0;
+    components[2] = 0;
+  }
+
+  void vertexeval(uint vertex_nodes[], unsigned int vertex, const Mesh& mesh) const
+  {
+    // FIXME: Temporary fix for Lagrange elements
+    vertex_nodes[0] = vertex;
+  }
+
+  const FiniteElement& operator[] (unsigned int i) const
+  {
+    return *this;
+  }
+
+  FiniteElement& operator[] (unsigned int i)
+  {
+    return *this;
+  }
+
+  FiniteElementSpec spec() const
+  {
+    FiniteElementSpec s("Lagrange", "triangle", 1);
+    return s;
+  }
+  
+private:
+
+  unsigned int* tensordims;
+  FiniteElement** subelements;
+
+};
+
+class LinearForm::FunctionElement_0 : public dolfin::FiniteElement
+{
+public:
+
+  FunctionElement_0() : dolfin::FiniteElement(), tensordims(0), subelements(0)
+  {
+    // Element is scalar, don't need to initialize tensordims
+
+    // Element is simple, don't need to initialize subelements
+  }
+
+  ~FunctionElement_0()
+  {
+    if ( tensordims ) delete [] tensordims;
+    if ( subelements )
+    {
+      for (unsigned int i = 0; i < elementdim(); i++)
+        delete subelements[i];
+      delete [] subelements;
+    }
+  }
+
+  inline unsigned int spacedim() const
+  {
+    return 3;
+  }
+
+  inline unsigned int shapedim() const
+  {
+    return 2;
+  }
+
+  inline unsigned int tensordim(unsigned int i) const
+  {
+    dolfin_error("Element is scalar.");
+    return 0;
+  }
+
+  inline unsigned int elementdim() const
+  {
+    return 1;
+  }
+
+  inline unsigned int rank() const
+  {
+    return 0;
+  }
+
+  void nodemap(int nodes[], const Cell& cell, const Mesh& mesh) const
+  {
+    nodes[0] = cell.entities(0)[0];
+    nodes[1] = cell.entities(0)[1];
+    nodes[2] = cell.entities(0)[2];
+  }
+
+  void pointmap(Point points[], unsigned int components[], const AffineMap& map) const
+  {
+    points[0] = map(0.000000000000000e+00, 0.000000000000000e+00);
+    points[1] = map(1.000000000000000e+00, 0.000000000000000e+00);
+    points[2] = map(0.000000000000000e+00, 1.000000000000000e+00);
+    components[0] = 0;
+    components[1] = 0;
+    components[2] = 0;
+  }
+
+  void vertexeval(uint vertex_nodes[], unsigned int vertex, const Mesh& mesh) const
+  {
+    // FIXME: Temporary fix for Lagrange elements
+    vertex_nodes[0] = vertex;
+  }
+
+  const FiniteElement& operator[] (unsigned int i) const
+  {
+    return *this;
+  }
+
+  FiniteElement& operator[] (unsigned int i)
+  {
+    return *this;
+  }
+
+  FiniteElementSpec spec() const
+  {
+    FiniteElementSpec s("Lagrange", "triangle", 1);
+    return s;
+  }
+  
+private:
+
+  unsigned int* tensordims;
+  FiniteElement** subelements;
+
+};
+
+LinearForm::LinearForm(Function& w0) : dolfin::LinearForm(1)
+{
+  // Create finite element for test space
+  _test = new TestElement();
+
+  // Add functions
+  initFunction(0, w0, new FunctionElement_0());
+}
+
+// Contribution from the interior
+bool LinearForm::interior_contribution() const { return true; }
+
+void LinearForm::eval(real block[], const AffineMap& map, real det) const
+{
+  // Compute coefficients
+  const real c0_0 = c[0][0];
+  const real c0_1 = c[0][1];
+  const real c0_2 = c[0][2];
+
+  // Compute geometry tensors
+  const real G0_0 = det*c0_0;
+  const real G0_1 = det*c0_1;
+  const real G0_2 = det*c0_2;
+
+  // Compute element tensor
+  block[0] = 8.333333333333318e-02*G0_0 + 4.166666666666659e-02*G0_1 + 4.166666666666658e-02*G0_2;
+  block[1] = 4.166666666666659e-02*G0_0 + 8.333333333333318e-02*G0_1 + 4.166666666666659e-02*G0_2;
+  block[2] = 4.166666666666658e-02*G0_0 + 4.166666666666659e-02*G0_1 + 8.333333333333316e-02*G0_2;
+}
+
+// No contribution from the boundary
+bool LinearForm::boundary_contribution() const { return false; }
+
+void LinearForm::eval(real block[], const AffineMap& map, real det, unsigned int facet) const {}
+
+// No contribution from interior boundaries
+bool LinearForm::interior_boundary_contribution() const { return false; }
+
+void LinearForm::eval(real block[], const AffineMap& map0, const AffineMap& map1, real det, unsigned int facet0, unsigned int facet1, unsigned int alignment) const {}
+
 } }
 
 #endif

e258cb23976432c0857736746c4e02855446577c
Magnus Vikstrøm <gustavv@ifi.uio.no>
2007-04-18 16:41:33 +0200

    Added partitioning tests and edge-cut calculation.

--- src/sandbox/graph/main.cpp
+++ src/sandbox/graph/main.cpp
@@ -35,58 +35,6 @@ void testMeshToGraph()
   Graph graph(mesh, "nodal");
 
 }
-void testGraphPartition()
-{
-  std::cout << "Testing graph partitioning" << std::endl;
-  // Test graph
-  /*
-  
-  Graph graph;
-  GraphEditor ge;
-  ge.open(graph, Graph::undirected);
-  ge.initVertices(6);
-  ge.addVertex(0, 3);
-  ge.addVertex(1, 2);
-  ge.addVertex(2, 3);
-  ge.addVertex(3, 3);
-  ge.addVertex(4, 2);
-  ge.addVertex(5, 3);
-
-  ge.initEdges(8);
-  ge.addEdge(0, 1);
-  ge.addEdge(0, 2);
-  ge.addEdge(0, 5);
-  ge.addEdge(1, 2);
-  ge.addEdge(2, 3);
-  ge.addEdge(3, 4);
-  ge.addEdge(3, 5);
-  ge.addEdge(4, 5);
-
-  
-  // -----
- 
-  */
-  UndirectedClique graph(20);
-  dolfin::uint* parts = new dolfin::uint[graph.numVertices()];
-  dolfin::uint num_part = 6;
-  GraphPartition::partition(graph, num_part, parts);
-
-  GraphPartition::check(graph, num_part, parts);
-  GraphPartition::eval(graph, num_part, parts);
-  GraphPartition::disp(graph, num_part, parts);
-}
-void testMeshNodalGraphPartition(Mesh& mesh, dolfin::uint num_part)
-{
-  std::cout << "Testing Mesh partitioning" << std::endl;
-
-  Graph graph(mesh, "nodal");
-  dolfin::uint* parts = new dolfin::uint[graph.numVertices()];
-  GraphPartition::partition(graph, num_part, parts);
-
-  GraphPartition::check(graph, num_part, parts);
-  GraphPartition::eval(graph, num_part, parts);
-  GraphPartition::disp(graph, num_part, parts);
-}
 void testInputOutput()
 {
   std::cout << "Testing InputOutput" << std::endl;
@@ -194,10 +142,6 @@ void testInitEdgesError2()
 int main(int argc, char* argv[])
 {
   //testMeshToGraph();
-  //testGraphPartition();
-
-  UnitSquare mesh(10, 10);
-  testMeshNodalGraphPartition(mesh, 9);
   //testGraphEditor();
   //testInputOutput(); 
   //testCloseError();
---
+++ src/sandbox/partitioning/main.cpp
@@ -0,0 +1,112 @@
+#include <dolfin.h>
+#include <dolfin/GraphPartition.h>
+#include <iostream>
+
+#include <parmetis.h>
+#include <scotch.h>
+extern "C"
+{
+  #include <metis.h>
+}
+using namespace dolfin;
+
+void testMetisGraph(Graph& graph, int num_partitions)
+{
+  int options[5];
+  int num_vertices = graph.numVertices();
+  int wgtflag = 0;
+  int pnumflag = 0;
+  int edgecut = 0;
+  idxtype* parts = new int[num_vertices];
+  idxtype* adjncy = (idxtype*) graph.connectivity();
+  idxtype* xadj = (idxtype*) graph.offsets();
+  options[0] = 0;
+  std::cout << "METIS_PartGraphKway" << std::endl;
+  METIS_PartGraphRecursive(&num_vertices, xadj, adjncy, NULL, NULL, &wgtflag, &pnumflag, &num_partitions, options, &edgecut, parts);
+}
+//-----------------------------------------------------------------------------
+void testMetisMesh(Mesh& mesh, int num_partitions)
+{
+  int num_cells     = mesh.numCells() ;
+  int num_vertices  = mesh.numVertices();
+  
+  int index_base = 0;  // zero-based indexing
+  int edges_cut  = 0;
+
+  int cell_type = 0;
+  idxtype* cell_partition   = new int[num_cells];
+  idxtype* vertex_partition = new int[num_vertices];
+  idxtype* mesh_data = 0;
+
+  // Set cell type and allocate memory for METIS mesh structure
+  if(mesh.type().cellType() == CellType::triangle)
+  {
+    cell_type = 1;
+    mesh_data = new int[3*num_cells];
+  }
+  else if(mesh.type().cellType() == CellType::tetrahedron) 
+  {
+    cell_type = 2;
+    mesh_data = new int[4*num_cells];
+  }
+  else
+    dolfin_error("Do not know how to partition mesh of this type");
+  
+  if(num_partitions > 1)
+  {
+    // Create mesh structure for METIS
+    dolfin::uint i = 0;
+    for (CellIterator cell(mesh); !cell.end(); ++cell)
+      for (VertexIterator vertex(cell); !vertex.end(); ++vertex)
+        mesh_data[i++] = vertex->index();
+
+      // Use METIS to partition mesh
+    METIS_PartMeshNodal(&num_cells, &num_vertices, mesh_data, &cell_type, &index_base, 
+                        &num_partitions, &edges_cut, cell_partition, vertex_partition);
+  
+  }
+  // Clean up
+  delete [] cell_partition;
+  delete [] vertex_partition;
+  delete [] mesh_data;
+}
+//-----------------------------------------------------------------------------
+
+void testScotch(Graph& graph, int num_partitions)
+{
+  //SCOTCH_Graph grafdat;
+  //FILE* fileptr;
+
+  //if ((fileptr = fopen ("/home/magnus/doc/uio/master/dolfin_tests/fem_graphs/unitcube.grf", "r")) == NULL) {
+  //}
+  //if (SCOTCH_graphLoad (&grafdat, fileptr, -1, 0) != 0) {
+ // }
+}
+
+void testDolfinGraph(Graph& graph, int num_part)
+{
+  std::cout << "Testing graph partitioning" << std::endl;
+  dolfin::uint* parts = new dolfin::uint[graph.numVertices()];
+  GraphPartition::partition(graph, num_part, parts);
+
+  GraphPartition::check(graph, num_part, parts);
+  GraphPartition::eval(graph, num_part, parts);
+  GraphPartition::disp(graph, num_part, parts);
+}
+
+void testDolfinMesh(Mesh& mesh, int num_partitions)
+{
+  //Array<Mesh> meshes 
+  //MeshPartitioning::partition(mesh, meshes, num_partitions);
+
+}
+
+int main(int argc, char* argv[])
+{
+  //testMetis(graph, 16);
+  //testMeshPartition(mesh, 16);
+
+  UnitSquare mesh(2, 2);
+  Graph graph(mesh, "nodal");
+  testDolfinGraph(graph, 3);
+}

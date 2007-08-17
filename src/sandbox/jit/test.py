54b8921de6477fab97c459feae5bfdf198a94fdc
Anders Logg <logg@simula.no>
2007-08-18 01:09:50 +0200

    More work on JIT compiler and Python interface: - Add partially working Poisson demo (see list of issues in src/demo/pde/poisson/python/demo.py) - Update TODO list

--- src/sandbox/jit/test.py
+++ src/sandbox/jit/test.py
@@ -1,10 +1,14 @@
 from dolfin import *
 
 element = FiniteElement("Lagrange", "triangle", 1)
+mesh = UnitSquare(2, 2)
+
+f = Function(element, mesh, 1.0)
 
 v = TestFunction(element)
 u = TrialFunction(element)
 a = dot(grad(v), grad(u))*dx
+L = v*f*dx
 
-mesh = UnitSquare(2, 2)
 A = assemble(a, mesh)
+b = assemble(L, mesh)

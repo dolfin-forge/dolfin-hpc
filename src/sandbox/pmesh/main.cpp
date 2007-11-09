00de73504096a1cb4c2f51c1d20dffb918ee3b14
unknown <gustavv@utlaan-laptop-1>
2007-11-09 14:49:04 +0100

    Started work on MPIMeshCommunicator

--- src/sandbox/pmesh/main.cpp
+++ src/sandbox/pmesh/main.cpp
@@ -13,49 +13,17 @@ using namespace dolfin;
 //-----------------------------------------------------------------------------
 int main(int argc, char* argv[])
 {
+
+  MPIMeshCommunicator comm;
+
+  UnitSquare mesh(3, 3);
+  MeshFunction<unsigned int> f(mesh, 0);
+
   cout << "Starting mesh distribution" << endl;    
+  comm.broadcast(mesh);
 
-  // Initialise MPI  
-  MPI_Init(&argc, &argv);
-
-  // Get number of processes
-  int num_processes_int;
-  MPI_Comm_size(MPI_COMM_WORLD, &num_processes_int);
-
-  if( num_processes_int != 2 )
-    error("This test program must be run with 2 processes.");
-
-  // Get this process number
-  int process_int;
-  MPI_Comm_rank(MPI_COMM_WORLD, &process_int);
-  unsigned int this_process = process_int;
-
-  //-- MPI test code ----------------------------------------------------------
-  int sent_message = -1;
-  int received_message = 0;
-  int receive_from = -1;
-  int send_to      = -1;
-  if(this_process == 0)
-  {
-    receive_from = 1;
-    send_to      = 1;
-    sent_message = 20;
-  }
-  if(this_process == 1)
-  {
-    receive_from = 0;
-    send_to      = 0;
-    sent_message = 22;
-  }
-  int tag = 55;
-//  int send_tag = 55;
-
-  MPI_Status status;
-  MPI_Send(&sent_message,     1, MPI_INT, send_to,     tag, MPI_COMM_WORLD);
-  MPI_Recv(&received_message, 1, MPI_INT, receive_from, 55, MPI_COMM_WORLD, &status);
-
-  cout << "Finished send/rec " << this_process << "  " << received_message << endl;
-  MPI_Finalize();
+  cout << "Starting meshfunction distribution" << endl;    
+  comm.broadcast(f);
 
   return 0;
 }

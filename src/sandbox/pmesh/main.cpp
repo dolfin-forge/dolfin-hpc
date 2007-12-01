7368df8a47570aa01340964fb329bea0f81d0a88
Magnus Vikstrøm <gustavv@ifi.uio.no>
2007-12-01 15:50:07 +0100

    Implemented MPIManager and MPIMeshCommunicator. See src/sandbox/pmesh.

--- src/sandbox/pmesh/main.cpp
+++ src/sandbox/pmesh/main.cpp
@@ -7,23 +7,41 @@
 // This file is used for testing distribution of the mesh using MPI
 
 #include <dolfin.h>
-#include <mpi.h>
 
 using namespace dolfin;
 //-----------------------------------------------------------------------------
 int main(int argc, char* argv[])
 {
 
-  MPIMeshCommunicator comm;
+  // Single processor initializes mesh and broadcasts
+  UnitCube mesh(9, 9, 9);
 
-  UnitSquare mesh(3, 3);
+  // All processors
+  mesh.init();
+
+  int p = MPIManager::processNum();
+
+  std::stringstream meshstream;
+  meshstream << "mesh_p" << p << ".xml";
+  std::string meshfile = meshstream.str();
+
+  File file(meshfile);
+  file << mesh;
+
+  std::stringstream stream;
+  stream << "mesh_function_" << p << ".xml";
+  std::string filename = stream.str();
+
   MeshFunction<unsigned int> f(mesh, 0);
 
-  cout << "Starting mesh distribution" << endl;    
-  comm.broadcast(mesh);
+  // Single processor partitions mesh and broadcasts
+  mesh.partition(6, f);
 
-  cout << "Starting meshfunction distribution" << endl;    
-  comm.broadcast(f);
+  // All processors
+  File funcfile(filename);
+  funcfile << f;
 
+  // This should not be necessary (finalize should be called by ~MPIManager())
+  MPIManager::finalize();
   return 0;
 }

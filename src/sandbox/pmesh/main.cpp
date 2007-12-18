4344ab2bc055b52d85154bd76ffdaee6b38117db
Gustav Magnus Vikstrøm <gustavv@ifi.uio.no>
2007-12-18 17:19:22 +0100

    Testing parallel assembly

--- src/sandbox/pmesh/main.cpp
+++ src/sandbox/pmesh/main.cpp
@@ -2,44 +2,25 @@
 // Licensed under the GNU LGPL Version 2.1.
 //
 // First added:  2007-10-30
-// Last changed: 2007-12-06
+// Last changed: 2007-12-17
 //
 // This file is used for testing distribution of the mesh using MPI
 
 #include <dolfin.h>
+#include "Poisson2D.h"
 
 using namespace dolfin;
 //-----------------------------------------------------------------------------
 int main(int argc, char* argv[])
 {
-  // Single processor initializes mesh and broadcasts
-  //UnitSquare mesh(9, 9);
   UnitCube mesh(12, 12, 12);
+  MeshFunction<dolfin::uint> partitions;
+  mesh.partition(MPIManager::numProcesses(), partitions);
 
-  // All processors
-  mesh.init();
-
-  unsigned int p = MPIManager::processNumber();
-
-  std::stringstream meshstream;
-  meshstream << "mesh_p" << p << "_" << ".pvd";
-  std::string meshfile = meshstream.str();
-
-  File file(meshfile);
-  file << mesh;
-
-  std::stringstream stream;
-  stream << "mesh_function_p" << p << "_" << ".pvd";
-  std::string filename = stream.str();
-
-  MeshFunction<unsigned int> f(mesh, 0);
-
-  // Single processor partitions mesh and broadcasts
-  mesh.partition(MPIManager::numProcesses(), f);
-
-  // All processors
-  File funcfile(filename);
-  funcfile << f;
+  Matrix B;
+  Poisson2DBilinearForm a;
+  pAssembler assembler(mesh);
+  assembler.assemble(B, a, true);
 
   return 0;
 }

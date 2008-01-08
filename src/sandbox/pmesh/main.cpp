ec25230749ff8fabd1a0ef64bb52ecaef9b97957
Gustav Magnus Vikstrøm <gustavv@ifi.uio.no>
2008-01-08 16:30:35 +0100

    Updated files to use new MPI and PETScObject classes.

--- src/sandbox/pmesh/main.cpp
+++ src/sandbox/pmesh/main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char* argv[])
 {
   UnitCube mesh(4, 4, 4);
   MeshFunction<dolfin::uint> partitions;
-  mesh.partition(MPIManager::numProcesses(), partitions);
+  mesh.partition(MPI::numProcesses(), partitions);
 
   Matrix B;
   Poisson2DBilinearForm a;

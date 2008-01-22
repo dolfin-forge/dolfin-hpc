699120507b68def78bc9b579862914155cdaebe2
Gustav Magnus Vikstrøm <gustavv@ifi.uio.no>
2008-01-22 01:07:04 +0100

    Added some options to pmesh test.

--- src/sandbox/pmesh/main.cpp
+++ src/sandbox/pmesh/main.cpp
@@ -8,105 +8,139 @@
 // This file is used for testing distribution of the mesh using MPI
 
 #include <dolfin.h>
-#include <dolfin/pUFC.h>
 #include "pPoisson2D.h"
 #include "Poisson2D.h"
+#include <iostream>
+#include <fstream>
+#include "getopts.h"
+#include "assemble.h"
 
 using namespace dolfin;
-//-----------------------------------------------------------------------------
-std::string appendRank(std::string base, std::string ext)
+
+void timer(Mesh& mesh, int num_iterations)
 {
-  std::stringstream stream;
-  stream << base << dolfin::MPI::processNumber() << "." << ext;
-  return stream.str();
-}
-
-void check(Mesh& mesh, MeshFunction<dolfin::uint>& partitions)
-{
-  // Do normal assembly on process 1
-  Poisson2DBilinearForm a;
-  pPoisson2DBilinearForm b;
-  Matrix A, B;
-
-  if(dolfin::MPI::numProcesses() == 1)
-  {
-    Assembler assembler(mesh);
-    assembler.assemble(A, a, true);
-  }
-  // Parallel assembly on all procs
-  {
-    pAssembler passembler(mesh, partitions);
-    passembler.assemble(B, b, true);
-  }
-  pDofMapSet& dof_map_set = b.dofMaps();
-
-  // Would be nice to have automatic testing of B = A * modified dofs
-  // Currently just printing so that matrices can be manually inspected
-  PetscViewer viewer_a(PETSC_VIEWER_STDOUT_SELF);
-  PetscViewerASCIIOpen(PETSC_COMM_WORLD, "A.m", &viewer_a);
-  //PetscViewerSetFormat(viewer_a, PETSC_VIEWER_ASCII_MATLAB);
-  MatView(A.mat().mat(), viewer_a);
-
-  dolfin::cout << "Mapping: " << dolfin::endl;
-  std::map<const dolfin::uint, dolfin::uint> map = dof_map_set[0].getMap();
-  for(dolfin::uint i=0; i<map.size(); ++i)
-  {
-    dolfin::cout << i << " => " << map[i] << dolfin::endl;
-  }
-  PetscViewer viewer(PETSC_VIEWER_STDOUT_WORLD);
-  PetscViewerASCIIOpen(PETSC_COMM_WORLD, "B.m", &viewer);
-  //PetscViewerSetFormat(viewer, PETSC_VIEWER_ASCII_MATLAB);
-  MatView(B.mat().mat(), viewer);
-}
-
-void timer(Mesh& mesh, MeshFunction<dolfin::uint>& partitions, int num_iterations)
-{
+  dolfin::cout << "Assembling with sequential assembler." << dolfin::endl;
   set("debug level", -1);
   Poisson2DBilinearForm a;
   Matrix A;
   Assembler assembler(mesh);
+
   tic();
   for(int i=0; i<num_iterations; ++i)
     assembler.assemble(A, a, true);
-  printf("Average assemble time: %.3e\n", toc() / static_cast<real>(num_iterations));
+  printf("Average assemble time: %.3e\n", toc()/static_cast<real>(num_iterations));
 }
 
 void p_timer(Mesh& mesh, MeshFunction<dolfin::uint>& partitions, int num_iterations)
 {
+  dolfin::cout << "Assembling with parallel assembler." << dolfin::endl;
   set("debug level", -1);
   pPoisson2DBilinearForm a;
   Matrix B;
   pAssembler passembler(mesh, partitions);
+
   tic();
   for(int i=0; i<num_iterations; ++i)
     passembler.assemble(B, a, true);
-  printf("Processor %d: Average assemble time: %.3e\n", dolfin::MPI::processNumber(), toc() / static_cast<real>(num_iterations));
+  printf("Processor %d: Average assemble time: %.3e\n", dolfin::MPI::processNumber(), toc()/static_cast<real>(num_iterations));
 }
-
 
 int main(int argc, char* argv[])
 {
-  if(argc < 3)
+  std::string meshfile, partitionsfile, resultfile;
+  std::string assembler = "parallel";
+  int cells = 200;
+  int num_iterations = 1;
+  int num_part = dolfin::MPI::numProcesses();
+  bool check = false;
+  bool sequential = false;
+
+  Options listOpts;
+  int switchInt;
+  listOpts.addOption("", "mesh", "Mesh File", true);
+  listOpts.addOption("", "partitions", "Mesh partitions file", true);
+  listOpts.addOption("", "sequential", "Run with sequential assembler", false);
+  listOpts.addOption("", "cells", "Number of cells", true);
+  listOpts.addOption("", "num_part", "Number of partitions", true);
+  listOpts.addOption("", "resultfile", "File to save results in", true);
+  listOpts.addOption("", "num_iterations", "Number of times to assemble", true);
+  listOpts.addOption("", "check", "Verify assembly result", false);
+
+  if (listOpts.parse(argc, argv))
+    while ((switchInt = listOpts.cycle()) >= 0)
+    {
+      switch(switchInt)
+      {
+        case 0:
+          meshfile = listOpts.getArgs(switchInt);
+          break;
+        case 1:
+          partitionsfile = listOpts.getArgs(switchInt);
+          break;
+        case 2:
+          sequential = true;
+          break;
+        case 3:
+          cells = atoi(listOpts.getArgs(switchInt).c_str());
+          break;
+        case 4:
+          num_part = atoi(listOpts.getArgs(switchInt).c_str());
+          break;
+        case 5:
+          resultfile = listOpts.getArgs(switchInt);
+          break;
+        case 6:
+          num_iterations = atoi(listOpts.getArgs(switchInt).c_str());
+          break;
+        case 7:
+          check = true;
+          break;
+        default:
+          break;
+      }
+    }
+
+  Mesh mesh;
+  MeshFunction<dolfin::uint>* partitions;
+  if(meshfile != "")
   {
-    std::cerr << "Usage: " << argv[0] << 
-      " <mesh_file> <num_iterations>\n";
-    exit(1);
+    dolfin::cout << "Reading mesh from file: " << meshfile << dolfin::endl;
+    mesh = Mesh(meshfile);
+  }
+  else
+  {
+    printf("Creating UnitSquare(%d,%d)\n", cells, cells);
+    mesh = UnitSquare(cells, cells);
+  }
+  if(partitionsfile != "")
+  {
+    dolfin::cout << "Reading partitions from file: " << partitionsfile << dolfin::endl;
+    partitions = new MeshFunction<dolfin::uint>(mesh, partitionsfile);
+  }
+  else
+  {
+    dolfin::cout << "Partitioning mesh into: " << num_part << " partitions" << dolfin::endl;
+    partitions = new MeshFunction<dolfin::uint>(mesh);
+    mesh.partition(num_part, *partitions);
+  }
+  if(sequential)
+  {
+    dolfin::cout << "Assembling with sequential assembler." << dolfin::endl;
+    printf("Number of iteration: %d Number of partitions: %d\n", num_iterations, num_part);
+    timer(mesh, num_iterations);
+  }
+  else
+  {
+    dolfin::cout << "Assembling with parallel assembler." << dolfin::endl;
+    printf("Number of iteration: %d Number of partitions: %d\n", num_iterations, num_part);
+    p_timer(mesh, *partitions, num_iterations);
   }
 
-  dolfin_init(argc, argv);
-
-  char* infilename = argv[1];
-  int num_iterations = atoi(argv[2]);
-
-  Mesh mesh(infilename);
-
-  MeshFunction<dolfin::uint> partitions;
-  mesh.partition(dolfin::MPI::numProcesses(), partitions);
-
-  if(dolfin::MPI::numProcesses() == 1)
-    timer(mesh, partitions, num_iterations);
-  else
-    p_timer(mesh, partitions, num_iterations);
-
-  check(mesh, partitions);
+  if(check)
+  {
+    set("debug level", 1);
+    dolfin::cout << "Verifying assembly" << dolfin::endl;
+    check_assembly(mesh, *partitions);
+  }
+  return 0;
 }

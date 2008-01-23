33c0a22f153b8a6497660fdd429509fd7d1d3839
Gustav Magnus Vikstrøm <gustavv@ifi.uio.no>
2008-01-23 15:17:34 +0100

    Added Poisson3D test and saving to file in pmesh.

--- src/sandbox/pmesh/main.cpp
+++ src/sandbox/pmesh/main.cpp
@@ -10,6 +10,8 @@
 #include <dolfin.h>
 #include "pPoisson2D.h"
 #include "Poisson2D.h"
+#include "pPoisson3D_1.h"
+#include "Poisson3D_1.h"
 #include <iostream>
 #include <fstream>
 #include "getopts.h"
@@ -17,42 +19,42 @@
 
 using namespace dolfin;
 
-void timer(Mesh& mesh, int num_iterations)
+real timer(Mesh& mesh, int num_iterations, Form& a)
 {
   dolfin::cout << "Assembling with sequential assembler." << dolfin::endl;
-  Poisson2DBilinearForm a;
   Matrix A;
   Assembler assembler(mesh);
 
   tic();
   for(int i=0; i<num_iterations; ++i)
     assembler.assemble(A, a, true);
-  printf("Average assemble time: %.3e\n", toc()/static_cast<real>(num_iterations));
+  return toc()/static_cast<real>(num_iterations);
 }
 
-void p_timer(Mesh& mesh, MeshFunction<dolfin::uint>& partitions, int num_iterations)
+real p_timer(Mesh& mesh, MeshFunction<dolfin::uint>& partitions, int num_iterations, pForm& a)
 {
   dolfin::cout << "Assembling with parallel assembler." << dolfin::endl;
-  pPoisson2DBilinearForm a;
   Matrix B;
   pAssembler passembler(mesh, partitions);
 
   tic();
   for(int i=0; i<num_iterations; ++i)
     passembler.assemble(B, a, true);
-  printf("Processor %d: Average assemble time: %.3e\n", dolfin::MPI::processNumber(), toc()/static_cast<real>(num_iterations));
+  return toc()/static_cast<real>(num_iterations);
 }
 
 int main(int argc, char* argv[])
 {
   std::string meshfile, partitionsfile, resultfile;
   std::string assembler = "parallel";
-  int cells = 200;
+  int cells = 400;
+  int cells_3D = 30;
   int num_iterations = 1;
   int num_part = dolfin::MPI::numProcesses();
   int debug = -1;
   bool check = false;
   bool sequential = false;
+  std::string testtype = "Poisson2D";
 
   Options listOpts;
   int switchInt;
@@ -65,6 +67,7 @@ int main(int argc, char* argv[])
   listOpts.addOption("", "num_iterations", "Number of times to assemble", true);
   listOpts.addOption("", "check", "Verify assembly result", false);
   listOpts.addOption("", "debug", "Prints debugging info", false);
+  listOpts.addOption("", "testtype", "Type of test: Poisson2D, Poisson3D", true);
 
   if (listOpts.parse(argc, argv))
     while ((switchInt = listOpts.cycle()) >= 0)
@@ -82,6 +85,7 @@ int main(int argc, char* argv[])
           break;
         case 3:
           cells = atoi(listOpts.getArgs(switchInt).c_str());
+          cells_3D = atoi(listOpts.getArgs(switchInt).c_str());
           break;
         case 4:
           num_part = atoi(listOpts.getArgs(switchInt).c_str());
@@ -98,6 +102,9 @@ int main(int argc, char* argv[])
         case 8:
           debug = 1;
           break;
+        case 9:
+          testtype = listOpts.getArgs(switchInt);
+          break;
         default:
           break;
       }
@@ -113,8 +120,16 @@ int main(int argc, char* argv[])
   }
   else
   {
-    printf("Creating UnitSquare(%d,%d)\n", cells, cells);
-    mesh = UnitSquare(cells, cells);
+    if(testtype == "Poisson3D")
+    {
+      printf("Creating UnitCube(%d,%d,%d)\n", cells_3D, cells_3D, cells_3D);
+      mesh = UnitCube(cells_3D, cells_3D, cells_3D);
+    }
+    else
+    {
+      printf("Creating UnitSquare(%d,%d)\n",  cells, cells);
+      mesh = UnitSquare(cells, cells);
+    }
   }
   if(partitionsfile != "")
   {
@@ -127,19 +142,44 @@ int main(int argc, char* argv[])
     partitions = new MeshFunction<dolfin::uint>(mesh);
     mesh.partition(num_part, *partitions);
   }
+  real time = 0;
   if(sequential)
   {
+    Form* a;
+    if(testtype == "Poisson3D")
+      a = new Poisson3D_1BilinearForm();
+    else
+      a = new Poisson2DBilinearForm();
+
+    dolfin::cout << "Running test " << testtype << dolfin::endl;
     dolfin::cout << "Assembling with sequential assembler." << dolfin::endl;
     printf("Number of iteration: %d Number of partitions: %d\n", num_iterations, num_part);
-    timer(mesh, num_iterations);
+    time = timer(mesh, num_iterations, *a);
   }
   else
   {
+    pForm* a;
+    if(testtype == "Poisson3D")
+      a = new pPoisson3D_1BilinearForm();
+    else
+      a = new pPoisson2DBilinearForm();
+
+    dolfin::cout << "Running test " << testtype << dolfin::endl;
     dolfin::cout << "Assembling with parallel assembler." << dolfin::endl;
     printf("Number of iteration: %d Number of partitions: %d\n", num_iterations, num_part);
-    p_timer(mesh, *partitions, num_iterations);
+    time = p_timer(mesh, *partitions, num_iterations, *a);
   }
-
+  if(resultfile != "")
+  {
+    dolfin::cout << "Appending results to " << resultfile << dolfin::endl;
+    std::ofstream outfile(resultfile.c_str(), std::ofstream::app);
+    outfile << cells << " " << dolfin::MPI::numProcesses() << " " << time << std::endl;
+    outfile.close();
+  }
+  else
+  {
+    printf("Average assemble time: %.3e\n", time);
+  }
   if(check)
   {
     dolfin::cout << "Verifying assembly" << dolfin::endl;

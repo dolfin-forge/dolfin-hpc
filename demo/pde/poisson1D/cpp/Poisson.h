fba3838d954bce2a4faeb4420642c9cbec1ad3bf
Anders Logg <logg@simula.no>
2008-04-28 13:18:08 +0200

    Activate plotting in Poisson 1D demo and fix demo so both C++ and Python return the same solution.

--- demo/pde/poisson1D/cpp/Poisson.h
+++ demo/pde/poisson1D/cpp/Poisson.h
@@ -1044,10 +1044,10 @@ public:
     const double G0_0_0 = det*Jinv_00*Jinv_00;
     
     // Compute element tensor
-    A[0] = 1*G0_0_0;
-    A[1] = -1*G0_0_0;
-    A[2] = -1*G0_0_0;
-    A[3] = 1*G0_0_0;
+    A[0] = 0.999999999999999*G0_0_0;
+    A[1] = -0.999999999999999*G0_0_0;
+    A[2] = -0.999999999999999*G0_0_0;
+    A[3] = 0.999999999999999*G0_0_0;
   }
 
 };
@@ -1861,352 +1861,6 @@ public:
 
 };
 
-/// This class defines the interface for a finite element.
-
-class UFC_PoissonLinearForm_finite_element_2: public ufc::finite_element
-{
-public:
-
-  /// Constructor
-  UFC_PoissonLinearForm_finite_element_2() : ufc::finite_element()
-  {
-    // Do nothing
-  }
-
-  /// Destructor
-  virtual ~UFC_PoissonLinearForm_finite_element_2()
-  {
-    // Do nothing
-  }
-
-  /// Return a string identifying the finite element
-  virtual const char* signature() const
-  {
-    return "Lagrange finite element of degree 1 on a interval";
-  }
-
-  /// Return the cell shape
-  virtual ufc::shape cell_shape() const
-  {
-    return ufc::interval;
-  }
-
-  /// Return the dimension of the finite element function space
-  virtual unsigned int space_dimension() const
-  {
-    return 2;
-  }
-
-  /// Return the rank of the value space
-  virtual unsigned int value_rank() const
-  {
-    return 0;
-  }
-
-  /// Return the dimension of the value space for axis i
-  virtual unsigned int value_dimension(unsigned int i) const
-  {
-    return 1;
-  }
-
-  /// Evaluate basis function i at given point in cell
-  virtual void evaluate_basis(unsigned int i,
-                              double* values,
-                              const double* coordinates,
-                              const ufc::cell& c) const
-  {
-    // Extract vertex coordinates
-    const double * const * element_coordinates = c.coordinates;
-    
-    // Compute Jacobian of affine map from reference cell
-    const double J_00 = element_coordinates[1][0] - element_coordinates[0][0];
-    
-    // Get coordinates and map to the reference (UFC) element
-    double x = (coordinates[0] - element_coordinates[0][0]) / J_00;
-    
-    // No mapping needed for 1D.
-    
-    // Reset values
-    *values = 0;
-    
-    // Map degree of freedom to element degree of freedom
-    const unsigned int dof = i;
-    
-    // Generate scalings not needed for 1D
-    
-    // Compute psitilde_a
-    const double psitilde_a_0 = 1;
-    const double psitilde_a_1 = x;
-    
-    // Compute basisvalues
-    const double basisvalue0 = 0.707106781186548*psitilde_a_0;
-    const double basisvalue1 = 1.22474487139159*psitilde_a_1;
-    
-    // Table(s) of coefficients
-    const static double coefficients0[2][2] = \
-    {{0.707106781186547, -0.408248290463863},
-    {0.707106781186547, 0.408248290463863}};
-    
-    // Extract relevant coefficients
-    const double coeff0_0 = coefficients0[dof][0];
-    const double coeff0_1 = coefficients0[dof][1];
-    
-    // Compute value(s)
-    *values = coeff0_0*basisvalue0 + coeff0_1*basisvalue1;
-  }
-
-  /// Evaluate all basis functions at given point in cell
-  virtual void evaluate_basis_all(double* values,
-                                  const double* coordinates,
-                                  const ufc::cell& c) const
-  {
-    throw std::runtime_error("The vectorised version of evaluate_basis() is not yet implemented.");
-  }
-
-  /// Evaluate order n derivatives of basis function i at given point in cell
-  virtual void evaluate_basis_derivatives(unsigned int i,
-                                          unsigned int n,
-                                          double* values,
-                                          const double* coordinates,
-                                          const ufc::cell& c) const
-  {
-    // Extract vertex coordinates
-    const double * const * element_coordinates = c.coordinates;
-    
-    // Compute Jacobian of affine map from reference cell
-    const double J_00 = element_coordinates[1][0] - element_coordinates[0][0];
-    
-    // Get coordinates and map to the reference (UFC) element
-    double x = (coordinates[0] - element_coordinates[0][0]) / J_00;
-    
-    // No mapping needed for 1D.
-    
-    // Compute number of derivatives
-    unsigned int num_derivatives = 1;
-    
-    for (unsigned int j = 0; j < n; j++)
-      num_derivatives *= 1;
-    
-    
-    // Declare pointer to two dimensional array that holds combinations of derivatives and initialise
-    unsigned int **combinations = new unsigned int *[num_derivatives];
-        
-    for (unsigned int j = 0; j < num_derivatives; j++)
-    {
-      combinations[j] = new unsigned int [n];
-      for (unsigned int k = 0; k < n; k++)
-        combinations[j][k] = 0;
-    }
-        
-    // Generate combinations of derivatives
-    for (unsigned int row = 1; row < num_derivatives; row++)
-    {
-      for (unsigned int num = 0; num < row; num++)
-      {
-        for (unsigned int col = n-1; col+1 > 0; col--)
-        {
-          if (combinations[row][col] + 1 > 0)
-            combinations[row][col] = 0;
-          else
-          {
-            combinations[row][col] += 1;
-            break;
-          }
-        }
-      }
-    }
-    
-    // Compute inverse of Jacobian
-    const double Jinv[1][1] =  {{1.0 / J_00}};
-    
-    // Declare transformation matrix
-    // Declare pointer to two dimensional array and initialise
-    double **transform = new double *[num_derivatives];
-        
-    for (unsigned int j = 0; j < num_derivatives; j++)
-    {
-      transform[j] = new double [num_derivatives];
-      for (unsigned int k = 0; k < num_derivatives; k++)
-        transform[j][k] = 1;
-    }
-    
-    // Construct transformation matrix
-    for (unsigned int row = 0; row < num_derivatives; row++)
-    {
-      for (unsigned int col = 0; col < num_derivatives; col++)
-      {
-        for (unsigned int k = 0; k < n; k++)
-          transform[row][col] *= Jinv[combinations[col][k]][combinations[row][k]];
-      }
-    }
-    
-    // Reset values
-    for (unsigned int j = 0; j < 1*num_derivatives; j++)
-      values[j] = 0;
-    
-    // Map degree of freedom to element degree of freedom
-    const unsigned int dof = i;
-    
-    // Generate scalings not needed for 1D
-    
-    // Compute psitilde_a
-    const double psitilde_a_0 = 1;
-    const double psitilde_a_1 = x;
-    
-    // Compute basisvalues
-    const double basisvalue0 = 0.707106781186548*psitilde_a_0;
-    const double basisvalue1 = 1.22474487139159*psitilde_a_1;
-    
-    // Table(s) of coefficients
-    const static double coefficients0[2][2] = \
-    {{0.707106781186547, -0.408248290463863},
-    {0.707106781186547, 0.408248290463863}};
-    
-    // Interesting (new) part
-    // Tables of derivatives of the polynomial base (transpose)
-    const static double dmats0[2][2] = \
-    {{0, 0},
-    {3.46410161513775, 0}};
-    
-    // Compute reference derivatives
-    // Declare pointer to array of derivatives on FIAT element
-    double *derivatives = new double [num_derivatives];
-    
-    // Declare coefficients
-    double coeff0_0 = 0;
-    double coeff0_1 = 0;
-    
-    // Declare new coefficients
-    double new_coeff0_0 = 0;
-    double new_coeff0_1 = 0;
-    
-    // Loop possible derivatives
-    for (unsigned int deriv_num = 0; deriv_num < num_derivatives; deriv_num++)
-    {
-      // Get values from coefficients array
-      new_coeff0_0 = coefficients0[dof][0];
-      new_coeff0_1 = coefficients0[dof][1];
-    
-      // Loop derivative order
-      for (unsigned int j = 0; j < n; j++)
-      {
-        // Update old coefficients
-        coeff0_0 = new_coeff0_0;
-        coeff0_1 = new_coeff0_1;
-    
-        if(combinations[deriv_num][j] == 0)
-        {
-          new_coeff0_0 = coeff0_0*dmats0[0][0] + coeff0_1*dmats0[1][0];
-          new_coeff0_1 = coeff0_0*dmats0[0][1] + coeff0_1*dmats0[1][1];
-        }
-    
-      }
-      // Compute derivatives on reference element as dot product of coefficients and basisvalues
-      derivatives[deriv_num] = new_coeff0_0*basisvalue0 + new_coeff0_1*basisvalue1;
-    }
-    
-    // Transform derivatives back to physical element
-    for (unsigned int row = 0; row < num_derivatives; row++)
-    {
-      for (unsigned int col = 0; col < num_derivatives; col++)
-      {
-        values[row] += transform[row][col]*derivatives[col];
-      }
-    }
-    // Delete pointer to array of derivatives on FIAT element
-    delete [] derivatives;
-    
-    // Delete pointer to array of combinations of derivatives and transform
-    for (unsigned int row = 0; row < num_derivatives; row++)
-    {
-      delete [] combinations[row];
-      delete [] transform[row];
-    }
-    
-    delete [] combinations;
-    delete [] transform;
-  }
-
-  /// Evaluate order n derivatives of all basis functions at given point in cell
-  virtual void evaluate_basis_derivatives_all(unsigned int n,
-                                              double* values,
-                                              const double* coordinates,
-                                              const ufc::cell& c) const
-  {
-    throw std::runtime_error("The vectorised version of evaluate_basis_derivatives() is not yet implemented.");
-  }
-
-  /// Evaluate linear functional for dof i on the function f
-  virtual double evaluate_dof(unsigned int i,
-                              const ufc::function& f,
-                              const ufc::cell& c) const
-  {
-    // The reference points, direction and weights:
-    const static double X[2][1][1] = {{{0}}, {{1}}};
-    const static double W[2][1] = {{1}, {1}};
-    const static double D[2][1][1] = {{{1}}, {{1}}};
-    
-    const double * const * x = c.coordinates;
-    double result = 0.0;
-    // Iterate over the points:
-    // Evaluate basis functions for affine mapping
-    const double w0 = 1.0 - X[i][0][0];
-    const double w1 = X[i][0][0];
-    
-    // Compute affine mapping y = F(X)
-    double y[1];
-    y[0] = w0*x[0][0] + w1*x[1][0];
-    
-    // Evaluate function at physical points
-    double values[1];
-    f.evaluate(values, y, c);
-    
-    // Map function values using appropriate mapping
-    // Affine map: Do nothing
-    
-    // Note that we do not map the weights (yet).
-    
-    // Take directional components
-    for(int k = 0; k < 1; k++)
-      result += values[k]*D[i][0][k];
-    // Multiply by weights 
-    result *= W[i][0];
-    
-    return result;
-  }
-
-  /// Evaluate linear functionals for all dofs on the function f
-  virtual void evaluate_dofs(double* values,
-                             const ufc::function& f,
-                             const ufc::cell& c) const
-  {
-    throw std::runtime_error("Not implemented (introduced in UFC v1.1).");
-  }
-
-  /// Interpolate vertex values from dof values
-  virtual void interpolate_vertex_values(double* vertex_values,
-                                         const double* dof_values,
-                                         const ufc::cell& c) const
-  {
-    // Evaluate at vertices and use affine mapping
-    vertex_values[0] = dof_values[0];
-    vertex_values[1] = dof_values[1];
-  }
-
-  /// Return the number of sub elements (for a mixed element)
-  virtual unsigned int num_sub_elements() const
-  {
-    return 1;
-  }
-
-  /// Create a new finite element for sub element i (for a mixed element)
-  virtual ufc::finite_element* create_sub_element(unsigned int i) const
-  {
-    return new UFC_PoissonLinearForm_finite_element_2();
-  }
-
-};
-
 /// This class defines the interface for a local-to-global mapping of
 /// degrees of freedom (dofs).
 
@@ -2503,154 +2157,6 @@ public:
 
 };
 
-/// This class defines the interface for a local-to-global mapping of
-/// degrees of freedom (dofs).
-
-class UFC_PoissonLinearForm_dof_map_2: public ufc::dof_map
-{
-private:
-
-  unsigned int __global_dimension;
-
-public:
-
-  /// Constructor
-  UFC_PoissonLinearForm_dof_map_2() : ufc::dof_map()
-  {
-    __global_dimension = 0;
-  }
-
-  /// Destructor
-  virtual ~UFC_PoissonLinearForm_dof_map_2()
-  {
-    // Do nothing
-  }
-
-  /// Return a string identifying the dof map
-  virtual const char* signature() const
-  {
-    return "FFC dof map for Lagrange finite element of degree 1 on a interval";
-  }
-
-  /// Return true iff mesh entities of topological dimension d are needed
-  virtual bool needs_mesh_entities(unsigned int d) const
-  {
-    switch ( d )
-    {
-    case 0:
-      return true;
-      break;
-    case 1:
-      return false;
-      break;
-    }
-    return false;
-  }
-
-  /// Initialize dof map for mesh (return true iff init_cell() is needed)
-  virtual bool init_mesh(const ufc::mesh& m)
-  {
-    __global_dimension = m.num_entities[0];
-    return false;
-  }
-
-  /// Initialize dof map for given cell
-  virtual void init_cell(const ufc::mesh& m,
-                         const ufc::cell& c)
-  {
-    // Do nothing
-  }
-
-  /// Finish initialization of dof map for cells
-  virtual void init_cell_finalize()
-  {
-    // Do nothing
-  }
-
-  /// Return the dimension of the global finite element function space
-  virtual unsigned int global_dimension() const
-  {
-    return __global_dimension;
-  }
-
-  /// Return the dimension of the local finite element function space
-  virtual unsigned int local_dimension() const
-  {
-    return 2;
-  }
-
-  // Return the geometric dimension of the coordinates this dof map provides
-  virtual unsigned int geometric_dimension() const
-  {
-    throw std::runtime_error("Not implemented (introduced in UFC v1.1).");
-  }
-
-  /// Return the number of dofs on each cell facet
-  virtual unsigned int num_facet_dofs() const
-  {
-    return 1;
-  }
-
-  /// Return the number of dofs associated with each cell entity of dimension d
-  virtual unsigned int num_entity_dofs(unsigned int d) const
-  {
-    throw std::runtime_error("Not implemented (introduced in UFC v1.1).");
-  }
-
-  /// Tabulate the local-to-global mapping of dofs on a cell
-  virtual void tabulate_dofs(unsigned int* dofs,
-                             const ufc::mesh& m,
-                             const ufc::cell& c) const
-  {
-    dofs[0] = c.entity_indices[0][0];
-    dofs[1] = c.entity_indices[0][1];
-  }
-
-  /// Tabulate the local-to-local mapping from facet dofs to cell dofs
-  virtual void tabulate_facet_dofs(unsigned int* dofs,
-                                   unsigned int facet) const
-  {
-    switch ( facet )
-    {
-    case 0:
-      dofs[0] = 0;
-      break;
-    case 1:
-      dofs[0] = 1;
-      break;
-    }
-  }
-
-  /// Tabulate the local-to-local mapping of dofs on entity (d, i)
-  virtual void tabulate_entity_dofs(unsigned int* dofs,
-                                    unsigned int d, unsigned int i) const
-  {
-    throw std::runtime_error("Not implemented (introduced in UFC v1.1).");
-  }
-
-  /// Tabulate the coordinates of all dofs on a cell
-  virtual void tabulate_coordinates(double** coordinates,
-                                    const ufc::cell& c) const
-  {
-    const double * const * x = c.coordinates;
-    coordinates[0][0] = x[0][0];
-    coordinates[1][0] = x[1][0];
-  }
-
-  /// Return the number of sub dof maps (for a mixed element)
-  virtual unsigned int num_sub_dof_maps() const
-  {
-    return 1;
-  }
-
-  /// Create a new dof_map for sub dof map i (for a mixed element)
-  virtual ufc::dof_map* create_sub_dof_map(unsigned int i) const
-  {
-    return new UFC_PoissonLinearForm_dof_map_2();
-  }
-
-};
-
 /// This class defines the interface for the tabulation of the cell
 /// tensor corresponding to the local contribution to a form from
 /// the integral over a cell.
@@ -2699,69 +2205,8 @@ public:
     const double G0_1 = det*c0_0_0_1;
     
     // Compute element tensor
-    A[0] = 0.333333333333333*G0_0 + 0.166666666666667*G0_1;
-    A[1] = 0.166666666666667*G0_0 + 0.333333333333333*G0_1;
-  }
-
-};
-
-/// This class defines the interface for the tabulation of the
-/// exterior facet tensor corresponding to the local contribution to
-/// a form from the integral over an exterior facet.
-
-class UFC_PoissonLinearForm_exterior_facet_integral_0: public ufc::exterior_facet_integral
-{
-public:
-
-  /// Constructor
-  UFC_PoissonLinearForm_exterior_facet_integral_0() : ufc::exterior_facet_integral()
-  {
-    // Do nothing
-  }
-
-  /// Destructor
-  virtual ~UFC_PoissonLinearForm_exterior_facet_integral_0()
-  {
-    // Do nothing
-  }
-
-  /// Tabulate the tensor for the contribution from a local exterior facet
-  virtual void tabulate_tensor(double* A,
-                               const double * const * w,
-                               const ufc::cell& c,
-                               unsigned int facet) const
-  {
-    // Extract vertex coordinates
-    
-    // Compute Jacobian of affine map from reference cell
-      
-    // Compute determinant of Jacobian
-      
-    // Compute inverse of Jacobian
-    
-    // Facet determinant 1D (vertex)
-    const double det = 1.0;
-    
-    // Compute coefficients
-    const double c1_0_0_0 = w[1][0];
-    const double c1_0_0_1 = w[1][1];
-    
-    // Compute geometry tensors
-    const double G0_0 = det*c1_0_0_0;
-    const double G0_1 = det*c1_0_0_1;
-    
-    // Compute element tensor for all facets
-    switch ( facet )
-    {
-    case 0:
-      A[0] = 1*G0_0;
-      A[1] = 0;
-      break;
-    case 1:
-      A[0] = 0;
-      A[1] = 1*G0_1;
-      break;
-    }
+    A[0] = 0.333333333333333*G0_0 + 0.166666666666666*G0_1;
+    A[1] = 0.166666666666666*G0_0 + 0.333333333333333*G0_1;
   }
 
 };
@@ -2800,7 +2245,7 @@ public:
   /// Return a string identifying the form
   virtual const char* signature() const
   {
-    return "w0_a0[0, 1] | vi0[0, 1]*va0[0, 1]*dX(0) + w1_a0[0, 1] | vi0[0, 1]*va0[0, 1]*ds(0)";
+    return "w0_a0[0, 1] | vi0[0, 1]*va0[0, 1]*dX(0)";
   }
 
   /// Return the rank of the global tensor (r)
@@ -2812,7 +2257,7 @@ public:
   /// Return the number of coefficients (n)
   virtual unsigned int num_coefficients() const
   {
-    return 2;
+    return 1;
   }
 
   /// Return the number of cell integrals
@@ -2824,7 +2269,7 @@ public:
   /// Return the number of exterior facet integrals
   virtual unsigned int num_exterior_facet_integrals() const
   {
-    return 1;
+    return 0;
   }
   
   /// Return the number of interior facet integrals
@@ -2844,9 +2289,6 @@ public:
     case 1:
       return new UFC_PoissonLinearForm_finite_element_1();
       break;
-    case 2:
-      return new UFC_PoissonLinearForm_finite_element_2();
-      break;
     }
     return 0;
   }
@@ -2862,9 +2304,6 @@ public:
     case 1:
       return new UFC_PoissonLinearForm_dof_map_1();
       break;
-    case 2:
-      return new UFC_PoissonLinearForm_dof_map_2();
-      break;
     }
     return 0;
   }
@@ -2878,7 +2317,7 @@ public:
   /// Create a new exterior facet integral on sub domain i
   virtual ufc::exterior_facet_integral* create_exterior_facet_integral(unsigned int i) const
   {
-    return new UFC_PoissonLinearForm_exterior_facet_integral_0();
+    return 0;
   }
 
   /// Create a new interior facet integral on sub domain i
@@ -2928,10 +2367,9 @@ class PoissonLinearForm : public dolfin::Form
 {
 public:
 
-  PoissonLinearForm(dolfin::Function& w0, dolfin::Function& w1) : dolfin::Form()
+  PoissonLinearForm(dolfin::Function& w0) : dolfin::Form()
   {
     __coefficients.push_back(&w0);
-    __coefficients.push_back(&w1);
   }
 
   /// Return UFC form

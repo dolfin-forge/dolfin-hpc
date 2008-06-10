7731d6583ecb79f271a0212ff2f3d064f88b1d0b
Anders Logg <logg@simula.no>
2008-06-10 19:05:26 +0200

    Add simple test for assembly over subdomains

--- sandbox/misc/main.cpp
+++ sandbox/misc/main.cpp
@@ -4,13 +4,27 @@
 
 using namespace dolfin;
 
+#include <dolfin.h>
+#include "MassMatrix2D.h"
+
+using namespace dolfin;
+
+class MySubDomain : public SubDomain
+{
+  bool inside(const dolfin::real *x, bool on_boundary) const
+  {
+    return true;
+  }
+};
+
 int main()
 {
-  UnitSquare mesh(10, 10);
-  mesh.disp();
+  UnitSquare mesh(3, 3);
 
-  StiffnessMatrix A(mesh);
+  PETScMatrix A;
+  MassMatrix2DBilinearForm a;
+  MySubDomain subDomain;
+  assemble(A, a, mesh, subDomain);
+
   A.disp();
-
-  return 0;
 }

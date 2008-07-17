4fa444dcd1dbd2f34be23309a612b9f297a0ae51
Garth N. Wells <gnw20@cam.ac.uk>
2008-07-17 23:59:01 +0100

    Get matrix-vector mult mtl4 sandbox working.
    Results look the same for uBLAS and MTL4.

--- sandbox/mtl4/mat_vec_mult_bench/main.cpp
+++ sandbox/mtl4/mat_vec_mult_bench/main.cpp
@@ -23,60 +23,59 @@ int main(int args, char* argv[])
 
   UnitCube mesh(N, N, N);
   //mesh.init(mesh.topology().dim());
-  Function f(mesh,1.0);
-  NSEMomentum3DBilinearForm a(f,f,f,f,f);
+  Array<real> f_array(0.0, 0.0, 1.0);
+  Function f_vec(mesh, f_array);
+  Function f_scalar(mesh, 1.0);
+  NSEMomentum3DBilinearForm a(f_vec, f_scalar, f_scalar, f_scalar, f_scalar);
 
 
-  if(1)
-    {
-      printf("MTL --------------------------------------------------------\n");
-      DirectAssembler ass(mesh); 
-      const int NN = 3*(N+1)*(N+1)*(N+1);
-      MTL_Matrix A(NN,NN,45);
+  cout << "MTL --------------------------------------------------------" << endl;;
+  DirectAssembler ass_mtl4(mesh); 
+  const int NN = 3*(N+1)*(N+1)*(N+1);
+  MTL_Matrix A_mtl4(NN, NN, 45);
 
-      ass.assemble(A,a,false);
+  ass_mtl4.assemble(A_mtl4, a, false);
 
-      MTL4_sparse_matrix& mat = A.mat(); 
-      MTL4_vector x1(NN, 3.14);
-      MTL4_vector x2(NN, 0.0);
+  MTL4_sparse_matrix& mat_mtl4 = A_mtl4.mat(); 
+  MTL4_vector x1_mtl4(NN);
+  MTL4_vector x2_mtl4(NN);
 
-      tic();
-      for(int i=0; i<N_rep; i++)
-	  x2 = mat*x1;
+  for(int i=0; i<NN; i++)
+    x1_mtl4[i] = 3.14;
 
-      printf("%d mat-vec mult, mat: %d-by-%d, elapsed %f seconds\n", 
-	     N_rep, NN, NN, toc());
-    }
-/*
-  if(1)
-    {
-      printf("UBLAS ------------------------------------------------------\n");
-      Assembler ass(mesh);
-      Matrix A;
-      tic(); ass.assemble(A,a); printf("assembly time: %f\n", toc());
+  tic();
+  for(int i = 0; i < N_rep; i++)
+	  x2_mtl4 = mat_mtl4*x1_mtl4;
+  real mtl_time = toc();
 
-      uBlasSparseMatrix& AA = A.down_cast<uBlasSparseMatrix>();
-      ublas_sparse_matrix& mat = AA.mat();
+  printf("%d mat-vec mult, mat: %d-by-%d, elapsed %f seconds\n", N_rep, NN, NN, mtl_time);
 
-      const int NN = A.size(0);
+  cout << "UBLAS ------------------------------------------------------" << endl;
+  Assembler ass_ublas(mesh);
+  Matrix A_ublas;
+  
+  ass_ublas.assemble(A_ublas, a); 
 
-      uBlasVector vec1(NN);
-      uBlasVector vec2(NN);
+  uBlasSparseMatrix& AA = A_ublas.down_cast<uBlasSparseMatrix>();
+  ublas_sparse_matrix& mat_ublas = AA.mat();
 
-      ublas_vector& x1 = vec1.vec();
-      ublas_vector& x2 = vec2.vec();
+  uBlasVector vec1_ublas(NN);
+  uBlasVector vec2_ublas(NN);
+
+  ublas_vector& x1_ublas = vec1_ublas.vec();
+  ublas_vector& x2_ublas = vec2_ublas.vec();
       
-      for(int i=0; i<NN; i++)
-	x1[i] = 3.14;
+  for(int i=0; i<NN; i++)
+    x1_ublas[i] = 3.14;
 
-      tic();
-      for(int i=0; i<N_rep; i++)
-	x2 = prod(mat,x1);
+  tic();
+  for(int i = 0; i < N_rep; i++)
+     ublas::axpy_prod(mat_ublas, x1_ublas, x2_ublas, true);
+  real ublas_time = toc();
 
-      printf("%d mat-vec mult, mat: %d-by-%d, elapsed %f seconds\n", 
-	     N_rep, NN, NN, toc());
+  printf("%d mat-vec mult, mat: %d-by-%d, elapsed %f seconds\n", N_rep, NN, NN, ublas_time);
 
-    }
-*/
+  cout << "Matrix-vector mult timing (MTL4, uBLAS): " << mtl_time << "  " << ublas_time << endl;
+
   return 0;
 }

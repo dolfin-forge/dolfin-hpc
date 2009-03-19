882646a739ca2ce08d7f53f0f24c9c98d446e5dd
Niclas Jansson <njansson@csc.kth.se>
2009-03-19 08:09:06 +0100

    Minor fixes

--- dolfin/main/SubSystemsManager.cpp
+++ dolfin/main/SubSystemsManager.cpp
@@ -88,7 +88,7 @@ void SubSystemsManager::initPETSc(int argc, char* argv[], bool cmd_line_args)
     return;
 
   // Get status of MPI before PETSc initialisation
-  const bool mpi_init_status = MPIinitialized();
+  //  const bool mpi_init_status = MPIinitialized();
 
   // FIXME: What does this do?
   if(cmd_line_args)
@@ -105,7 +105,7 @@ void SubSystemsManager::initPETSc(int argc, char* argv[], bool cmd_line_args)
   sub_systems_manager.petsc_initialized = true;
 
   // Determine if PETSc initialised MPI and is then responsible for MPI finalization
-  //  if(!mpi_init_status && MPIinitialized())
+  //if(!mpi_init_status && MPIinitialized())
     sub_systems_manager.petsc_controls_mpi = true;
 #else
   error("DOLFIN has not been configured for PETSc.");

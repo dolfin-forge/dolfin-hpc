ca26a66de014fba5632c5d228fac294d1bde477c
Niclas Jansson <njansson@csc.kth.se>
2011-04-01 21:34:29 +0200

    Added support for writing mesh files in parallel (w MPI I/O)

--- src/io/BinaryFile.cpp
+++ src/io/BinaryFile.cpp
@@ -2,7 +2,7 @@
 // Licensed under the GNU LGPL Version 2.1.
 //
 // First  added: 2009
-// Last changed: 2011-03-30
+// Last changed: 2011-04-01
 
 #include <fstream>
 #include <dolfin/common/types.h>
@@ -12,6 +12,10 @@
 #include <dolfin/mesh/MeshEditor.h>
 #include <dolfin/mesh/Mesh.h>
 #include <dolfin/mesh/Vertex.h>
+
+#ifdef HAVE_MPI
+#include <mpi.h>
+#endif
 
 using namespace dolfin;
 
@@ -143,35 +147,129 @@ void BinaryFile::operator>>(Mesh& mesh)
 //----------------------------------------------------------------------------
 void BinaryFile::operator<<(Mesh& mesh)
 {
-  std::ofstream fp(filename.c_str(), std::ofstream::binary);
 
-  // Write Header
   int dim = mesh.geometry().dim();
-  fp.write((char *)&dim, sizeof(int));
 
   int type = 0;
   CellType::Type cell_type = mesh.type().cellType();
   if (CellType::type2string(cell_type).c_str() == "tetrahedron")
     type = 1;
-  fp.write((char *)&type, sizeof(int));
+      
+  int num_vertices = (MPI::numProcesses() > 1 ? 
+		      mesh.distdata().global_numVertices() : 
+		      mesh.numVertices());
+  int num_cells = (MPI::numProcesses() > 1 ? 
+		      mesh.distdata().global_numCells() : mesh.numCells());
+  if (MPI::numProcesses() == 1) 
+  {    
+    std::ofstream fp(filename.c_str(), std::ofstream::binary);
+    
+    // Write Header
+    fp.write((char *)&dim, sizeof(int));
+    
+    fp.write((char *)&type, sizeof(int));
+    
+    // Write vertices 
+    fp.write((char *)&num_vertices, sizeof(int));      
+    for (VertexIterator v(mesh); !v.end(); ++v)
+      fp.write((char *)v->x(), dim * sizeof(real));
+    
+    // Write cells
+    fp.write((char *) &num_cells, sizeof(int));   
+    for (CellIterator c(mesh); !c.end(); ++c)
+      fp.write((char *)c->entities(0), (3 + type) * sizeof(int));
+        
+    fp.close();
+  }
+  else 
+  {
+    mesh.renumber();
+
+    MPI_File fh;
+
+    /* FIXME:
+     * Add MPI_Info data 
+     */
+    
+    MPI_File_open(dolfin::MPI::DOLFIN_COMM, (char *) filename.c_str(),
+		  MPI_MODE_WRONLY | MPI_MODE_CREATE, MPI_INFO_NULL, &fh);
+
+    // Write Header
+    if (MPI::processNumber() == 0) 
+    {
+      MPI_File_write(fh, &dim, 1, MPI_INT, MPI_STATUS_IGNORE);
+      MPI_File_write(fh, &type, 1, MPI_INT, MPI_STATUS_IGNORE);
+      MPI_File_write(fh, &num_vertices, 1, MPI_INT, MPI_STATUS_IGNORE);
+    }
+
+    // Write vertices
+    uint offset = 0;
+    uint vertex_data = dim * (mesh.numVertices() - mesh.distdata().num_ghost(0));
+#if ( MPI_VERSION > 1 )
+    MPI_Exscan(&vertex_data, &offset, 1, 
+	       MPI_UNSIGNED, MPI_SUM, MPI::DOLFIN_COMM);
+#else
+    MPI_Scan(&vertex_data, &offset, 1, 
+	     MPI_UNSIGNED, MPI_SUM, MPI::DOLFIN_COMM);
+    offset -= vertex_data;
+#endif
+
+    real *vertex_buffer = new real[vertex_data];
+    real *vp = &vertex_buffer[0];
+    for (VertexIterator v(mesh); !v.end(); ++v) 
+    {
+      if(!mesh.distdata().is_ghost(v->index(), 0))
+      {
+	*(vp++) = v->x()[0];
+	*(vp++) = v->x()[1];
+	if (dim == 3)
+	  *(vp++) = v->x()[2];
+      }
+    }
+
+    MPI_File_write_at(fh, 3*sizeof(int) + offset*sizeof(double), 
+		      vertex_buffer, vertex_data, 
+		      MPI_DOUBLE, MPI_STATUS_IGNORE);
+
+    delete[] vertex_buffer;
+
+    // Write Cells
+    if (MPI::processNumber() == 0)
+      MPI_File_write_at(fh, 3*sizeof(int) + 
+			(dim * num_vertices) * sizeof(double),
+			&num_cells, 1, MPI_INT, MPI_STATUS_IGNORE);
 
 
-  int size = mesh.numVertices();
-  fp.write((char *)&size, sizeof(int));  
+    offset = 0;
+    uint local_cell_entities = (3+type) * mesh.numCells();
+#if ( MPI_VERSION > 1 )
+    MPI_Exscan(&local_cell_entities, &offset, 1, 
+	       MPI_UNSIGNED, MPI_SUM, MPI::DOLFIN_COMM);
+#else
+    MPI_Scan(&local_cell_entities, &offset, 1, 
+	     MPI_UNSIGNED, MPI_SUM, MPI::DOLFIN_COMM);
+    offset -= local_cell_entities;
+#endif
+    
+    int *cell_buffer = new int[local_cell_entities];
+    int *cp = &cell_buffer[0];
+    for (CellIterator c(mesh); !c.end(); ++c) 
+      for (uint i = 0; i < c->numEntities(0); i++) 
+	*(cp++) = mesh.distdata().get_global(c->entities(0)[i], 0);
 
-  // Write vertices 
-  for (VertexIterator v(mesh); !v.end(); ++v)
-    fp.write((char *)v->x(), dim * sizeof(real));
+    MPI_File_write_at(fh, 4*sizeof(int) + 
+		      (dim * num_vertices) * sizeof(double) + 
+		      offset * sizeof(int), 
+		      cell_buffer,local_cell_entities , 
+		      MPI_INT, MPI_STATUS_IGNORE);    
 
-  size = mesh.numCells();
-  fp.write((char *) &size, sizeof(int));
+    delete[] cell_buffer;
 
-  // Write cells
-  for (CellIterator c(mesh); !c.end(); ++c)
-    fp.write((char *)c->entities(0), (3 + type) * sizeof(int));
-  
-  
-  fp.close();
+
+    MPI_File_close(&fh);
+    
+  }
+
   message(1, "Saved mesh to file %s in binary format.", filename.c_str());    
 }
 //----------------------------------------------------------------------------

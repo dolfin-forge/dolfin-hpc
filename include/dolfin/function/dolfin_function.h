39d564ec8a67b434541b23c990c17f4b5082f9bd
Aurélien Larcher <larcher@kth.se>
2013-05-24 21:55:09 +0200

    Remove common includes

--- include/dolfin/function/dolfin_function.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DOLFIN_FUNCTION_H
-#define __DOLFIN_FUNCTION_H
-
-// DOLFIN function interface
-
-#include <dolfin/function/Function.h>
-#include <dolfin/function/SpecialFunctions.h>
-
-#endif

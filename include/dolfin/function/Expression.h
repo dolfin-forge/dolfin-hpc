932cd0c9e637940b6a5964f33a450b086bff2372
Aurélien Larcher <larcher@kth.se>
2013-06-01 21:03:00 +0200

    Add RealReference class to simplify wrapping real in a Function

--- include/dolfin/function/Expression.h
+++ include/dolfin/function/Expression.h
@@ -6,33 +6,77 @@
 namespace dolfin
 {
 
-  /// Derived classes should implement the copy constructor if the allocate
-  /// new objects.
+/// Derived classes should implement the copy constructor if the allocate
+/// new objects.
 
-  class Expression
-  {
-  public:
+class Expression
+{
+public:
 
-    /// Create user-defined function
-    Expression() {};
+	/// Create user-defined function
+	Expression()
+	{
+	}
+	;
 
-    /// Return the rank of the value space
-    virtual uint rank() const = 0;
+	/// Return the rank of the value space
+	virtual uint rank() const = 0;
 
-    /// Return the dimension of the value space for axis i
-    virtual uint dim(uint i) const = 0;
+	/// Return the dimension of the value space for axis i
+	virtual uint dim(uint i) const = 0;
 
-    /// Evaluate function at given point
-    virtual void eval(real* values, const real* x) const = 0;
+	/// Evaluate function at given point
+	virtual void eval(real* values, const real* x) const = 0;
 
-  protected:
+protected:
 
-    /// Destructor
-    virtual ~Expression() {};
+	/// Destructor
+	virtual ~Expression()
+	{
+	}
 
-  private:
+private:
 
-  };
+};
+
+class RealReference: public Expression
+{
+public:
+
+	/// Create user-defined function
+	RealReference(real const r) :
+			Expression(),
+			r_(r)
+	{
+	}
+
+	/// Destructor
+	~RealReference()
+	{
+	}
+
+	/// Return the rank of the value space
+	virtual uint rank() const
+	{
+		return 0;
+	}
+
+	/// Return the dimension of the value space for axis i
+	virtual uint dim(uint i) const
+	{
+		return 1;
+	}
+
+	/// Evaluate function at given point
+	virtual void eval(real* values, const real* x) const
+	{
+		values[0] = r_;
+	}
+
+private:
+	real const& r_;
+
+};
 
 }
 

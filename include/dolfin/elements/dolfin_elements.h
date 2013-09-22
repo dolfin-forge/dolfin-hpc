46503669bcfa56daf78f20ca48be037d61dc2448
Niclas Jansson <njansson@csc.kth.se>
2013-09-22 23:01:22 +0200

    Revert old headers files, and try to get the merged branch to build...nope :(

---
+++ include/dolfin/elements/dolfin_elements.h
@@ -0,0 +1,10 @@
+#ifndef __DOLFIN_ELEMENTS_H
+#define __DOLFIN_ELEMENTS_H
+
+// DOLFIN element library
+
+#include <dolfin/elements/ElementLibrary.h>
+#include <dolfin/elements/ProjectionLibrary.h>
+
+#endif
+
--- include/dolfin/mesh/SubDomain.h
+++ include/dolfin/mesh/SubDomain.h
@@ -10,6 +10,7 @@
 #define __SUB_DOMAIN_H
 
 #include <dolfin/common/types.h>
+#include <dolfin/common/simple_array.h>
 #include "MeshFunction.h"
 
 namespace dolfin
@@ -34,12 +35,18 @@ namespace dolfin
     /// Destructor
     virtual ~SubDomain();
 
+    /// Return true for points inside the sub domain (used for subclassing through SWIG interface)
+    virtual bool inside(const simple_array<real>& x, bool on_boundary) const { return inside(x.data, on_boundary); }
+
     /// Return true for points inside the sub domain
     virtual bool inside(const real* x, bool on_boundary) const;
+    
+    /// Map coordinate x in domain H to coordinate y in domain G (used for subclassing through SWIG interface)
+    virtual void map(const simple_array<real>& x, simple_array<real>& y) const { map(x.data, y.data); }
 
     /// Map coordinate x in domain H to coordinate y in domain G (used for periodic boundary conditions)
     virtual void map(const real* x, real* y) const;
-
+    
     /// Set sub domain markers for given sub domain
     void mark(MeshFunction<uint>& sub_domains, uint sub_domain) const;
 

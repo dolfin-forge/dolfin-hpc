e6b855b11c238902fa5991317fb4258e0f57a851
Niclas Jansson <njansson@csc.kth.se>
2013-10-07 21:41:16 +0200

    Minor fixes and dead code cleanup

--- include/dolfin/mesh/MeshData.h
+++ include/dolfin/mesh/MeshData.h
@@ -23,9 +23,8 @@ namespace dolfin
   /// entities or Arrays. Each dataset is identified by a unique
   /// user-specified string.
   ///
-  /// Currently, only uint-valued data is supported. 
-	/// Extended so that also int and float values are supported
-	//TODO maybe templates make sense here.
+  /// Currently, only uint-valued data is supported.
+
   class MeshData
   {
   public:
@@ -50,31 +49,7 @@ namespace dolfin
 
     /// Return Array with given name (returning zero if data is not available)
     Array<uint>* array(std::string name);
-/*
-		/// Create MeshFunction with given name (uninitialized)
-    MeshFunction<int>* createMeshFunctionInt(std::string name);
 
-    /// Create Array with given name and size
-    Array<int>* createArrayInt(std::string name, uint size);
-    
-    /// Return MeshFunction with given name (returning zero if data is not available)
-    MeshFunction<int>* meshFunctionInt(std::string name);
-
-    /// Return Array with given name (returning zero if data is not available)
-    Array<int>* arrayInt(std::string name);
-
-    /// Create MeshFunction with given name (uninitialized)
-    MeshFunction<real>* createMeshFunctionReal(std::string name);
-
-    /// Create Array with given name and size
-    Array<real>* createArrayReal(std::string name, uint size);
-    
-    /// Return MeshFunction with given name (returning zero if data is not available)
-    MeshFunction<real>* meshFunctionReal(std::string name);
-
-    /// Return Array with given name (returning zero if data is not available)
-    Array<real>* arrayReal(std::string name);
-*/
     /// Display data
     void disp() const;
 
@@ -88,19 +63,7 @@ namespace dolfin
 
     // A map from named mesh data to Arrays
     std::map<std::string, Array<uint>*> arrays;
-/*
-    // A map from named mesh data to MeshFunctions
-    std::map<std::string, MeshFunction<int>*> meshfunctions_int;
 
-    // A map from named mesh data to Arrays
-    std::map<std::string, Array<int>*> arrays_int;
-
-    // A map from named mesh data to MeshFunctions
-    std::map<std::string, MeshFunction<real>*> meshfunctions_real;
-
-    // A map from named mesh data to Arrays
-    std::map<std::string, Array<real>*> arrays_real;
-*/
   };
 
 }

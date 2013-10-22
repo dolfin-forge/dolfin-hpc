448e48b8aaf3feab5e0eec3a226753117826746e
Niclas Jansson <njansson@csc.kth.se>
2013-10-22 08:15:35 +0200

    Revert back to oldschool NaN test, closes #108

--- src/adaptivity/SpaceTimeFunction.cpp
+++ src/adaptivity/SpaceTimeFunction.cpp
@@ -80,7 +80,7 @@ void SpaceTimeFunction::eval(real t)
   real t0 = (*it0).first;
   real t1 = (*it1).first;
   
-  if (isnan(t0) || isnan(t1))
+  if (t0 != t0 || t1 != t1)
   {
     error("At least one of the iteration times"
 	  "used for interpolation is a Nan.");

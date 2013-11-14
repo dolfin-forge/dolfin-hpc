9b814787a8a912ddc2e3d48b0b1f8a5460c57c54
Kaspar Müller <kasparm@csc.kth.se>
2013-11-14 09:28:31 +0100

    Added signatures for UFC1

--- include/dolfin/elements/FE.h
+++ include/dolfin/elements/FE.h
@@ -37,6 +37,7 @@ struct attributes {
 };
 
 #if ENABLE_UFL
+//UFC2.1
 //-----------------------------------------------------------------------------
 static char const FINITE_ELEMENT     [] =  "FiniteElement";
 static char const VECTOR_ELEMENT     [] =  "VectorElement";
@@ -55,6 +56,7 @@ static char const LAGRANGE2DP2V[] =  "VectorElement('Lagrange', Cell('triangle',
 static char const LAGRANGE3DP1V[] =  "VectorElement('Lagrange', Cell('tetrahedron', Space(3)), 1, 3, None)";
 static char const LAGRANGE3DP2V[] =  "VectorElement('Lagrange', Cell('tetrahedron', Space(3)), 2, 3, None)";
 
+//-----------------------------------------------------------------------------
 static char const DG           [] =  "Discontinuous Lagrange";
 static char const DG1DP0S      [] =  "FiniteElement('Discontinuous Lagrange', Cell('interval', Space(1)), 0, None)";
 static char const DG1DP1S      [] =  "FiniteElement('Discontinuous Lagrange', Cell('interval', Space(1)), 1, None)";
@@ -72,8 +74,52 @@ static char const DG3DP0V      [] =  "VectorElement('Discontinuous Lagrange', Ce
 static char const DG3DP1V      [] =  "VectorElement('Discontinuous Lagrange', Cell('tetrahedron', Space(3)), 1, 3, None)";
 static char const DG3DP2V      [] =  "VectorElement('Discontinuous Lagrange', Cell('tetrahedron', Space(3)), 2, 3, None)";
 
+//-----------------------------------------------------------------------------
 static char const BDM          [] =  "Brezzi-Douglas-Marini";
 static char const BDM2DP1      [] =  "FiniteElement('Brezzi-Douglas-Marini', Cell('triangle', Space(2)), 1, None)";
+
+
+#else
+// UFC1.1
+//-----------------------------------------------------------------------------
+static char const FINITE_ELEMENT     [] =  "FiniteElement";
+static char const VECTOR_ELEMENT     [] =  "VectorElement";
+static char const MIXED_ELEMENT      [] =  "MixedElement";
+
+//-----------------------------------------------------------------------------
+static char const LAGRANGE     [] =  "Lagrange";
+static char const LAGRANGE1DP1S[] =  "Lagrange finite element of degree 1 on a interval";
+static char const LAGRANGE1DP2S[] =  "Lagrange finite element of degree 2 on a interval";
+static char const LAGRANGE2DP1S[] =  "Lagrange finite element of degree 1 on a triangle";
+static char const LAGRANGE2DP2S[] =  "Lagrange finite element of degree 2 on a triangle";
+static char const LAGRANGE3DP1S[] =  "Lagrange finite element of degree 1 on a tetrahedron";
+static char const LAGRANGE3DP2S[] =  "Lagrange finite element of degree 2 on a tetrahedron";
+static char const LAGRANGE2DP1V[] =  "Mixed finite element: [Lagrange finite element of degree 1 on a triangle, Lagrange finite element of degree 1 on a triangle]";
+static char const LAGRANGE2DP2V[] =  "Mixed finite element: [Lagrange finite element of degree 2 on a triangle, Lagrange finite element of degree 2 on a triangle]";
+static char const LAGRANGE3DP1V[] =  "Mixed finite element: [Lagrange finite element of degree 1 on a tetrahedron, Lagrange finite element of degree 1 on a tetrahedron, Lagrange finite element of degree 1 on a tetrahedron]";
+static char const LAGRANGE3DP2V[] =  "Mixed finite element: [Lagrange finite element of degree 2 on a tetrahedron, Lagrange finite element of degree 2 on a tetrahedron, Lagrange finite element of degree 2 on a tetrahedron]";
+
+//-----------------------------------------------------------------------------
+static char const DG           [] =  "Discontinuous Lagrange";
+static char const DG1DP0S      [] =  "Discontinuous Lagrange finite element of degree 0 on a interval";
+static char const DG1DP1S      [] =  "Discontinuous Lagrange finite element of degree 1 on a interval";
+static char const DG1DP2S      [] =  "Discontinuous Lagrange finite element of degree 2 on a interval";
+static char const DG2DP0S      [] =  "Discontinuous Lagrange finite element of degree 0 on a triangle";
+static char const DG2DP1S      [] =  "Discontinuous Lagrange finite element of degree 1 on a triangle";
+static char const DG2DP2S      [] =  "Discontinuous Lagrange finite element of degree 2 on a triangle";
+static char const DG3DP0S      [] =  "Discontinuous Lagrange finite element of degree 0 on a tetrahedron";
+static char const DG3DP1S      [] =  "Discontinuous Lagrange finite element of degree 1 on a tetrahedron";
+static char const DG3DP2S      [] =  "Discontinuous Lagrange finite element of degree 0 on a tetrahedron";
+static char const DG2DP0V      [] =  "Mixed finite element: [Discontinuous Lagrange finite element of degree 0 on a triangle, Discontinuous Lagrange finite element of degree 0 on a triangle]";
+static char const DG2DP1V      [] =  "Mixed finite element: [Discontinuous Lagrange finite element of degree 1 on a triangle, Discontinuous Lagrange finite element of degree 1 on a triangle]";
+static char const DG2DP2V      [] =  "Mixed finite element: [Discontinuous Lagrange finite element of degree 2 on a triangle, Discontinuous Lagrange finite element of degree 2 on a triangle]";
+static char const DG3DP0V      [] =  "Mixed finite element: [Discontinuous Lagrange finite element of degree 0 on a tetrahedron, Discontinuous Lagrange finite element of degree 0 on a tetrahedron, Discontinuous Lagrange finite element of degree 0 on a tetrahedron]";
+static char const DG3DP1V      [] =  "Mixed finite element: [Discontinuous Lagrange finite element of degree 1 on a tetrahedron, Discontinuous Lagrange finite element of degree 1 on a tetrahedron, Discontinuous Lagrange finite element of degree 1 on a tetrahedron]";
+static char const DG3DP2V      [] =  "Mixed finite element: [Discontinuous Lagrange finite element of degree 2 on a tetrahedron, Discontinuous Lagrange finite element of degree 2 on a tetrahedron, Discontinuous Lagrange finite element of degree 2 on a tetrahedron]";
+
+//-----------------------------------------------------------------------------
+static char const BDM          [] =  "Brezzi-Douglas-Marini";
+static char const BDM2DP1      [] =  "Brezzi-Douglas-Marini finite element of degree 1 on a triangle";
 
 #endif
 

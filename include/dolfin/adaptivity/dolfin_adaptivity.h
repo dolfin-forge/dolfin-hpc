8c0cab083a072966e4fd13f5900f0131ce4edb85
Aurélien Larcher <aurelien.larcher@gmail.com>
2014-01-13 11:28:30 +0100

    Missing change of header to adapt to ufc1 or ufc2 choice in dolfin_adaptivity.h

--- include/dolfin/adaptivity/dolfin_adaptivity.h
+++ include/dolfin/adaptivity/dolfin_adaptivity.h
@@ -2,10 +2,15 @@
 #define __DOLFIN_ADAPTIVITY_H
 
 // DOLFIN adaptivity interface
-
+#include <dolfin/config/dolfin_config.h>
 #include <dolfin/adaptivity/AdaptiveRefinement.h>
-#include <dolfin/adaptivity/AdaptiveRefinementProjectScalar.h>
-#include <dolfin/adaptivity/AdaptiveRefinementProjectVector.h>
+#ifdef ENABLE_UFL
+#include <dolfin/adaptivity/ufc2/AdaptiveRefinementProjectScalar.h>
+#include <dolfin/adaptivity/ufc2/AdaptiveRefinementProjectVector.h>
+#else
+#include <dolfin/adaptivity/ufc1/AdaptiveRefinementProjectScalar.h>
+#include <dolfin/adaptivity/ufc1/AdaptiveRefinementProjectVector.h>
+#endif
 #include <dolfin/adaptivity/SpaceTimeFunction.h>
 
 #endif

5e5655c9016953b0f812af7aec3f410558c9c3d9
Aurélien Larcher <aurelien.larcher@gmail.com>
2014-01-29 18:58:09 +0100

    Adding UFLFamily

---
+++ include/dolfin/ufl/UFLFamily.h
@@ -0,0 +1,70 @@
+// Copyright (C) 2014 Aurélien Larcher.
+// Licensed under the GNU LGPL Version 2.1.
+//
+// First added:  2014-01-28
+// Last changed: 2014-01-28
+
+#ifndef __UFL_FAMILY_H_
+#define __UFL_FAMILY_H_
+
+#include <dolfin/ufl/UFLType.h>
+
+#include <map>
+#include <set>
+#include <string>
+
+namespace ufl
+{
+
+/**
+ *  DOCUMENTATION:
+ *
+ *  @class  UFLFamily
+ *
+ *  @brief  Provides a C++ equivalent to family types.
+ */
+
+class Family : public Type<std::string>
+{
+
+public:
+
+  enum Type
+  {
+    ARG,
+    AW,
+    BDFM,
+    BDM,
+    CR,
+    DG,
+    HER,
+    CG,
+    MTW,
+    MOR,
+    N1curl,
+    N2curl,
+    RT,
+    BQ,
+    B,
+    Q,
+    R,
+    U,
+    Mixed,
+    Vector,
+    Tensor,
+    Enriched,
+    Restricted
+  };
+
+  Family(Family::Type const& t);
+
+  ~Family();
+
+private:
+
+  Type const type_;
+
+};
+
+} /* namespace ufl */
+#endif /* __UFL_FAMILY_H_ */
---
+++ src/ufl/UFLFamily.cpp
@@ -0,0 +1,26 @@
+// Copyright (C) 2014 Aurélien Larcher.
+// Licensed under the GNU LGPL Version 2.1.
+//
+// First added:  2014-01-28
+// Last changed: 2014-01-28
+
+#include <dolfin/ufl/UFLFamily.h>
+#include <dolfin/ufl/UFLElementList.h>
+
+namespace ufl
+{
+
+//-----------------------------------------------------------------------------
+Family::Family(Family::Type const& t) :
+    ufl::Type<std::string>(ElementList::Supported().name(t),
+                           "'" + ElementList::Supported().name(t) + "'"),
+    type_(t)
+{
+}
+
+//-----------------------------------------------------------------------------
+Family::~Family()
+{
+}
+
+} /* namespace icorne */

fcf4376652eec2ac35a36f1241ba9b94d4a2698c
Aurélien Larcher <aurelien.larcher@gmail.com>
2014-02-01 02:05:56 +0100

    Missing function directory for tests

---
+++ tests/function/function_test.cpp
@@ -0,0 +1,67 @@
+#include <dolfin/config/dolfin_config.h>
+
+#include <iostream>
+#include <iomanip>
+
+#ifdef HAVE_CHECK
+
+#include <check.h>
+
+int argc;
+char * argv;
+
+void setup()
+{
+}
+
+void teardown()
+{
+}
+
+//-----------------------------------------------------------------------------
+START_TEST( test_init_constant )
+  {
+    int init_failed = 0;
+
+    fail_unless( init_failed == 0 );
+  }END_TEST
+//-----------------------------------------------------------------------------
+
+Suite *ufl_suite()
+{
+  TCase *tc;
+  Suite *s;
+
+  s = suite_create("FUNCTION");
+  tc = tcase_create("function");
+
+  tcase_add_test(tc, test_init_constant);
+
+  suite_add_tcase(s, tc);
+  tcase_add_checked_fixture(tc, setup, teardown);
+
+  return s;
+}
+
+int main(void)
+{
+  int number_failed;
+  Suite* s = ufl_suite();
+  SRunner* sr = srunner_create(s);
+
+  srunner_run_all(sr, CK_NORMAL);
+  number_failed = srunner_ntests_failed(sr);
+  srunner_free(sr);
+
+  return (number_failed == 0) ? 0 : 1;
+}
+
+#else
+
+int main(void)
+{
+  fprintf(stderr, "*** Check is required for dolfin/function tests ***\n");
+  return 0;
+}
+
+#endif

eef7d23434560abac7fa16025c432a886c6b4019
Aurélien Larcher <aurelien.larcher@gmail.com>
2014-02-04 00:31:18 +0100

    Unused variable and debugging output in UFLObject

--- include/dolfin/ufl/UFLObject.h
+++ include/dolfin/ufl/UFLObject.h
@@ -124,7 +124,6 @@ inline std::vector<Object::repr_t> const Object::make_args_repr(
   size_t scpos = 0;
   size_t currpos = 0;
   size_t openbrace = 0;
-  size_t closebrace = 0;
   std::string token;
   while (scpos != std::string::npos)
   {
@@ -142,7 +141,6 @@ inline std::vector<Object::repr_t> const Object::make_args_repr(
     {
       token = str.substr(0, scpos);
       args.push_back(Object::repr_t(token));
-      std::cout << "new arg = " << token << std::endl;
       str.erase(0, scpos + delimiter.length());
       currpos = 0;
     }

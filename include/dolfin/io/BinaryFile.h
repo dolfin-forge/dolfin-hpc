da856c35de1cea8347c2a4af9f40cccf5b271db8
Aurélien Larcher <aurelien.larcher@gmail.com>
2014-02-09 19:33:30 +0100

    Nasty tabs in BinaryFile

--- include/dolfin/io/BinaryFile.h
+++ include/dolfin/io/BinaryFile.h
@@ -21,12 +21,12 @@ namespace dolfin
 {
   class BinaryFile : public GenericFile
   {
-    
+
   public:
     BinaryFile(const std::string filename);
     BinaryFile(const std::string filename, real& t);
     ~BinaryFile();
-    
+
     // Input
     void operator>> (GenericVector& x);
     void operator>> (Mesh& mesh);
@@ -35,7 +35,7 @@ namespace dolfin
     void operator>> (MeshFunction<int>& meshfunction);
     void operator>> (MeshFunction<unsigned int>& meshfunction);
     void operator>> (MeshFunction<double>& meshfunction);
-    
+
     // Output
     void operator<< (GenericVector& x);
     void operator<< (Mesh& mesh);
@@ -47,16 +47,16 @@ namespace dolfin
 
 
   private:
-    
-    enum Binary_data_t { BINARY_MESH_DATA, 
-			 BINARY_VECTOR_DATA,
-			 BINARY_FUNCTION_DATA,
-			 BINARY_MESH_FUNCTION_DATA};
-    
+
+    enum Binary_data_t { BINARY_MESH_DATA,
+                         BINARY_VECTOR_DATA,
+                         BINARY_FUNCTION_DATA,
+                         BINARY_MESH_FUNCTION_DATA };
+
 
     typedef struct {
       uint32_t magic;
-      uint32_t bendian; 
+      uint32_t bendian;
       uint32_t pe_size;
       Binary_data_t type;
     } BinaryFileHeader;
@@ -66,28 +66,28 @@ namespace dolfin
       uint32_t dim;
       uint32_t size;
       real t;
-      char name[FNAME_LENGTH];      
+      char name[FNAME_LENGTH];
     } BinaryFunctionHeader;
-#endif      
+#endif
 
     typedef struct {
-      uint v1; 
+      uint v1;
       uint v2;
       uint v3;
       uint v4;
     } atomic_cell;
 
     template<class T>
-    void write_meshfunction(T& meshfunction);    
+    void write_meshfunction(T& meshfunction);
 
     template<class T>
-    void read_meshfunction(T& meshfunction, uint type);    
+    void read_meshfunction(T& meshfunction, uint type);
 
-    inline int vertex_owner(uint L, uint R, uint i ) 
+    inline int vertex_owner(uint L, uint R, uint i )
     {
       return (int) std::max( floor( (double) i / (double) (L + 1) ),
-			     floor( (double) ((double) i - (double) R) / 
-				    (double)  L));
+                             floor( (double) ((double) i - (double) R) /
+                                    (double)  L));
     };
 
 
@@ -95,28 +95,28 @@ namespace dolfin
 
     void write_function(std::vector<std::pair<Function*, std::string> >& f);
 
-    inline void hdr_check(BinaryFileHeader hdr, 
-			  Binary_data_t type, uint pe_size)
-    {     
-      
+    inline void hdr_check(BinaryFileHeader hdr,
+                          Binary_data_t type, uint pe_size)
+    {
+
       if (hdr.magic != BINARY_MAGIC)
-	error("Corrupt header");
+        error("Corrupt header");
 
 #ifdef HAVE_BIG_ENDIAN
       if (!hdr.bendian)
-	error("File written in little endian");
+        error("File written in little endian");
 #else
       if (hdr.bendian)
-	error("File written in big endian");
+        error("File written in big endian");
 #endif
 
       if (hdr.type != type)
-	error("Invalid data type in file");
-      
+        error("Invalid data type in file");
+
       if ((hdr.type == BINARY_FUNCTION_DATA ||
-	   hdr.type == BINARY_VECTOR_DATA) && (hdr.pe_size != pe_size))
-	error("File stored on %d PEs, currently running on %d PEs", 
-	      hdr.pe_size, pe_size);
+          hdr.type == BINARY_VECTOR_DATA) && (hdr.pe_size != pe_size))
+        error("File stored on %d PEs, currently running on %d PEs",
+              hdr.pe_size, pe_size);
     };
 
 

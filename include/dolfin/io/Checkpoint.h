7a754ae976777d42a2cbad4b6da23d79ccb8a6cb
Aurélien Larcher <aurelien.larcher@gmail.com>
2014-02-09 18:07:21 +0100

    Nasty tabs in Checkpoint.h, reordering of initializer list, unused variable local_size, forward declaration of Mesh instead of include.

--- include/dolfin/io/Checkpoint.h
+++ include/dolfin/io/Checkpoint.h
@@ -12,7 +12,6 @@
 #include <vector>
 
 #include <dolfin/la/Vector.h>
-#include <dolfin/mesh/Mesh.h>
 #include <dolfin/function/Function.h>
 
 #ifdef ENABLE_MPIIO
@@ -20,97 +19,101 @@
 #endif
 
 namespace dolfin
-{  
-  class Checkpoint 
-  {
-  public:
-    
-    Checkpoint();
-    ~Checkpoint();
-    
+{
 
-    void write(std::string fname,
-	       uint id, real t, Mesh& mesh,
-	       std::vector<Function *> func,
-	       std::vector<Vector *> vec,
-	       bool static_mesh = false);
+class Mesh;
 
-    void restart(std::string fname);
+class Checkpoint
+{
+public:
 
-    void load(Mesh& mesh);
-    void load(std::vector<Function *> func);
-    void load(std::vector<Vector *> vec);
+  Checkpoint();
+  ~Checkpoint();
 
-    inline bool restart() {return state == RESTART;};
 
-    inline dolfin::uint id()
-    { if(state != RESTART)
-	error("Shut her down, Scotty, she's sucking mud again!");
-	return _id;
-    };
+  void write(std::string fname,
+             uint id, real t, Mesh& mesh,
+             std::vector<Function *> func,
+             std::vector<Vector *> vec,
+             bool static_mesh = false);
 
-    inline dolfin::real restart_time()
-    { if(state != RESTART) 
-	error("Shut her down, Scotty, she's sucking mud again!");
-      return _t;
-    };
+  void restart(std::string fname);
 
-    inline void reset() 
-    { 
-      state = CHECKPOINT; restart_state = OPEN; 
-      hdr_initialized = false; disp_initialized = false;
-    };
+  void load(Mesh& mesh);
+  void load(std::vector<Function *> func);
+  void load(std::vector<Vector *> vec);
 
-  private:
-    
-#ifdef ENABLE_MPIIO
-    typedef MPI_File chkp_outstream;
-#else
-    typedef std::ofstream chkp_outstream;
-#endif
-    
-    void hdr_init(Mesh& mesh, bool static_mesh);
-    void write(Mesh& mesh, chkp_outstream& out);
-    void write(std::vector<Function *> func, chkp_outstream& out);
-    void write(std::vector<Vector *> vec, chkp_outstream& out);
+  inline bool restart() {return state == RESTART;};
 
-    enum CheckpointState {CHECKPOINT, RESTART};
-    enum RestartState {OPEN, MESH, FUNC, VEC};
-
-    CheckpointState state;
-    RestartState restart_state;
-    
-#ifdef ENABLE_MPIIO
-    MPI_File in;
-    MPI_Offset byte_offset;
-#else
-    std::ifstream in;
-#endif
-    
-    typedef struct {
-      CellType::Type type;
-      uint tdim;
-      uint gdim;
-      uint num_vertices;
-      uint num_cells;
-      uint num_entities;
-      uint num_centities;
-      uint num_coords;
-      uint num_ghosts;
-      uint num_shared;
-#ifdef ENABLE_MPIIO
-      uint offsets[5];
-      uint disp[5];
-#endif
-    } chkp_mesh_hdr;
-
-    chkp_mesh_hdr hdr;
-
-    uint n;
-    uint _id;
-    real _t;
-    bool hdr_initialized;
-    bool disp_initialized;
+  inline dolfin::uint id()
+  { if(state != RESTART)
+      error("Shut her down, Scotty, she's sucking mud again!");
+    return _id;
   };
+
+  inline dolfin::real restart_time()
+  { if(state != RESTART)
+      error("Shut her down, Scotty, she's sucking mud again!");
+    return _t;
+  };
+
+  inline void reset()
+  {
+    state = CHECKPOINT; restart_state = OPEN;
+    hdr_initialized = false; disp_initialized = false;
+  };
+
+private:
+
+#ifdef ENABLE_MPIIO
+  typedef MPI_File chkp_outstream;
+#else
+  typedef std::ofstream chkp_outstream;
+#endif
+
+  void hdr_init(Mesh& mesh, bool static_mesh);
+  void write(Mesh& mesh, chkp_outstream& out);
+  void write(std::vector<Function *> func, chkp_outstream& out);
+  void write(std::vector<Vector *> vec, chkp_outstream& out);
+
+  enum CheckpointState {CHECKPOINT, RESTART};
+  enum RestartState {OPEN, MESH, FUNC, VEC};
+
+  CheckpointState state;
+  RestartState restart_state;
+
+#ifdef ENABLE_MPIIO
+  MPI_Offset byte_offset;
+  MPI_File in;
+#else
+  std::ifstream in;
+#endif
+
+  typedef struct {
+    CellType::Type type;
+    uint tdim;
+    uint gdim;
+    uint num_vertices;
+    uint num_cells;
+    uint num_entities;
+    uint num_centities;
+    uint num_coords;
+    uint num_ghosts;
+    uint num_shared;
+#ifdef ENABLE_MPIIO
+    uint offsets[5];
+    uint disp[5];
+#endif
+  } chkp_mesh_hdr;
+
+  chkp_mesh_hdr hdr;
+
+  uint n;
+  uint _id;
+  real _t;
+  bool hdr_initialized;
+  bool disp_initialized;
+};
+
 }
 #endif

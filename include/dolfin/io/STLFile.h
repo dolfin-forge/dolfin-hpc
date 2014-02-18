36280fd920f9038f5da9508860dfd2cc9200144b
Aurélien Larcher <aurelien.larcher@gmail.com>
2014-02-18 20:21:25 +0100

    Tabs in STLFile

--- include/dolfin/io/STLFile.h
+++ include/dolfin/io/STLFile.h
@@ -9,44 +9,40 @@
 #include <dolfin/common/types.h>
 #include "GenericFile.h"
 
-
 namespace dolfin
 {
-  
-  class STLFile : public GenericFile
+
+class STLFile : public GenericFile
+{
+public:
+
+  STLFile(const std::string filename);
+  ~STLFile();
+
+  // Input
+  void operator>>(Mesh& mesh);
+
+private:
+
+  struct stl_vertex
   {
-  public:
-    
-    STLFile(const std::string filename);
-    ~STLFile();
+    double v1, v2, v3;
+    dolfin::uint index;
 
-    // Input
-    void operator>> (Mesh& mesh);    
-
-  private:
-    
-    struct stl_vertex
+    bool operator <(const stl_vertex& v) const
     {
-      double v1, v2, v3;
-      dolfin::uint index;
+      return (v1 < v.v1
+          || (v1 == v.v1 && (v2 < v.v2 || (v2 == v.v2 && v3 < v.v3))));
+    }
 
-      bool operator < (const stl_vertex& v) const
-      {
-	return (v1 < v.v1 || 
-		(v1 == v.v1 && 
-		 (v2 < v.v2 || (v2 == v.v2 && v3 < v.v3))));
-      }
-      
-      bool operator == (const stl_vertex& v) const
-      {
-	return (v1 == v.v1 && 
-		v2 == v.v2 &&
-		v3 == v.v3);
-      }
-    };        
-
+    bool operator ==(const stl_vertex& v) const
+    {
+      return (v1 == v.v1 && v2 == v.v2 && v3 == v.v3);
+    }
   };
-  
+
+};
+
 }
 
 #endif

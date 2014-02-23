3c80f8f8b589128b2ef5f21f64f4f6c191c0348c
Aurélien Larcher <aurelien.larcher@gmail.com>
2014-02-23 18:18:23 +0100

    Add basic test for ufl::type

--- tests/ufl/type/test.cpp
+++ tests/ufl/type/test.cpp
@@ -1,7 +1,17 @@
 #include <dolfin/config/dolfin_config.h>
 
+#include <dolfin/common/types.h>
+
+using dolfin::uint;
+using dolfin::real;
+
+#include <dolfin/ufl/UFLtype.h>
+
+using ufl::type;
+
 #include <iostream>
 #include <iomanip>
+#include <vector>
 
 #ifdef HAVE_CHECK
 
@@ -19,9 +29,48 @@ void teardown()
 }
 
 //-----------------------------------------------------------------------------
-START_TEST( test_init )
+START_TEST( test_init_uint )
   {
     int init_failed = 0;
+
+    for (uint d = 0; d < 4; ++d)
+    {
+      type<uint> t(d);
+      t.display();
+    }
+
+    fail_unless( init_failed == 0 );
+  }END_TEST
+//-----------------------------------------------------------------------------
+START_TEST( test_init_real )
+  {
+    int init_failed = 0;
+
+    for (uint d = 0; d < 4; ++d)
+    {
+      real val = 1.1 * (real) d;
+      type<real> t(val);
+      t.display();
+    }
+
+    fail_unless( init_failed == 0 );
+  }END_TEST
+//-----------------------------------------------------------------------------
+START_TEST( test_init_string )
+  {
+    int init_failed = 0;
+
+    std::vector<std::string> vals;
+    vals.push_back("'interval'");
+    vals.push_back("'triangle'");
+    vals.push_back("'tetrahedron'");
+
+    for (std::vector<std::string>::const_iterator it = vals.begin();
+        it != vals.end(); ++it)
+    {
+      type<std::string> t(*it);
+      t.display();
+    }
 
     fail_unless( init_failed == 0 );
   }END_TEST
@@ -35,7 +84,9 @@ Suite *ufl_suite()
   s = suite_create("UFL");
   tc = tcase_create("ufl");
 
-  tcase_add_test(tc, test_init );
+  tcase_add_test(tc, test_init_uint);
+  tcase_add_test(tc, test_init_real);
+  tcase_add_test(tc, test_init_string);
 
   suite_add_tcase(s, tc);
   tcase_add_checked_fixture(tc, setup, teardown);

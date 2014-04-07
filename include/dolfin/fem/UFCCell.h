6ac30e481c0657ea8ceec04ad960ffd5f74d4604
Aurélien Larcher <aurelien.larcher@gmail.com>
2014-04-07 22:16:08 +0200

    Add dolfin cell pointer to UFCCell.

--- include/dolfin/fem/UFCCell.h
+++ include/dolfin/fem/UFCCell.h
@@ -29,16 +29,18 @@ public:
   /// Create emtpy UFC cell
   UFCCell() :
       ufc::cell(),
-      num_vertices(0)
+      cell(NULL),
+      num_vertices_(0)
   {
   }
 
   /// Create UFC cell from DOLFIN cell
-  UFCCell(Cell& cell) :
+  UFCCell(Cell& dolfin_cell) :
       ufc::cell(),
-      num_vertices(0)
+      cell(&dolfin_cell),
+      num_vertices_(0)
   {
-    init(cell);
+    init(dolfin_cell);
   }
 
   /// Destructor
@@ -47,139 +49,157 @@ public:
     clear();
   }
 
-  /// Initialize UFC cell data
-  void init(Cell& cell)
-  {
-    // Clear old data
-    clear();
+  //
+  Cell const * cell;
 
-    // Set cell shape
-    switch (cell.type())
-    {
-      case CellType::interval:
-        cell_shape = ufc::interval;
-        num_vertices = 2;
-        break;
-      case CellType::triangle:
-        cell_shape = ufc::triangle;
-        num_vertices = 3;
-        break;
-      case CellType::tetrahedron:
-        cell_shape = ufc::tetrahedron;
-        num_vertices = 4;
-        break;
-      default:
-        error("Unknown cell type.");
-        break;
-    }
-
-    // Set topological dimension
-    topological_dimension = cell.mesh().topology().dim();
-
-    // Set geometric dimension
-    geometric_dimension = cell.mesh().geometry().dim();
-
-    // Set entity indices
-    entity_indices = new uint*[topological_dimension + 1];
-    entity_indices[topological_dimension] = new uint[1];
-    entity_indices[topological_dimension][0] = cell.index();
-
-    // Cell index (short-cut for entity_indices[topological_dimension][0])
-    index = entity_indices[topological_dimension][0];
-
-    // Two different cases
-    if (MPI::numProcesses() == 1)
-    {
-      // Single process, pointer to mesh topological data
-      for (uint d = 0; d < topological_dimension; d++)
-        entity_indices[d] = cell.entities(d);
-    }
-    else
-    {
-      // Parallel case, store topological data in object
-      for (uint d = 0; d < topological_dimension; d++)
-      {
-        entity_indices[d] = new uint[cell.numEntities(d)];
-        for (uint i = 0; i < cell.numEntities(d); i++)
-          entity_indices[d][i] = (cell.entities(d))[i];
-      }
-    }
-
-    /// Set vertex coordinates
-    uint* vertices = cell.entities(0);
-    coordinates = new real*[num_vertices];
-    for (uint i = 0; i < num_vertices; i++)
-      coordinates[i] = cell.mesh().geometry().x(vertices[i]);
-  }
+  // Initialize UFC cell data
+  void init(Cell& cell);
 
   // Clear UFC cell data
-  void clear()
-  {
-    if (entity_indices)
-    {
-      if (MPI::numProcesses() > 1)
-      {
-        for (uint i = 0; i < (topological_dimension + 1); i++)
-          delete[] entity_indices[i];
-        delete[] entity_indices;
-      }
-      else
-      {
-        delete[] entity_indices[topological_dimension];
-        delete[] entity_indices;
-      }
-    }
-    entity_indices = 0;
+  void clear();
 
-    if (coordinates)
-      delete[] coordinates;
-    coordinates = 0;
-
-    cell_shape = ufc::interval;
-    topological_dimension = 0;
-    geometric_dimension = 0;
-  }
-
-  // Update cell entities and coordinates
-  inline void update(Cell& cell, MeshDistributedData& distdata)
-  {
-
-    // Set entity indices
-    if (MPI::numProcesses() == 1)
-    {
-      for (uint d = 0; d < topological_dimension; d++)
-        entity_indices[d] = cell.entities(d);
-      entity_indices[topological_dimension][0] = cell.index();
-    }
-    else
-    {
-#if ENABLE_P1_OPTIMIZATIONS
-      for(uint i = 0; i < cell.numEntities(0); i++)
-      entity_indices[0][i] = distdata.get_vertex_global((cell.entities(0))[i]);
-#else
-      for (uint d = 0; d < topological_dimension; d++)
-        for (uint i = 0; i < cell.numEntities(d); i++)
-          entity_indices[d][i] = distdata.get_global((cell.entities(d))[i], d);
-#endif
-      entity_indices[topological_dimension][0] = distdata.get_cell_global(
-          cell.index());
-    }
-
-    // Cell index (short-cut for entity_indices[topological_dimension][0])
-    index = entity_indices[topological_dimension][0];
-
-    /// Set vertex coordinates
-    const uint* vertices = cell.entities(0);
-    for (uint i = 0; i < num_vertices; i++)
-      coordinates[i] = cell.mesh().geometry().x(vertices[i]);
-  }
+  // Update cell entities to global indices and coordinates
+  void update(Cell& cell, MeshDistributedData& distdata);
 
 private:
 
   // Number of cell vertices
-  uint num_vertices;
+  uint num_vertices_;
 
 };
+
+//--- INLINES -----------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+inline void UFCCell::init(Cell& cell)
+{
+  // Clear old data
+  clear();
+
+  // Update dolfin cell pointer
+  this->cell = &cell;
+
+  // Set cell shape
+  switch (cell.type())
+  {
+    case CellType::interval:
+      cell_shape = ufc::interval;
+      break;
+    case CellType::triangle:
+      cell_shape = ufc::triangle;
+      break;
+    case CellType::tetrahedron:
+      cell_shape = ufc::tetrahedron;
+      break;
+    default:
+      error("Unknown cell type.");
+      break;
+  }
+  num_vertices_ = cell.numEntities(0);
+
+  // Set topological dimension
+  topological_dimension = cell.mesh().topology().dim();
+
+  // Set geometric dimension
+  geometric_dimension = cell.mesh().geometry().dim();
+
+  // Set entity indices
+  entity_indices = new uint*[topological_dimension + 1];
+  entity_indices[topological_dimension] = new uint[1];
+  entity_indices[topological_dimension][0] = cell.index();
+
+  // Cell index (short-cut for entity_indices[topological_dimension][0])
+  index = entity_indices[topological_dimension][0];
+
+  // Two different cases
+  if (MPI::numProcesses() == 1)
+  {
+    // Single process, pointer to mesh topological data
+    for (uint d = 0; d < topological_dimension; ++d)
+      entity_indices[d] = cell.entities(d);
+  }
+  else
+  {
+    // Parallel case, store topological data in object
+    for (uint d = 0; d < topological_dimension; ++d)
+    {
+      entity_indices[d] = new uint[cell.numEntities(d)];
+      for (uint i = 0; i < cell.numEntities(d); ++i)
+        entity_indices[d][i] = (cell.entities(d))[i];
+    }
+  }
+
+  /// Set vertex coordinates
+  uint* vertices = cell.entities(0);
+  coordinates = new real*[num_vertices_];
+  for (uint i = 0; i < num_vertices_; ++i)
+    coordinates[i] = cell.mesh().geometry().x(vertices[i]);
+}
+
+//-----------------------------------------------------------------------------
+inline void UFCCell::clear()
+{
+  if (entity_indices)
+  {
+    if (MPI::numProcesses() > 1)
+    {
+      for (uint i = 0; i < (topological_dimension + 1); ++i)
+        delete[] entity_indices[i];
+      delete[] entity_indices;
+    }
+    else
+    {
+      delete[] entity_indices[topological_dimension];
+      delete[] entity_indices;
+    }
+  }
+  entity_indices = 0;
+
+  delete[] coordinates;
+  coordinates = 0;
+
+  cell_shape = ufc::interval;
+  topological_dimension = 0;
+  geometric_dimension = 0;
+}
+
+//-----------------------------------------------------------------------------
+inline void UFCCell::update(Cell& cell, MeshDistributedData& distdata)
+{
+  // Update dolfin cell pointer
+  this->cell = &cell;
+
+  // Set entity indices
+  if (MPI::numProcesses() == 1)
+  {
+    for (uint d = 0; d < topological_dimension; ++d)
+      entity_indices[d] = cell.entities(d);
+    entity_indices[topological_dimension][0] = cell.index();
+  }
+  else
+  {
+#if ENABLE_P1_OPTIMIZATIONS
+    for(uint i = 0; i < cell.numEntities(0); ++i)
+    entity_indices[0][i] = distdata.get_vertex_global((cell.entities(0))[i]);
+#else
+    for (uint d = 0; d < topological_dimension; ++d)
+      for (uint i = 0; i < cell.numEntities(d); ++i)
+        entity_indices[d][i] = distdata.get_global((cell.entities(d))[i], d);
+#endif
+    entity_indices[topological_dimension][0] = distdata.get_cell_global(
+        cell.index());
+  }
+
+  // Cell index (short-cut for entity_indices[topological_dimension][0])
+  index = entity_indices[topological_dimension][0];
+
+  /// Set vertex coordinates
+  const uint* vertices = cell.entities(0);
+  for (uint i = 0; i < num_vertices_; ++i)
+    coordinates[i] = cell.mesh().geometry().x(vertices[i]);
+}
+
+//-----------------------------------------------------------------------------
 
 }
 

8af9841306549df77f1818b69fefa90c0bb55748
Aurélien Larcher <aurelien.larcher@gmail.com>
2014-11-03 17:05:54 +0100

    Remove mesh classes

--- include/dolfin/mesh/dolfin_mesh.h
+++ include/dolfin/mesh/dolfin_mesh.h
@@ -25,13 +25,6 @@
 #include <dolfin/mesh/SubDomain.h>
 #include <dolfin/mesh/DomainBoundary.h>
 #include <dolfin/mesh/BoundaryMesh.h>
-#include <dolfin/mesh/UnitCube.h>
-#include <dolfin/mesh/UnitInterval.h>
-#include <dolfin/mesh/UnitSquare.h>
-#include <dolfin/mesh/UnitCircle.h>
-#include <dolfin/mesh/Box.h>
-#include <dolfin/mesh/Rectangle.h>
-#include <dolfin/mesh/UnitSphere.h>
 #include <dolfin/mesh/IntersectionDetector.h>
 #include <dolfin/mesh/RivaraRefinement.h>
 #include <dolfin/mesh/AffineMapping.h>

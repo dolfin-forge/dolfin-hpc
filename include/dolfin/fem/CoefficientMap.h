78b5d606b1f9d51508e12591347f8cfe126f8969
Aurélien Larcher <aurelien.larcher@gmail.com>
2014-11-10 15:52:31 +0100

    Add CoefficientMap class.

---
+++ include/dolfin/fem/CoefficientMap.h
@@ -0,0 +1,59 @@
+// Copyright (C) 2014 Aurélien Larcher.
+// Licensed under the GNU LGPL Version 2.1.
+//
+// First added:  2014-11-10
+// Last changed: 2014-11-10
+
+#ifndef __COEFFICIENT_MAP_H_
+#define __COEFFICIENT_MAP_H_
+
+#include <dolfin/common/Array.h>
+#include <dolfin/function/Function.h>
+
+#include <map>
+#include <string>
+
+namespace dolfin
+{
+
+class CoefficientMap
+{
+
+public:
+
+  /// Constructor
+  CoefficientMap();
+
+  /// Destructor
+  ~CoefficientMap();
+
+  /// Constructor
+  CoefficientMap(CoefficientMap const& other);
+
+  /// Assignment
+  CoefficientMap& operator=(CoefficientMap const& other);
+
+  /// Check if coefficient label is in the map
+  bool has(std::string const& label) const;
+
+  /// Get coefficient function mapped from the given label
+  Function * get(std::string const& label) const;
+
+  /// Return the size of the coefficient map
+  bool size() const;
+
+  /// Set coefficient label to map to the given function
+  void set(std::string const& label, dolfin::Function& coefficient);
+
+  /// Display information
+  void display() const;
+
+private:
+
+  std::map<std::string const, dolfin::Function *> map_;
+
+};
+
+}
+
+#endif /* __COEFFICIENT_MAP_H_ */
---
+++ src/fem/CoefficientMap.cpp
@@ -0,0 +1,97 @@
+// Copyright (C) 2014 Aurélien Larcher.
+// Licensed under the GNU LGPL Version 2.1.
+//
+// First added:  2014-11-10
+// Last changed: 2014-11-10
+
+#include <dolfin/fem/CoefficientMap.h>
+
+namespace dolfin
+{
+
+//-----------------------------------------------------------------------------
+CoefficientMap::CoefficientMap()
+{
+}
+
+//-----------------------------------------------------------------------------
+CoefficientMap::~CoefficientMap()
+{
+  map_.clear();
+}
+
+//-----------------------------------------------------------------------------
+CoefficientMap::CoefficientMap(CoefficientMap const& other)
+{
+  *this = other;
+}
+
+//-----------------------------------------------------------------------------
+CoefficientMap& CoefficientMap::operator=(CoefficientMap const& other)
+{
+  if (this == &other)
+  {
+    return *this;
+  }
+  this->map_ = other.map_;
+  return *this;
+}
+
+//-----------------------------------------------------------------------------
+bool CoefficientMap::has(std::string const& label) const
+{
+  return (map_.count(label) > 0);
+}
+
+//-----------------------------------------------------------------------------
+Function * CoefficientMap::get(std::string const& label) const
+{
+  std::map<std::string const, dolfin::Function *>::const_iterator it =
+      map_.find(label);
+  if (it == map_.end())
+  {
+    error("Coefficient label does not exist in map.");
+  }
+  return it->second;
+}
+
+//-----------------------------------------------------------------------------
+bool CoefficientMap::size() const
+{
+  return this->map_.size();
+}
+
+//-----------------------------------------------------------------------------
+void CoefficientMap::set(std::string const& label,
+                         dolfin::Function& coefficient)
+{
+  std::map<std::string const, dolfin::Function *>::iterator it = map_.find(
+      label);
+  if (it != map_.end())
+  {
+    error("Coefficient label is already assigned to a function.");
+  }
+  map_[label] = &coefficient;
+}
+
+//-----------------------------------------------------------------------------
+void CoefficientMap::display() const
+{
+  cout << "CoefficientMap" << endl;
+  cout << "--------------" << endl;
+
+  // Begin indentation
+  begin("");
+  cout << "Number of coefficients      :" << this->size() << endl;
+  uint ii = 0;
+  for (std::map<std::string const, dolfin::Function *>::const_iterator it =
+      map_.begin(); it != map_.end(); ++it)
+  {
+    cout << ii << " : " << it->first << endl;
+    ++ii;
+  }
+  end();
+}
+
+}
+

2309310588fd5c9af1092d039d2c55cd4d9a3e1b
Niclas Jansson <leifniclas.jansson@riken.jp>
2015-01-15 19:30:11 +0900

    Workarounds for Sun's C++ library (refs. #28)

--- include/dolfin/ufl/UFLObject.h
+++ include/dolfin/ufl/UFLObject.h
@@ -179,8 +179,15 @@ inline std::vector<Object::repr_t> const Object::make_args_repr(
     {
       open_delimiter_positions[i] = str.find(open_delimiters[i], currpos);
       close_delimiter_positions[i] = str.find(close_delimiters[i], currpos);
+#ifdef __SUNPRO_CC
+      n_open_delimiters[i] = 0;
+      n_close_delimiters[i] = 0;
+      std::count(str.begin(), it, open_delimiters[i], n_open_delimiters[i]);
+      std::count(str.begin(), it, close_delimiters[i], n_close_delimiters[i]);
+#else
       n_open_delimiters[i] = std::count(str.begin(), it, open_delimiters[i]);
       n_close_delimiters[i] = std::count(str.begin(), it, close_delimiters[i]);
+#endif
     }
 
     bool equal_number_open_close = true;
@@ -189,9 +196,16 @@ inline std::vector<Object::repr_t> const Object::make_args_repr(
         equal_number_open_close = false;
 
     open_pos = *std::min_element(open_delimiter_positions.begin(), open_delimiter_positions.end());
+#ifdef __SUNPRO_CC
+    dolfin::uint index = 0;
+    std::distance(open_delimiter_positions.begin(), 
+		  std::find(open_delimiter_positions.begin(),
+			    open_delimiter_positions.end(), open_pos), index);
+#else
     dolfin::uint index = std::distance(open_delimiter_positions.begin(), 
 				       std::find(open_delimiter_positions.begin(),
 						 open_delimiter_positions.end(), open_pos));
+#endif
     close_pos = close_delimiter_positions[index];
 
     // Take into account of

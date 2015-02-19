0cbdf13aeaf4334f14e7521449b197b1f95dcf40
Aurélien Larcher <aurelien.larcher@gmail.com>
2015-02-19 18:04:06 +0100

    Add simple Test class

---
+++ include/dolfin/common/Test.h
@@ -0,0 +1,63 @@
+// Copyright (C) 2010 Aurélien Larcher.
+// Licensed under the GNU LGPL Version 2.1.
+//
+// First added:  2014-03-13
+// Last changed: 2014-03-13
+
+#ifndef __TEST_H
+#define __TEST_H
+
+#include <dolfin.h>
+
+#include <mpi.h>
+
+#include <cassert>
+#include <iostream>
+
+namespace dolfin
+{
+
+class Test
+{
+
+  struct Args
+  {
+    uint debug_level;
+    std::string mesh_file;
+    bool benchmark;
+
+    Args() :
+        debug_level(0),
+        mesh_file(""),
+        benchmark(false)
+    {
+    }
+  };
+
+public:
+
+  ///
+  Test(int argc, char *argv[]);
+
+  ///
+  Test();
+
+  ///
+  void print_args();
+
+  ///
+  void begin_test(std::string const& description);
+
+  ///
+  void end_test();
+
+  ///
+  ~Test();
+
+  Args args;
+
+};
+
+}
+
+#endif /* __TEST_H */
---
+++ src/common/Test.cpp
@@ -0,0 +1,89 @@
+// Copyright (C) 2010 Aurélien Larcher.
+// Licensed under the GNU LGPL Version 2.1.
+//
+// First added:  2014-03-13
+// Last changed: 2014-03-13
+
+#include <dolfin/common/Test.h>
+
+#include <dolfin/log/log.h>
+#include <dolfin/log/LogManager.h>
+#include <dolfin/main/MPI.h>
+#include <dolfin/parameter/parameters.h>
+
+#include <cstdlib>
+#include <getopt.h>
+
+namespace dolfin
+{
+
+//-----------------------------------------------------------------------------
+Test::Test(int argc, char *argv[])
+{
+  dolfin_init(argc, argv);
+  if (dolfin::MPI::processNumber() == 0)
+  {
+    message("Running on %d %s", dolfin::MPI::numProcesses(),
+            (dolfin::MPI::numProcesses() > 1 ? "nodes" : "node"));
+  }
+
+  int flag;
+  while (-1 != (flag = getopt(argc, argv, "d:m:b")))
+  {
+    switch (flag)
+      {
+      case 'b':
+        args.benchmark = true;
+        break;
+      case 'd':
+        LogManager::logger().setDebugLevel(std::atoi(optarg));
+        break;
+      case 'm':
+        args.mesh_file = optarg;
+        break;
+      default:
+        message("Unknown or missing argument");
+        dolfin_finalize();
+        std::exit(1);
+        break;
+      }
+  }
+}
+
+//-----------------------------------------------------------------------------
+Test::Test()
+{
+}
+
+//-----------------------------------------------------------------------------
+void Test::print_args()
+{
+  if (dolfin::MPI::processNumber() == 0)
+  {
+    std::cout << "mesh : " << args.mesh_file << std::endl;
+  }
+}
+
+//-----------------------------------------------------------------------------
+void Test::begin_test(std::string const& description)
+{
+  begin("TEST:");
+  message(description);
+  tic();
+}
+
+//-----------------------------------------------------------------------------
+void Test::end_test()
+{
+  message("Completed in %16f s.", toc());
+  end();
+  skip();
+}
+
+//-----------------------------------------------------------------------------
+Test::~Test()
+{
+  dolfin_finalize();
+}
+
+}

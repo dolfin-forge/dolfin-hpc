e302b3b3dec81ff55e0acd23ef6af7bc265e2479
Niclas Jansson <leifniclas.jansson@riken.jp>
2015-03-29 13:19:52 +0900

    Restructure mesh tests

---
+++ tests/mesh/UnitMeshes/UnitMeshes.h
@@ -0,0 +1,106 @@
+#ifdef HAVE_CHECK
+
+#include <check.h>
+
+#include <dolfin/config/dolfin_config.h>
+
+#include <dolfin/common/Array.h>
+#include <dolfin/mesh/UnitInterval.h>
+#include <dolfin/mesh/UnitSquare.h>
+#include <dolfin/mesh/UnitCube.h>
+#include <dolfin/mesh/Box.h>
+#include <dolfin/mesh/UnitDisk.h>
+
+using namespace dolfin;
+
+int test_mesh(Mesh& mesh)
+{
+  message("Test mesh");
+  mesh.refine();
+  mesh.refine();
+  return 0;
+}
+
+//-----------------------------------------------------------------------------
+START_TEST( test_create_interval )
+{
+  dolfin::uint const Nx = 2;
+
+ // Interval
+  UnitInterval mesh(Nx);  
+  fail_unless(test_mesh(mesh) == 0);
+
+}END_TEST
+//-----------------------------------------------------------------------------
+START_TEST( test_create_square )
+{ 
+  dolfin::uint const Nx = 2;
+  dolfin::uint const Ny = 4;
+
+  // Square
+  Array<UnitSquare::Type> types;
+  types.push_back(UnitSquare::right);
+  types.push_back(UnitSquare::left);
+  types.push_back(UnitSquare::crisscross);
+  for(dolfin::uint tp = 0; tp < types.size(); ++tp)
+  {
+    UnitSquare mesh(Nx, Ny, types[tp]);
+    fail_unless(test_mesh(mesh) == 0);
+  }
+}END_TEST
+//-----------------------------------------------------------------------------
+START_TEST( test_create_cube )
+{
+  dolfin::uint const Nx = 2;
+  dolfin::uint const Ny = 4;
+  dolfin::uint const Nz = 8;
+
+  // Cube
+  UnitCube mesh(Nx, Ny, Nz);
+  fail_unless(test_mesh(mesh) == 0);
+
+}END_TEST
+//-----------------------------------------------------------------------------
+START_TEST( test_create_box)
+{
+  // Box
+  dolfin::uint const Nx = 2;
+  dolfin::uint const Ny = 4;
+  dolfin::uint const Nz = 8;
+
+  real a = 0.0;
+  real b = 4.0;
+  real c = 0.0;
+  real d = 2.0;
+  real e = 0.0;
+  real f = 1.0;
+  Box mesh(a,b,c, d, e, f, Nx, Ny, Nz);
+  fail_unless(test_mesh(mesh) == 0);
+
+
+}END_TEST
+//-----------------------------------------------------------------------------
+START_TEST( test_create_disk )
+{
+  // Disk
+  dolfin::uint const Nx = 2;
+  Array<UnitDisk::Type> types;
+  types.push_back(UnitDisk::right);
+  types.push_back(UnitDisk::left);
+  types.push_back(UnitDisk::crisscross);
+  Array<UnitDisk::Transformation> trans;
+  trans.push_back(UnitDisk::maxn);
+  trans.push_back(UnitDisk::sumn);
+  trans.push_back(UnitDisk::rotsumn);
+  for(dolfin::uint tp = 0; tp < types.size(); ++tp)
+  {
+    for(dolfin::uint tr = 0; tr < trans.size(); ++tr)
+    {
+      UnitDisk mesh(Nx, types[tp], trans[tr]);
+      fail_unless(test_mesh(mesh) == 0);
+    }
+  }
+}END_TEST
+//-----------------------------------------------------------------------------
+
+#endif
---
+++ tests/mesh/VertexNormal/VertexNormal.h
@@ -0,0 +1,43 @@
+#ifdef HAVE_CHECK
+
+#include <check.h>
+
+
+#include <dolfin/config/dolfin_config.h>
+#include <dolfin/mesh/VertexNormal.h>
+
+using namespace dolfin;
+
+//-----------------------------------------------------------------------------
+void test_vertex_normal(std::string filename, VertexNormal::Type type)
+{
+  Mesh mesh(filename);
+  VertexNormal vn(mesh, type);
+
+//  uint const gdim = mesh.geometry().dim();
+//  for(uint i = 0 ; i < gdim ; ++i)
+//  {
+//    std::stringstream ss;
+//    ss << "E" << i;
+//    size_t pos = filename.find('.');
+//    File f(filename.substr(0,pos)+".pvd");
+//    f << vn.basis()[d][i];
+//  }
+}
+//-----------------------------------------------------------------------------
+START_TEST( test_init_weight_none )
+{
+  int init_failed = 0;
+  
+  // NOTE the test is initiated from @top_srcdir@/tests
+  std::string const relpath = "../data/meshes/";
+  //test_vertex_normal(relpath+"square16.bin", VertexNormal::none);
+  test_vertex_normal(relpath+"cylinder.xml.gz", VertexNormal::none);
+  test_vertex_normal(relpath+"aneurysm.xml.gz", VertexNormal::none);
+  test_vertex_normal(relpath+"sphere.xml.gz", VertexNormal::none);
+  
+  fail_unless( init_failed == 0 );
+}END_TEST
+//-----------------------------------------------------------------------------
+
+#endif

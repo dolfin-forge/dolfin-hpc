9fc663267be0e11f7c814930ff35406f3adf74a8
Aurélien Larcher <aurelien.larcher@gmail.com>
2015-10-07 05:51:13 +0200

    Format Cell

--- include/dolfin/mesh/Cell.h
+++ include/dolfin/mesh/Cell.h
@@ -17,61 +17,68 @@
 namespace dolfin
 {
 
-  /// A Cell is a MeshEntity of topological codimension 0.
+/**
+ *  @class  Cell
+ *
+ *  @brief  A Cell is a MeshEntity of topological codimension 0.
+ *
+ */
 
-  class Cell : public MeshEntity
-  {
-  public:
+class Cell : public MeshEntity
+{
+public:
 
-    /// Constructor
-    Cell(Mesh& mesh, uint index) : MeshEntity(mesh, mesh.topology().dim(), index) {}
+  /// Constructor
+  Cell(Mesh& mesh, uint index) : MeshEntity(mesh, mesh.topology().dim(), index) {}
 
-    /// Destructor
-    ~Cell() {}
+  /// Destructor
+  ~Cell() {}
 
-    /// Return type of cell
-    inline CellType::Type type() const { return mesh_.type().cellType(); }
+  /// Return type of cell
+  inline CellType::Type type() const { return mesh_.type().cellType(); }
 
-    /// Compute orientation of cell (0 is right, 1 is left)
-    inline real orientation() const { return mesh_.type().orientation(*this); }
+  /// Compute orientation of cell (0 is right, 1 is left)
+  inline real orientation() const { return mesh_.type().orientation(*this); }
 
-    /// Compute (generalized) volume of cell
-    inline real volume() const { return mesh_.type().volume(*this); }
+  /// Compute (generalized) volume of cell
+  inline real volume() const { return mesh_.type().volume(*this); }
 
-    /// Compute diameter of cell
-    inline real diameter() const { return mesh_.type().diameter(*this); }
+  /// Compute diameter of cell
+  inline real diameter() const { return mesh_.type().diameter(*this); }
 
-    /// Compute circumradius of cell
-    inline real circumradius() const { return mesh_.type().circumradius(*this); }
+  /// Compute circumradius of cell
+  inline real circumradius() const { return mesh_.type().circumradius(*this); }
 
-    /// Compute midpoint of cell
-    Point midpoint();
+  /// Compute normal of given facet with respect to the cell
+  inline Point normal(uint facet) const { return mesh_.type().normal(*this, facet); }
 
-    /// Compute component i of normal of given facet with respect to the cell
-    inline real normal(uint facet, uint i) const { return mesh_.type().normal(*this, facet, i); }
+  /// Compute the area/length of given facet with respect to the cell
+  inline real facetArea(uint facet) const { return mesh_.type().facetArea(*this, facet); }
 
-    /// Compute normal of given facet with respect to the cell
-    inline Point normal(uint facet) const { return mesh_.type().normal(*this, facet); }
+  /// Compute midpoint of cell
+  Point midpoint();
 
-    /// Compute the area/length of given facet with respect to the cell
-    inline real facetArea(uint facet) const { return mesh_.type().facetArea(*this, facet); }
+};
 
-  };
+/**
+ *  @class  CellIterator
+ *
+ *  @brief  A CellIterator is a MeshEntityIterator of topological codimension 0.
+ *
+ */
 
-  /// A CellIterator is a MeshEntityIterator of topological codimension 0.
+class CellIterator : public MeshEntityIterator
+{
+public:
 
-  class CellIterator : public MeshEntityIterator
-  {
-  public:
+  CellIterator(Mesh& mesh) : MeshEntityIterator(mesh, mesh.topology().dim()) {}
+  CellIterator(MeshEntity& entity) : MeshEntityIterator(entity, entity.mesh().topology().dim()) {}
 
-    CellIterator(Mesh& mesh) : MeshEntityIterator(mesh, mesh.topology().dim()) {}
-    CellIterator(MeshEntity& entity) : MeshEntityIterator(entity, entity.mesh().topology().dim()) {}
+  inline Cell& operator*() { return *operator->(); }
+  inline Cell* operator->() { return static_cast<Cell*>(MeshEntityIterator::operator->()); }
 
-    inline Cell& operator*() { return *operator->(); }
-    inline Cell* operator->() { return static_cast<Cell*>(MeshEntityIterator::operator->()); }
+};
 
-  };
+}/* namespace dolfin */
 
-}
-
-#endif
+#endif /* __DOLFIN_CELL_H */

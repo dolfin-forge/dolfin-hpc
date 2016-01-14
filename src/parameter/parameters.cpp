84bb29b98d560df68d81b8523e25836bb14f97c7
Aurélien Larcher <aurelien.larcher@gmail.com>
2016-01-14 14:55:49 +0100

    Remove garbage in parameters

--- src/parameter/parameters.cpp
+++ src/parameter/parameters.cpp
@@ -6,54 +6,28 @@
 // First added:  2005-12-19
 // Last changed: 2008-02-11
 
-#include <dolfin/log/LogManager.h>
-#include <dolfin/parameter/ParameterSystem.h>
 #include <dolfin/parameter/parameters.h>
 
-//-----------------------------------------------------------------------------
-dolfin::Parameter dolfin::dolfin_get(std::string key)
+#include <dolfin/parameter/ParameterSystem.h>
+
+namespace dolfin
 {
-  if (key == "debug level")
-    return Parameter(LogManager::logger().getDebugLevel());
+
+//-----------------------------------------------------------------------------
+Parameter dolfin_get(std::string key)
+{
   return ParameterSystem::parameters.get(key);
 }
 //-----------------------------------------------------------------------------
-void dolfin::dolfin_set(std::string key, Parameter value)
+void dolfin_set(std::string key, Parameter value)
 {
-  // Special cases: pass on to log system
-  if (key == "debug level")
-  {
-    LogManager::logger().setDebugLevel(value);
-    return;
-  }
-  else if (key == "output destination")
-  {
-    LogManager::logger().setOutputDestination(value);
-    return;
-  }
-
   ParameterSystem::parameters.set(key, value);
 }
 //-----------------------------------------------------------------------------
-void dolfin::dolfin_set(std::string key, std::ostream& stream)
-{
-  if (key == "output destination"){
-    LogManager::logger().setOutputDestination(stream);
-    {
-        if (key == "output destination"){
-              LogManager::logger().setOutputDestination(stream);
-                }
-          else
-                error("Only key 'output destination' can take a stream as value.");
-    }
-    
-  }
-  else
-    error("Only key 'output destination' can take a stream as value.");
-}
-//-----------------------------------------------------------------------------
-void dolfin::dolfin_add(std::string key, Parameter value)
+void dolfin_add(std::string key, Parameter value)
 {
   ParameterSystem::parameters.add(key, value);
 }
 //-----------------------------------------------------------------------------
+
+} /* namespace dolfin */

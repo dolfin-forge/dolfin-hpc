7ffb0ac0101d87292692fca7f57674881a8e7b7c
Aurélien Larcher <aurelien.larcher@gmail.com>
2016-03-31 20:29:00 +0200

    Import MeshConnectivity from frenchie

--- include/dolfin/mesh/MeshConnectivity.h
+++ include/dolfin/mesh/MeshConnectivity.h
@@ -8,22 +8,31 @@
 #define __DOLFIN_MESH_CONNECTIVITY_H
 
 #include <dolfin/common/types.h>
-#include <dolfin/common/Array.h>
+#include <dolfin/log/log.h>
 
 namespace dolfin
 {
 
-/// Mesh connectivity stores a sparse data structure of connections
-/// (incidence relations) between mesh entities for a fixed pair of
-/// topological dimensions.
-///
-/// The connectivity can be specified either by first giving the
-/// number of entities and the number of connections for each entity,
-/// which may either be equal for all entities or different, or by
-/// giving the entire (sparse) connectivity pattern.
+template<class T> class Array;
+
+/**
+ *  DOCUMENTATION:
+ *
+ *  @class  MeshConnectivity
+ *
+ *  @brief  Mesh connectivity stores a sparse data structure of connections
+ *          (incidence relations) between mesh entities for a fixed pair of
+ *          topological dimensions.
+ *          The connectivity can be specified either by first giving the
+ *          number of entities and the number of connections for each entity,
+ *          which may either be equal for all entities or different, or by
+ *          giving the entire (sparse) connectivity pattern.
+ *
+ */
 
 class MeshConnectivity
 {
+
 public:
 
   /// Create empty connectivity
@@ -44,11 +53,14 @@ public:
   /// Non-equality
   bool operator!=(MeshConnectivity const& other) const;
 
-  /// Return total number of connections
-  uint size() const;
+  /// Initialize number of entities and number of connections (equal for all)
+  void init(uint num_entities, uint num_connections);
 
-  /// Return number of connections for given entity
-  uint size(uint entity) const;
+  /// Initialize number of entities and number of connections (individually)
+  void init(Array<uint> const& num_connections);
+
+  /// Clear all data
+  void clear();
 
   /// Return array of connections for given entity
   uint* operator()(uint entity);
@@ -62,14 +74,19 @@ public:
   /// Return contiguous array of connections for all entities
   uint const * operator()() const;
 
-  /// Clear all data
-  void clear();
+  //---------------------------------------------------------------------------
 
-  /// Initialize number of entities and number of connections (equal for all)
-  void init(uint num_entities, uint num_connections);
+  ///
+  bool is_initialized() const;
 
-  /// Initialize number of entities and number of connections (individually)
-  void init(Array<uint> const& num_connections);
+  /// Return number of entities
+  uint num_entities() const;
+
+  /// Return total number of connections
+  uint size() const;
+
+  /// Return number of connections for given entity
+  uint size(uint entity) const;
 
   /// Set given connection for given entity
   void set(uint entity, uint connection, uint pos);
@@ -83,18 +100,32 @@ public:
   /// Set all connections for all entities
   void set(Array<Array<uint> > const& connectivity);
 
+  /// Remap entities connectivities from old to new ordering, left operator
+  void remap_left(Array<uint> const& map);
+
+  /// Remap entities connectivities from old to new ordering, right operator
+  void remap_right(Array<uint> const& map);
+
   /// Display data
   void disp() const;
+
+  //--- CHECK ROUTINES --------------------------------------------------------
+
+  /// Check
+  void check() const;
 
 private:
 
   friend class MPIMeshCommunicator;
 
-  /// Total number of connections
-  uint size_;
+  /// Return if
+  bool is_initialized_;
 
   /// Number of entities
   uint num_entities_;
+
+  /// Total number of connections
+  uint size_;
 
   /// Connections for all entities stored as a contiguous array
   uint * connections_;
@@ -106,43 +137,31 @@ private:
 
 //--- INLINES -----------------------------------------------------------------
 
-inline uint MeshConnectivity::size() const
-{
-  return size_;
-}
-
-//-----------------------------------------------------------------------------
 inline uint MeshConnectivity::size(uint entity) const
 {
-  return (entity < num_entities_ ? offsets_[entity + 1] - offsets_[entity] : 0);
+  dolfin_assert(num_entities_ > 0);
+  dolfin_assert(entity < num_entities_);
+  return (offsets_[entity + 1] - offsets_[entity]);
 }
 
 //-----------------------------------------------------------------------------
 inline uint * MeshConnectivity::operator()(uint entity)
 {
-  return (entity < num_entities_ ? connections_ + offsets_[entity] : 0);
+  dolfin_assert(num_entities_ > 0);
+  dolfin_assert(entity < num_entities_);
+  return (connections_ + offsets_[entity]);
 }
 
 //-----------------------------------------------------------------------------
-inline const uint * MeshConnectivity::operator()(uint entity) const
+inline uint const * MeshConnectivity::operator()(uint entity) const
 {
-  return (entity < num_entities_ ? connections_ + offsets_[entity] : 0);
-}
-
-//-----------------------------------------------------------------------------
-inline uint * MeshConnectivity::operator()()
-{
-  return connections_;
-}
-
-//-----------------------------------------------------------------------------
-inline const uint * MeshConnectivity::operator()() const
-{
-  return connections_;
+  dolfin_assert(num_entities_ > 0);
+  dolfin_assert(entity < num_entities_);
+  return (connections_ + offsets_[entity]);
 }
 
 //-----------------------------------------------------------------------------
 
-}
+} /* namespace dolfin */
 
-#endif
+#endif /* __DOLFIN_MESH_CONNECTIVITY_H */
--- src/mesh/MeshConnectivity.cpp
+++ src/mesh/MeshConnectivity.cpp
@@ -4,16 +4,21 @@
 // First added:  2006-05-09
 // Last changed: 2007-03-01
 
-#include <dolfin/log/dolfin_log.h>
 #include <dolfin/mesh/MeshConnectivity.h>
+
+#include <dolfin/common/Array.h>
+#include <dolfin/log/LogStream.h>
+
+#include <cstring>
 
 namespace dolfin
 {
 
 //-----------------------------------------------------------------------------
 MeshConnectivity::MeshConnectivity() :
-    size_(0),
+    is_initialized_(false),
     num_entities_(0),
+    size_(0),
     connections_(NULL),
     offsets_(NULL)
 {
@@ -21,8 +26,9 @@ MeshConnectivity::MeshConnectivity() :
 }
 //-----------------------------------------------------------------------------
 MeshConnectivity::MeshConnectivity(MeshConnectivity const& other) :
-    size_(0),
+    is_initialized_(false),
     num_entities_(0),
+    size_(0),
     connections_(NULL),
     offsets_(NULL)
 {
@@ -41,8 +47,9 @@ MeshConnectivity const& MeshConnectivity::operator=(
   clear();
 
   // Allocate data
-  size_ = other.size_;
+  is_initialized_ = other.is_initialized_;
   num_entities_ = other.num_entities_;
+  size_ = other.size_;
   if(size_ > 0)
   {
     connections_ = new uint[size_];
@@ -70,6 +77,11 @@ bool MeshConnectivity::operator==(MeshConnectivity const& other) const
   if (this == &other)
   {
     return true;
+  }
+  //
+  if(is_initialized_ != other.is_initialized_)
+  {
+    return false;
   }
   //
   if (size_ != other.size_)
@@ -106,33 +118,18 @@ bool MeshConnectivity::operator!=(MeshConnectivity const& other) const
   return !(*this == other);
 }
 //-----------------------------------------------------------------------------
-void MeshConnectivity::clear()
-{
-  size_ = 0;
-  num_entities_ = 0;
-  delete[] connections_;
-  connections_ = NULL;
-  delete[] offsets_;
-  offsets_ = NULL;
-}
-//-----------------------------------------------------------------------------
 void MeshConnectivity::init(uint num_entities, uint num_connections)
 {
-  // Clear old data if any
   clear();
-
-  // Compute the total size
+  //
+  is_initialized_= true;
+  num_entities_ = num_entities;
   size_ = num_entities * num_connections;
-  this->num_entities_ = num_entities;
-
-  // Initialize offsets
   offsets_ = new uint[num_entities + 1];
   for (uint e = 0; e <= num_entities; ++e)
   {
     offsets_[e] = e * num_connections;
   }
-
-  // Initialize connections
   if (size_ > 0)
   {
     connections_ = new uint[size_];
@@ -145,21 +142,18 @@ void MeshConnectivity::init(uint num_entities, uint num_connections)
 //-----------------------------------------------------------------------------
 void MeshConnectivity::init(Array<uint> const& num_connections)
 {
-  // Clear old data if any
   clear();
-
-  // Initialize offsets and compute total size
+  //
+  is_initialized_= true;
   num_entities_ = num_connections.size();
-  offsets_ = new uint[num_entities_ + 1];
   size_ = 0;
+  offsets_ = new uint[num_entities_ + 1];
   for (uint e = 0; e < num_entities_; ++e)
   {
     offsets_[e] = size_;
     size_ += num_connections[e];
   }
   offsets_[num_entities_] = size_;
-
-  // Initialize connections
   if (size_ > 0)
   {
     connections_ = new uint[size_];
@@ -170,6 +164,42 @@ void MeshConnectivity::init(Array<uint> const& num_connections)
   }
 }
 //-----------------------------------------------------------------------------
+void MeshConnectivity::clear()
+{
+  is_initialized_= false;
+  num_entities_ = 0;
+  size_ = 0;
+  delete[] connections_;
+  connections_ = NULL;
+  delete[] offsets_;
+  offsets_ = NULL;
+}
+//-----------------------------------------------------------------------------
+uint * MeshConnectivity::operator()()
+{
+  return connections_;
+}
+//-----------------------------------------------------------------------------
+uint const * MeshConnectivity::operator()() const
+{
+  return connections_;
+}
+//-----------------------------------------------------------------------------
+bool MeshConnectivity::is_initialized() const
+{
+  return is_initialized_;
+}
+//-----------------------------------------------------------------------------
+uint MeshConnectivity::num_entities() const
+{
+  return num_entities_;
+}
+//-----------------------------------------------------------------------------
+uint MeshConnectivity::size() const
+{
+  return size_;
+}
+//-----------------------------------------------------------------------------
 void MeshConnectivity::set(uint entity, uint connection, uint pos)
 {
   dolfin_assert(entity < num_entities_);
@@ -178,7 +208,7 @@ void MeshConnectivity::set(uint entity, uint connection, uint pos)
   connections_[offsets_[entity] + pos] = connection;
 }
 //-----------------------------------------------------------------------------
-void MeshConnectivity::set(uint entity, const Array<uint>& connections)
+void MeshConnectivity::set(uint entity, Array<uint> const& connections)
 {
   dolfin_assert(entity < num_entities_);
   dolfin_assert(connections.size() == offsets_[entity + 1] - offsets_[entity]);
@@ -203,15 +233,15 @@ void MeshConnectivity::set(uint entity, uint const * connections)
   }
 }
 //-----------------------------------------------------------------------------
-void MeshConnectivity::set(const Array<Array<uint> >& connections)
+void MeshConnectivity::set(Array<Array<uint> > const& connections)
 {
   // Clear old data if any
   clear();
 
   // Initialize offsets and compute total size
   num_entities_ = connections.size();
-  offsets_ = new uint[num_entities_ + 1];
   size_ = 0;
+  offsets_ = new uint[num_entities_ + 1];
   for (uint e = 0; e < num_entities_; ++e)
   {
     offsets_[e] = size_;
@@ -232,15 +262,51 @@ void MeshConnectivity::set(const Array<Array<uint> >& connections)
     }
   }
 }
+
+//-----------------------------------------------------------------------------
+void MeshConnectivity::remap_left(Array<uint> const& map)
+{
+  message(1, "MeshConnectivity : remap left");
+  if (num_entities_ > 0)
+  {
+    if(map.size() != num_entities_)
+    {
+      error("MeshConnectivity : remap_left mapping has invalid size");
+    }
+    // Set sizes in place of offsets to depict connectivities layout
+    uint * onew = new uint[num_entities_];
+    onew[0] = 0;
+    for(uint e = 0; e < num_entities_; ++e)
+    {
+      dolfin_assert(map[e] < num_entities_);
+      onew[e + 1] = offsets_[map[e] + 1] - offsets_[map[e]];
+    }
+    delete[] offsets_;
+    offsets_ = onew;
+    uint * cnew = new uint[size_];
+    for (uint e = 0; e < num_entities_; ++e)
+    {
+      std::memcpy(&cnew[onew[e]], &connections_[offsets_[map[e]]],
+                  offsets_[e + 1] * sizeof(uint));
+      offsets_[e + 1] += offsets_[e];
+    }
+    delete[] connections_;
+    connections_ = cnew;
+  }
+}
+//-----------------------------------------------------------------------------
+void MeshConnectivity::remap_right(Array<uint> const& map)
+{
+  message(1, "MeshConnectivity : remap right");
+  for (uint i = 0; i < size_; ++i)
+  {
+    connections_[i] = map[connections_[i]];
+  }
+}
 //-----------------------------------------------------------------------------
 void MeshConnectivity::disp() const
 {
-  // Begin indentation
-  cout << "MeshConnectivity" << endl;
-  begin("----------------");
-  cout << endl;
-
-  // Check if there are any connections
+  section("MeshConnectivity");
   if (size_ == 0)
   {
     cout << "empty" << endl;
@@ -258,10 +324,42 @@ void MeshConnectivity::disp() const
       cout << endl;
     }
   }
-  // End indentation
   end();
 }
 //-----------------------------------------------------------------------------
+void MeshConnectivity::check() const
+{
+  message("MeshConnectivity : check");
 
+  /**
+   *  CHECK:
+   *
+   *  Check connectivity size, number of entities and verify that connected
+   *  entities are not listed twice.
+   *
+   */
+
+  if(size_ < num_entities_)
+  {
+    message("MeshConnectivity : connectivities size %u < %u number of entities",
+            size_, num_entities_);
+  }
+  //
+  for(uint e0 = 0; e0 < this->num_entities(); ++e0)
+  {
+    _set<uint> ce;
+    for(uint e1 = 0; e1 <this->size(e0); ++e1)
+    {
+      uint ec = (*this)(e0)[e1];
+      if(ce.count(ec) > 0)
+      {
+        error("Entity %u appears twice in connectivities for %u", ec, e0);
+      }
+      ce.insert(ec);
+    }
+  }
 }
+//-----------------------------------------------------------------------------
+
+} /* namespace dolfin */
 

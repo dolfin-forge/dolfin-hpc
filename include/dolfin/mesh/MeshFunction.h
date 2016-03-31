fcfe4f664c0148e9eef4992a7c129c6560879595
Aurélien Larcher <aurelien.larcher@gmail.com>
2016-03-31 18:35:32 +0200

    Cleanup MeshFunction

--- include/dolfin/mesh/MeshFunction.h
+++ include/dolfin/mesh/MeshFunction.h
@@ -11,275 +11,286 @@
 
 #include <dolfin/common/types.h>
 #include <dolfin/io/File.h>
-#include "Mesh.h"
-#include "MeshEntity.h"
+#include <dolfin/mesh/Mesh.h>
+#include <dolfin/mesh/MeshEntity.h>
 
 namespace dolfin
 {
 
-/// A MeshFunction is a function that can be evaluated at a set of
-/// mesh entities. A MeshFunction is discrete and is only defined
-/// at the set of mesh entities of a fixed topological dimension.
-/// A MeshFunction may for example be used to store a global
-/// numbering scheme for the entities of a (parallel) mesh, marking
-/// sub domains or boolean markers for mesh refinement.
+/**
+ *  @class  MeshFunction
+ *
+ *  @brief  A MeshFunction is a function that can be evaluated at a set of
+ *          mesh entities.
+ *          A MeshFunction is discrete and is only defined at the set of mesh
+ *          entities of a fixed topological dimension.
+ *          A MeshFunction may for example be used to store a global numbering
+ *          scheme for the entities of a (parallel) mesh, marking sub domains or
+ *          boolean markers for mesh refinement.
+ *
+ *          MeshFunctions are default initialized to the zero value of the
+ *          underlying type:
+ *            - bool : false
+ *            - uint : 0
+ *            - real : 0.0
+ */
 
 template<class T>
-  class MeshFunction
+class MeshFunction
+{
+public:
+
+  /// Create empty mesh function
+  MeshFunction() :
+      values_(NULL),
+      mesh_(0),
+      dim_(0),
+      size_(0)
   {
-  public:
+  }
 
-    /// Create empty mesh function
-    MeshFunction() :
-        values_(0),
-        mesh_(0),
-        dim_(0),
-        size_(0)
+  /// Create empty mesh function on given mesh
+  MeshFunction(Mesh& mesh) :
+      values_(NULL),
+      mesh_(&mesh),
+      dim_(0),
+      size_(0)
+  {
+  }
+
+  /// Create mesh function on given mesh of given dimension
+  MeshFunction(Mesh& mesh, uint dim) :
+      values_(NULL),
+      mesh_(&mesh),
+      dim_(0),
+      size_(0)
+  {
+    init(dim);
+  }
+
+  /// Create function from data file
+  MeshFunction(Mesh& mesh, const std::string filename) :
+      values_(NULL),
+      mesh_(&mesh),
+      dim_(0),
+      size_(0)
+  {
+    File file(filename);
+    file >> *this;
+  }
+
+  /// Destructor
+  ~MeshFunction()
+  {
+    delete[] values_;
+  }
+
+  /// Return mesh associated with mesh function
+  inline Mesh& mesh()
+  {
+    dolfin_assert(mesh_);
+    return *mesh_;
+  }
+
+  /// Return topological dimension
+  inline uint dim() const
+  {
+    return dim_;
+  }
+
+  /// Return size (number of entities)
+  inline uint size() const
+  {
+    return size_;
+  }
+
+  /// Return array of values
+  inline const T* values() const
+  {
+    return values_;
+  }
+
+  /// Return array of values
+  inline T* values()
+  {
+    return values_;
+  }
+
+  /// Return value at given entity
+  inline T& operator()(MeshEntity& entity)
+  {
+    dolfin_assert(values_);
+    dolfin_assert(&entity.mesh() == mesh_);
+    dolfin_assert(entity.dim() == dim_);
+    dolfin_assert(entity.index() < size_);
+    return values_[entity.index()];
+  }
+
+  /// Return value at given entity
+  inline const T& operator()(MeshEntity& entity) const
+  {
+    dolfin_assert(values_);
+    dolfin_assert(&entity.mesh() == mesh_);
+    dolfin_assert(entity.dim() == dim_);
+    dolfin_assert(entity.index() < size_);
+    return values_[entity.index()];
+  }
+
+  /// Set all values to given value
+  const MeshFunction<T>& operator=(const T& value)
+  {
+    dolfin_assert(values_);
+    for (uint i = 0; i < size_; ++i)
     {
+      values_[i] = value;
     }
+    return *this;
+  }
 
-    /// Create empty mesh function on given mesh
-    MeshFunction(Mesh& mesh) :
-        values_(0),
-        mesh_(&mesh),
-        dim_(0),
-        size_(0)
+  /// Equality
+  bool operator==(const MeshFunction<T>& other)
+  {
+    if(this == &other)
     {
+      return true;
     }
-
-    /// Create mesh function on given mesh of given dimension
-    MeshFunction(Mesh& mesh, uint dim) :
-        values_(0),
-        mesh_(&mesh),
-        dim_(0),
-        size_(0)
+    if (size_ != other.size_)
     {
-      init(dim);
+      return false;
     }
-
-    /// Create function from data file
-    MeshFunction(Mesh& mesh, const std::string filename) :
-        values_(0),
-        mesh_(&mesh),
-        dim_(0),
-        size_(0)
+    if (size_ == 0)
     {
-      File file(filename);
-      file >> *this;
+      return true;
     }
-
-    /// Destructor
-    ~MeshFunction()
+    bool cmp = true;
+    for (uint i = 0; i < size_; i++)
     {
-      delete[] values_;
+      cmp &= (values_[i] == other.values_[i]);
     }
+    return cmp;
+  }
 
-    /// Return mesh associated with mesh function
-    inline Mesh& mesh()
+  /// Equality
+  bool operator!=(const MeshFunction<T>& other)
+  {
+    return !(*this == other);
+  }
+
+  /// Initialize mesh function for given topological dimension
+  void init(uint dim)
+  {
+    if (!mesh_)
     {
-      dolfin_assert(mesh_);
-      return *mesh_;
+      error("Mesh undefined, unable to initialize mesh function.");
     }
+    mesh_->init(dim);
+    init(*mesh_, dim, mesh_->size(dim));
+  }
 
-    /// Return topological dimension
-    inline uint dim() const
+  /// Initialize mesh function for given topological dimension of given size
+  void init(uint dim, uint size)
+  {
+    if (!mesh_)
     {
-      return dim_;
+      error("Mesh undefined, unable to initialize mesh function.");
     }
+    mesh_->init(dim);
+    init(*mesh_, dim, size);
+  }
 
-    /// Return size (number of entities)
-    inline uint size() const
+  /// Initialize mesh function for given topological dimension
+  void init(Mesh& mesh, uint dim)
+  {
+    mesh.init(dim);
+    init(mesh, dim, mesh.size(dim));
+  }
+
+  /// Initialize mesh function for given topological dimension of given size
+  void init(Mesh& mesh, uint dim, uint size)
+  {
+    // Initialize mesh for entities of given dimension
+    mesh.init(dim);
+    dolfin_assert(mesh.size(dim) == size);
+
+    // Initialize data
+    mesh_ = &mesh;
+    dim_ = dim;
+    size_ = size;
+    delete[] values_;
+    values_ = NULL;
+
+    if(size_ > 0)
     {
-      return size_;
-    }
-
-    /// Return array of values
-    inline const T* values() const
-    {
-      return values_;
-    }
-
-    /// Return array of values
-    inline T* values()
-    {
-      return values_;
-    }
-
-    /// Return value at given entity
-    inline T& operator()(MeshEntity& entity)
-    {
-      // FIXME: Removed temporarily, to get parallel assembly working
-      //dolfin_assert(&entity.mesh() == _mesh);
-      
-      dolfin_assert(values_);
-      dolfin_assert(entity.dim() == dim_);
-      dolfin_assert(entity.index() < size_);
-      return values_[entity.index()];
-    }
-
-    /// Return value at given entity
-    inline const T& operator()(MeshEntity& entity) const
-    {
-      dolfin_assert(values_);
-      dolfin_assert(&entity.mesh() == mesh_);
-      dolfin_assert(entity.dim() == dim_);
-      dolfin_assert(entity.index() < size_);
-      return values_[entity.index()];
-    }
-
-    /// Set all values to given value
-    const MeshFunction<T>& operator=(const T& value)
-    {
-      dolfin_assert(values_);
-      for (uint i = 0; i < size_; i++)
-        values_[i] = value;
-      return *this;
-    }
-
-    /// Equality
-    bool operator==(const MeshFunction<T>& other)
-    {
-      if(this == &other)
-      {
-        return true;
-      }
-      if (size_ != other.size_)
-      {
-        return false;
-      }
-      if (size_ == 0)
-      {
-        return true;
-      }
-      bool cmp = true;
-      for (uint i = 0; i < size_; i++)
-      {
-        cmp &= (values_[i] == other.values_[i]);
-      }
-      return cmp;
-    }
-
-    /// Equality
-    bool operator!=(const MeshFunction<T>& other)
-    {
-      return !(*this == other);
-    }
-
-    /// Initialize mesh function for given topological dimension
-    void init(uint dim)
-    {
-      if (!mesh_)
-      {
-        error("Mesh undefined, unable to initialize mesh function.");
-      }
-      mesh_->init(dim);
-      init(*mesh_, dim, mesh_->size(dim));
-    }
-
-    /// Initialize mesh function for given topological dimension of given size
-    void init(uint dim, uint size)
-    {
-      if (!mesh_)
-      {
-        error("Mesh undefined, unable to initialize mesh function.");
-      }
-      mesh_->init(dim);
-      init(*mesh_, dim, size);
-    }
-
-    /// Initialize mesh function for given topological dimension
-    void init(Mesh& mesh, uint dim)
-    {
-      mesh.init(dim);
-      init(mesh, dim, mesh.size(dim));
-    }
-
-    /// Initialize mesh function for given topological dimension of given size
-    void init(Mesh& mesh, uint dim, uint size)
-    {
-      // Initialize mesh for entities of given dimension
-      mesh.init(dim);
-      dolfin_assert(mesh.size(dim) == size);
-      
-      if (size == 0)
-      {
-        error("Trying to initialize a mesh function of zero size");
-      }
-
-      // Initialize data
-      mesh_ = &mesh;
-      dim_ = dim;
-      size_ = size;
-      delete[] values_;
       values_ = new T[size];
       std::fill(values_, values_ + size, static_cast<T>(0));
     }
+  }
 
-    /// Get value at given entity
-    inline T get(const MeshEntity& entity) const
+  /// Get value at given entity
+  inline T get(const MeshEntity& entity) const
+  {
+    dolfin_assert(values_);
+    dolfin_assert(&entity.mesh() == mesh_);
+    dolfin_assert(entity.dim() == dim_);
+    dolfin_assert(entity.index() < size_);
+    return values_[entity.index()];
+  }
+
+  /// Get value at given entity
+  inline T get(uint index) const
+  {
+    dolfin_assert(values_);
+    dolfin_assert(index < size_);
+    return values_[index];
+  }
+
+  /// Set value at given entity
+  inline void set(const MeshEntity& entity, const T& value)
+  {
+    dolfin_assert(values_);
+    dolfin_assert(&entity.mesh() == mesh_);
+    dolfin_assert(entity.dim() == dim_);
+    dolfin_assert(entity.index() < size_);
+    values_[entity.index()] = value;
+  }
+
+  /// Set value at given entity
+  inline void set(uint index, const T& value)
+  {
+    dolfin_assert(values_);
+    dolfin_assert(index < size_);
+    values_[index] = value;
+  }
+
+  /// Display mesh function data
+  void disp() const
+  {
+    section("Mesh function data");
+    cout << "Topological dimension: " << dim_ << endl;
+    cout << "Number of values:      " << size_ << endl;
+    cout << endl;
+    for (uint i = 0; i < size_; ++i)
     {
-      dolfin_assert(values_);
-      dolfin_assert(&entity.mesh() == mesh_);
-      dolfin_assert(entity.dim() == dim_);
-      dolfin_assert(entity.index() < size_);
-      return values_[entity.index()];
+      cout << "(" << dim_ << ", " << i << "): " << values_[i] << endl;
     }
+    end();
+  }
 
-    /// Get value at given entity
-    inline T get(uint index) const
-    {
-      dolfin_assert(values_);
-      dolfin_assert(index < size_);
-      return values_[index];
-    }
+private:
 
-    /// Set value at given entity
-    inline void set(const MeshEntity& entity, const T& value)
-    {
-      dolfin_assert(values_);
-      dolfin_assert(&entity.mesh() == mesh_);
-      dolfin_assert(entity.dim() == dim_);
-      dolfin_assert(entity.index() < size_);
-      values_[entity.index()] = value;
-    }
-    
-    /// Set value at given entity
-    inline void set(uint index, const T& value)
-    {
-      dolfin_assert(values_);
-      dolfin_assert(index < size_);
-      values_[index] = value;
-    }
-    
-    /// Display mesh function data
-    void disp() const
-    {
-      cout << "Mesh function data" << endl;
-      cout << "------------------" << endl;
-      begin("");
-      cout << "Topological dimension: " << dim_ << endl;
-      cout << "Number of values:      " << size_ << endl;
-      cout << endl;
-      for (uint i = 0; i < size_; i++)
-        cout << "(" << dim_ << ", " << i << "): " << values_[i] << endl;
-      end();
-    }
+  /// Values at the set of mesh entities
+  T * values_;
 
-  private:
+  /// The mesh
+  Mesh* mesh_;
 
-    /// Values at the set of mesh entities
-    T * values_;
+  /// Topological dimension
+  uint dim_;
 
-    /// The mesh
-    Mesh* mesh_;
+  /// Number of mesh entities
+  uint size_;
+};
 
-    /// Topological dimension
-    uint dim_;
+} /* namespace dolfin */
 
-    /// Number of mesh entities
-    uint size_;
-  };
-
-}
-
-#endif
+#endif /* __DOLFIN_MESH_FUNCTION_H */

2544e7f604cc805f30e1a2b70703fc6d39bcb8b1
Aurélien Larcher <aurelien.larcher@gmail.com>
2016-04-01 16:14:23 +0200

    Import Tokenized from frenchie

---
+++ include/dolfin/common/Tokenized.h
@@ -0,0 +1,46 @@
+// Copyright (C) 2015 Aurelien Larcher.
+// Licensed under the GNU LGPL Version 2.1.
+//
+// First added:
+// Last changed:
+
+#ifndef __DOLFIN_TOKENIZED_H
+#define __DOLFIN_TOKENIZED_H
+
+#include <string>
+
+namespace dolfin
+{
+
+/**
+ *  @class    Tokenized
+ *
+ *  @brief    Implements an interface for tracking the internal state of an
+ *            instance with a token.
+ */
+
+
+class Tokenized
+{
+
+public:
+
+  ///
+  virtual int token() const = 0;
+
+protected:
+
+  ///
+  Tokenized() {}
+
+  ///
+  virtual ~Tokenized() {}
+
+  ///
+  virtual void update_token() = 0;
+
+};
+
+} /* namespace dolfin */
+
+#endif /* __DOLFIN_TOKENIZED_H */

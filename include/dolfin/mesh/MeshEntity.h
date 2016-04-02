a7b1470447ca8742742192abceac9c2a47cd6beb
Aurelien Larcher <aurelien.larcher@gmail.com>
2016-04-02 23:58:15 +0200

    MeshEntity can rely on connectivity auto-creation

--- include/dolfin/mesh/MeshEntity.h
+++ include/dolfin/mesh/MeshEntity.h
@@ -135,21 +135,24 @@ inline uint MeshEntity::index() const
 //-----------------------------------------------------------------------------
 inline uint MeshEntity::num_entities(uint dim) const
 {
-  dolfin_assert(mesh_.topology()(tdim_, dim).is_initialized());
+  //dolfin_assert(mesh_.topology()(tdim_, dim).is_initialized());
+  //NOTE: New MeshTopology class auto-creates connectivity on demand.
   return mesh_.topology()(tdim_, dim).size(index_);
 }
 
 //-----------------------------------------------------------------------------
 inline uint * MeshEntity::entities(uint dim)
 {
-  dolfin_assert(mesh_.topology()(tdim_, dim).is_initialized());
+  //dolfin_assert(mesh_.topology()(tdim_, dim).is_initialized());
+  //NOTE: New MeshTopology class auto-creates connectivity on demand.
   return mesh_.topology()(tdim_, dim)(index_);
 }
 
 //-----------------------------------------------------------------------------
 inline uint const * MeshEntity::entities(uint dim) const
 {
-  dolfin_assert(mesh_.topology()(tdim_, dim).is_initialized());
+  //dolfin_assert(mesh_.topology()(tdim_, dim).is_initialized());
+  //NOTE: New MeshTopology class auto-creates connectivity on demand.
   return mesh_.topology()(tdim_, dim)(index_);
 }
 

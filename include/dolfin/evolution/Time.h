19dbd36c190497387097ec707d6aefe802fc6af6
Aurelien Larcher <aurelien.larcher@gmail.com>
2016-05-20 19:25:03 +0200

    Add Time from licorne

---
+++ include/dolfin/evolution/Time.h
@@ -0,0 +1,110 @@
+// Copyright (C) 2014 Aurelien Larcher.
+// Licensed under the GNU GPL Version 2.
+//
+// Imported from licorne
+
+#ifndef DOLFIN_EVOLUTION_TIME_H
+#define DOLFIN_EVOLUTION_TIME_H
+
+#include <dolfin/common/types.h>
+
+#include <map>
+
+namespace dolfin
+{
+
+class Time
+{
+
+public:
+
+  Time(real T0, real T1);
+
+  ~Time();
+
+  /// Return time interval as a pair of real numbers
+  std::pair<real, real> const& interval() const;
+
+  /// Return time interval as a pair of real numbers
+  int sign() const;
+
+  /// Is the current time in the time interval
+  bool is_valid() const;
+
+  /// T0
+  real begin() const;
+
+  /// T1
+  real end() const;
+
+  /// Measure of time interval
+  real measure() const;
+
+  /// Clock
+  real const& clock() const;
+
+  /// Elapsed time
+  real elapsed() const;
+
+  /// Remaining time
+  real remaining() const;
+
+  /// Display current time
+  void show() const;
+
+  /// Display basic information
+  void disp() const;
+
+  //--- ITERATOR --------------------------------------------------------------
+
+  class iterator
+  {
+
+  public:
+
+    /// Constructor
+    iterator()
+    {
+    }
+
+    /// Destructor
+    virtual ~iterator()
+    {
+    }
+
+    //--- INTERFACE -----------------------------------------------------------
+
+    /// Pre-increment
+    virtual Time& operator++() = 0;
+
+    /// Pre-decrement
+    virtual Time& operator--() = 0;
+
+    /// Is the iterator valid ?
+    virtual bool is_valid() const = 0;
+
+    /// Display information
+    virtual void disp() const = 0;
+
+    /// Iteration count
+    virtual uint count() const = 0;
+
+    //-------------------------------------------------------------------------
+
+  };
+
+  //---------------------------------------------------------------------------
+
+private:
+
+  std::pair<real, real> const T_;
+  int const sign_;
+
+  //
+  real t_;
+
+};
+
+} /* namespace dolfin */
+
+#endif /* DOLFIN_EVOLUTION_TIME_H */
---
+++ src/evolution/Time.cpp
@@ -0,0 +1,94 @@
+// Copyright (C) 2014 Aurelien Larcher.
+// Licensed under the GNU GPL Version 2.
+//
+// Imported from licorne
+
+#include <dolfin/evolution/Time.h>
+
+#include <dolfin/log/log.h>
+
+namespace dolfin
+{
+
+//-----------------------------------------------------------------------------
+Time::Time(real T0, real T1) :
+    T_(T0, T1),
+    sign_((T0 < T1) - (T1 < T0)),
+    t_(T0)
+{
+}
+//-----------------------------------------------------------------------------
+Time::~Time()
+{
+}
+//-----------------------------------------------------------------------------
+std::pair<real, real> const& Time::interval() const
+{
+  return T_;
+}
+//-----------------------------------------------------------------------------
+int Time::sign() const
+{
+  return sign_;
+}
+//-----------------------------------------------------------------------------
+bool Time::is_valid() const
+{
+  return std::fabs(t_ - T_.first) <= std::fabs(T_.second - T_.first)
+      && std::fabs(t_ - T_.first) <= std::fabs(t_ - T_.first);
+}
+//-----------------------------------------------------------------------------
+real Time::begin() const
+{
+  return T_.first;
+}
+//-----------------------------------------------------------------------------
+real Time::end() const
+{
+  return T_.second;
+}
+//-----------------------------------------------------------------------------
+real Time::measure() const
+{
+  return std::fabs(T_.second - T_.first);
+}
+//-----------------------------------------------------------------------------
+real const& Time::clock() const
+{
+  return t_;
+}
+//-----------------------------------------------------------------------------
+real Time::elapsed() const
+{
+  return std::fabs(t_ - T_.first);
+}
+//-----------------------------------------------------------------------------
+real Time::remaining() const
+{
+  return std::fabs(T_.second - t_);
+}//-----------------------------------------------------------------------------
+void Time::show() const
+{
+  real p = 100.0 * elapsed() / (sign_ == 0 ? 1.0 : measure());
+  message("----------------------------------------------------------------");
+  message("t = %-49.10f [ %6.2f ]", t_, p);
+  message("----------------------------------------------------------------");
+}
+//-----------------------------------------------------------------------------
+void Time::disp() const
+{
+  section("Time");
+  message("T0        : %f", T_.first);
+  message("T1        : %f", T_.second);
+  message("t         : %f", t_);
+  message("valid     : %d", this->is_valid());
+  message("sign      : %d", this->sign());
+  message("measure   : %f", this->measure());
+  message("elapsed   : %f", this->elapsed());
+  message("remaining : %f", this->remaining());
+  end();
+  skip();
+}
+//-----------------------------------------------------------------------------
+
+} /* namespace dolfin */

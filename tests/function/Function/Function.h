f44f8f28e7480e572ad1fe9a16a665f20fd8d08b
Aurelien Larcher <aurelien.larcher@gmail.com>
2016-05-20 23:06:43 +0200

    Function test

--- tests/function/Function/Function.h
+++ tests/function/Function/Function.h
@@ -3,8 +3,10 @@
 #ifdef HAVE_CHECK
 
 #include <dolfin/common/Test.h>
+#include <dolfin/fem/FiniteElementSpace.h>
 #include <dolfin/function/Function.h>
 #include <dolfin/mesh/UnitInterval.h>
+#include <dolfin/ufl/UFLFiniteElement.h>
 
 #include <check.h>
 
@@ -18,9 +20,36 @@ START_TEST( test_Function )
   //---
   T.begin("test_Function");
   {
-    UnitInterval mesh(42);
+    UnitInterval m0(16);
+    ufl::FiniteElement DG0(ufl::Family::DG, m0.type(), 0);
+    FiniteElementSpace Vh0(m0, DG0);
+    Function U0(Vh0);
+
+    //
+    U0 = 1.0;
+    fail_unless( U0.min() == 1.0 );
+    fail_unless( U0.max() == 1.0 );
+    U0.disp();
 
 
+    UnitInterval m1(32);
+    FiniteElementSpace Vh1(m1, DG0);
+    Function U1(Vh1);
+
+    //
+    real * U1blck = U1.create_block();
+    for (CellIterator cell(m1); !cell.end(); ++cell)
+    {
+      U1blck[cell->index()] = cell->index();
+    }
+    U1.set_block(U1blck);
+    delete [] U1blck;
+
+    //
+    Function U2(m1);
+    U2 = U1;
+
+    U2.disp();
 
 
 

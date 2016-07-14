76944fe2d1150e67336a0472a96df554fb72013d
Aurélien Larcher <aurelien.larcher@gmail.com>
2016-07-14 16:19:52 +0200

    Remove legacy pde/nls classes to speed-up compilation

--- include/dolfin/nls/dolfin_nls.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DOLFIN_HEADER_NLS_H
-#define __DOLFIN_HEADER_NLS_H
-
-// DOLFIN nonlinear solver
-
-#include <dolfin/nls/NewtonSolver.h>
-#include <dolfin/nls/NonlinearProblem.h>
-
-#endif /* __DOLFIN_HEADER_NLS_H */
--- include/dolfin/pde/dolfin_pde.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DOLFIN_HEADER_PDE_H
-#define __DOLFIN_HEADER_PDE_H
-
-// DOLFIN pde interface
-
-#include <dolfin/pde/LinearPDE.h>
-#include <dolfin/pde/NonlinearPDE.h>
-
-#endif /* __DOLFIN_HEADER_PDE_H */

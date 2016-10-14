5c97bad23e0ac355832f7679078e24b74e72db96
Aurélien Larcher <aurelien.larcher@gmail.com>
2016-10-14 21:38:27 +0200

    Subsys

--- include/dolfin/main/SubSystemsManager.h
+++ include/dolfin/main/SubSystemsManager.h
@@ -40,6 +40,10 @@ namespace dolfin
     /// Finalize PETSc
     static void finalizePETSc();
 
+    // Check if MPI has been initialised (returns true if MPI has been
+    //   initialised, even if it is later finalised)
+    static bool MPIinitialized();
+
   private:
 
     // Constructor
@@ -50,10 +54,6 @@ namespace dolfin
 
     // Destructor
     ~SubSystemsManager();
-
-    // Check if MPI has been initialised (returns true if MPI has been
-    //   initialised, even if it is later finalised)
-    static bool MPIinitialized();
 
     // Singleton instance
     static SubSystemsManager sub_systems_manager;

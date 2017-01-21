c9a1f316cf4b10a3007e08ceb9781c8b26df526f
Aurélien Larcher <aurelien.larcher@gmail.com>
2017-01-21 21:15:28 +0100

    Import UFCExpression from old licorne

---
+++ include/dolfin/fem/UFCExpression.h
@@ -0,0 +1,42 @@
+// Copyright (C) 2014 Aurelien Larcher.
+// Licensed under the GNU LGPL Version 2.1.
+//
+// Import from licorne
+//
+// First added:
+// Last changed:
+
+#ifndef __DOLFIN_UFC_EXPRESSION_H
+#define __DOLFIN_UFC_EXPRESSION_H
+
+#include <dolfin/function/Expression.h>
+
+#include <ufc.h>
+
+namespace dolfin
+{
+
+//-----------------------------------------------------------------------------
+struct UFCExpression : ufc::function
+{
+  UFCExpression(Expression const& E) :
+    E_(E)
+  {
+  }
+
+  ///
+  inline void evaluate(real* values, const real* coordinates,
+                       const ufc::cell& cell) const
+  {
+    E_.eval(values, coordinates);
+  }
+
+private:
+
+  Expression const& E_;
+};
+//-----------------------------------------------------------------------------
+
+} /* namespace dolfin */
+
+#endif /* __DOLFIN_UFC_EXPRESSION_H */

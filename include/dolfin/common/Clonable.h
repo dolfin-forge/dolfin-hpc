7c9ea427c607d7c65a0b1aa70198f84cdab08d39
Aurélien Larcher <aurelien.larcher@gmail.com>
2017-06-07 17:50:37 +0200

    Add static clone function

--- include/dolfin/common/Clonable.h
+++ include/dolfin/common/Clonable.h
@@ -7,6 +7,8 @@
 #ifndef __DOLFIN_CLONABLE_H
 #define __DOLFIN_CLONABLE_H
 
+#include <dolfin/common/types.h>
+
 namespace dolfin
 {
 
@@ -14,7 +16,15 @@ template <class T>
 struct Clonable
 {
 
+  /// Clone instance
   inline T * clone() const { return new T(static_cast<T const&>(*this)); };
+
+  /// Clone second argument into first argument if non-NULL
+  static void clone(Clonable<T> *& p0, Clonable<T> const * p1)
+  {
+    dolfin_assert(p0 == NULL);
+    if (p1 != NULL) { p0 = p1->clone(); }
+  }
 
 };
 

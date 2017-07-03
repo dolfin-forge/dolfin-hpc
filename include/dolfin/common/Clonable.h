2a2e4b15aec678c9b1e4bc610291463f89ba8560
Aurélien Larcher <aurelien.larcher@gmail.com>
2017-07-03 18:39:57 +0200

    Add cloneptr with pointer to const

--- include/dolfin/common/Clonable.h
+++ include/dolfin/common/Clonable.h
@@ -8,6 +8,7 @@
 #define __DOLFIN_CLONABLE_H
 
 #include <dolfin/common/types.h>
+#include <dolfin/log/log.h>
 
 namespace dolfin
 {
@@ -31,6 +32,14 @@ struct Clonable
 /// Clone second argument into first argument if non-NULL
 template <class CloneT>
 static void cloneptr(CloneT *& p0, CloneT const * p1)
+{
+  dolfin_assert(p0 == NULL);
+  if (p1 != NULL) { p0 = p1->clone(); }
+}
+
+/// Clone second argument into first argument if non-NULL
+template <class CloneT>
+static void cloneptr(CloneT const *& p0, CloneT const * p1)
 {
   dolfin_assert(p0 == NULL);
   if (p1 != NULL) { p0 = p1->clone(); }

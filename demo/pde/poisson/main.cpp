f0ca0c8e61db44d1a57e46b03300c4cac366d61e
Niclas Jansson <leifniclas.jansson@riken.jp>
2017-09-05 05:39:10 +0900

    Convert poisson demo to the new interface

--- demo/pde/poisson/main.cpp
+++ demo/pde/poisson/main.cpp
@@ -20,104 +20,81 @@
 
 #include "Poisson.h"
 
-#include <dolfin/common/constants.h>
-#include <dolfin/fem/Assembler.h>
-#include <dolfin/fem/DirichletBC.h>
-
-#include <dolfin/function/Function.h>
-#include <dolfin/mesh/UnitSquare.h>
-#include <dolfin/pde/LinearPDE.h>
-#include <dolfin/parameter/parameters.h>
+#include <dolfin.h>
 
 using namespace dolfin;
 
+//-----------------------------------------------------------------------------
+
+// Source term
+struct Source : public Value<Source>
+{
+  void eval(real * value, const real* x) const
+  {
+    real dx = x[0] - 0.5;
+    real dy = x[1] - 0.5;
+    value[0] = 500.0*std::exp(-(dx*dx + dy*dy)/0.02);
+  }
+};
+
+// Neumann boundary condition
+struct Flux : public Value<Flux>
+{
+  void eval(real * value, const real* x) const
+  {
+    if (x[0] > DOLFIN_EPS)
+      value[0] = 25.0*std::sin(5.0*DOLFIN_PI*x[1]);
+    else
+      value[0] = 0.0;
+  }
+};
+
+//-----------------------------------------------------------------------------
+
+// Sub domain for Dirichlet boundary condition
+struct DirichletBoundary : public SubDomain
+{
+  bool inside(const real* x, bool on_boundary) const
+  {
+    return x[0] < DOLFIN_EPS && on_boundary;
+  }
+};
+
+//-----------------------------------------------------------------------------
+
 int main()
 {
-  // Source term
-  class Source : public Function
-  {
-  public:
-    
-    Source(Mesh& mesh) : Function(mesh) {}
-    
-    void eval(real * value, const real* x) const
-    {
-      real dx = x[0] - 0.5;
-      real dy = x[1] - 0.5;
-      value[0] = 500.0*exp(-(dx*dx + dy*dy)/0.02);
-    }
-
-    uint rank() const
-    {
-      return 0;
-    }
-
-    uint dim(uint i) const
-    {
-      return 1;
-  }
-
-  };
-
-  // Neumann boundary condition
-  class Flux : public Function
-  {
-  public:
-
-    Flux(Mesh& mesh) : Function(mesh) {}
-
-    void eval(real * value, const real* x) const
-    {
-      if (x[0] > DOLFIN_EPS)
-        value[0] = 25.0*sin(5.0*DOLFIN_PI*x[1]);
-      else
-        value[0] = 0.0;
-    }
-
-    uint rank() const
-    {
-      return 0;
-    }
-
-    uint dim(uint i) const
-    {
-      return 1;
-    }
-
-  };
-
-  // Sub domain for Dirichlet boundary condition
-  class DirichletBoundary : public SubDomain
-  {
-    bool inside(const real* x, bool on_boundary) const
-    {
-      return x[0] < DOLFIN_EPS && on_boundary;
-    }
-  };
-
   // Create mesh
-  //UnitSquare mesh(32, 32);
   Mesh mesh("UnitSquareMesh_32x32.xml");
 
-  // Create functions
-  Source f(mesh);
-  Flux g(mesh);
+  // Create coefficients
+  Analytic<Source>  f(mesh);
+  Analytic<Flux>    g(mesh);
 
   // Create boundary condition
-  Function u0(mesh, 0.0);
+  Constant u0(0.0);
   DirichletBoundary boundary;
   DirichletBC bc(u0, mesh, boundary);
-  
+
   // Define PDE
-  PoissonBilinearForm a(mesh);
-  PoissonLinearForm L(f, g);
-  LinearPDE pde(a, L, mesh, bc);
+  Poisson::BilinearForm a(mesh);
+  Poisson::LinearForm   L(mesh, f, g);
 
   // Solve PDE
-  Function u(mesh);
+  Matrix A;
+  Vector b;
 
-  pde.solve(u);
-   
+  a.assemble(A, true);
+  L.assemble(b, true);
+  bc.apply(A, b, a);
+
+
+  Function u(a.trial_space());
+  KrylovSolver solver(bicgstab, bjacobi);
+
+  solver.solve(A, u.vector(), b);
+  u.sync();
+  
   // Save solution to file
   File file("poisson.pvd");
   file << u;

8937698909bae5119d3013ef85bcfa26a36f2874
Aurelien Larcher <aurelien.larcher@gmail.com>
2017-09-05 15:06:10 +0200

    Add Value in dolfin_function.h

--- include/dolfin/function/dolfin_function.h
+++ include/dolfin/function/dolfin_function.h
@@ -6,14 +6,15 @@
 #include <dolfin/function/Analytic.h>
 #include <dolfin/function/Constant.h>
 #include <dolfin/function/Expression.h>
+#include <dolfin/function/Field.h>
 #include <dolfin/function/Function.h>
 #include <dolfin/function/FunctionDecomposition.h>
 #include <dolfin/function/FunctionInterpolation.h>
 #include <dolfin/function/Real.h>
 #include <dolfin/function/UFCFunction.h>
-#include <dolfin/function/Field.h>
+#include <dolfin/function/Value.h>
 
-//
+// Functors implementation
 
 #include <dolfin/function/impl/CellDiameter.h>
 

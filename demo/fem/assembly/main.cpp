1c0e41437ba7eda850a54dbc8de759d98741c34c
Aurelien Larcher <aurelien.larcher@gmail.com>
2017-10-04 14:52:21 +0200

    demo: update fem/assembly

--- demo/fem/assembly/main.cpp
+++ demo/fem/assembly/main.cpp
@@ -1,48 +1,30 @@
-// Copyright (C) 2008 Anders Logg and Magnus VikstrÃ¸m
+// Copyright (C) 2008 Anders Logg and Magnus Vikstrøm
 // Licensed under the GNU LGPL Version 2.1.
 //
 // First added:  2008-02-04
-// Last changed: 2008-04-12
+// Last changed: 2017-10-04
 //
-// This demonstrates parallel assembly in DOLFIN. Parallel
-// assembly is currently experimental and exists in the form
-// of the alternate assembler pAssembler. This will be merged
-// with the main DOLFIN assembler.
-//
-// To run this demo in parallel, type
-//
-//     mpirun -n <num_processes> demo
-//
-// Note that currently one needs to edit the code generated
-// by FFC and replace dolfin::Form with dolfin::pForm. This
-// extra step will be removed once Assembler and pAssembler
-// have merged.
 
-#include <dolfin.h>
+#include <dolfin/common/Test.h>
+#include <dolfin/la/Matrix.h>
+#include <dolfin/mesh/UnitCube.h>
+
 #include "ReactionDiffusion.h"
 
 using namespace dolfin;
 
 int main()
 {
-//#ifndef HAS_SCOTCH
-//  message("Sorry, this demo requires SCOTCH.");
-//  return 0;
-//#endif
+  Test T;
 
   // Create mesh and form
   UnitCube mesh(20, 20, 20);
-  ReactionDiffusionBilinearForm a(mesh); 
-
-  // This is from an old demo 
-  // Partition mesh
-//  MeshFunction<dolfin::uint> partitions(mesh);
-//  mesh.partition(partitions);
+  ReactionDiffusion::BilinearForm a(mesh); 
 
   // Assemble matrix
   Matrix A;
-  Assembler assembler;
-  assembler.assemble(A, a, true);
+  a.assemble(A, true);
+  message("|A| = %e", A.norm());
  
   return 0;
 }

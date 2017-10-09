64fa6671dc55b38cd2fd28eac846fcfbd2625b31
Aurélien Larcher <aurelien.larcher@gmail.com>
2017-10-09 17:36:30 +0200

    Add incident and index to MeshConnectivity

--- include/dolfin/mesh/MeshConnectivity.h
+++ include/dolfin/mesh/MeshConnectivity.h
@@ -88,6 +88,12 @@ public:
   /// Return contiguous array of connections for all entities
   uint const * operator()() const;
 
+  /// Return incidence of the edge
+  bool incident(uint entity, uint edge) const;
+
+  /// Return index of the edge, -1 if not incident
+  int index(uint entity, uint edge) const;
+
   //---------------------------------------------------------------------------
 
   ///
@@ -182,6 +188,29 @@ inline uint const * MeshConnectivity::operator()(uint entity) const
   dolfin_assert(order_ > 0);
   dolfin_assert(entity < order_);
   return (connections_ + offsets_[entity]);
+}
+
+//-----------------------------------------------------------------------------
+inline bool MeshConnectivity::incident(uint entity, uint edge) const
+{
+  dolfin_assert(order_ > 0);
+  dolfin_assert(entity < order_);
+  uint const * e = connections_ + offsets_[entity];
+  uint const * const n = connections_ + offsets_[entity + 1];
+  while (e != n && *e != edge) { ++e; }
+  return (e != n);
+}
+
+//-----------------------------------------------------------------------------
+inline int MeshConnectivity::index(uint entity, uint edge) const
+{
+  dolfin_assert(order_ > 0);
+  dolfin_assert(entity < order_);
+  uint const * const b = connections_ + offsets_[entity];
+  uint const * const n = connections_ + offsets_[entity + 1];
+  uint const * e = b;
+  while (e != n && *e != edge) { ++e; }
+  return (e == n ? -1 : (e - b));
 }
 
 //-----------------------------------------------------------------------------

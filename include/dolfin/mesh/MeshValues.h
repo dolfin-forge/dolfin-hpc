5d2f139ac667df1e52886a00973118889ce2e35b
Aurelien Larcher <aurelien.larcher@gmail.com>
2017-10-11 03:57:41 +0200

    Add MeshValues class

---
+++ include/dolfin/mesh/MeshValues.h
@@ -0,0 +1,185 @@
+// Copyright (C) 2016 Aurelien Larcher.
+// Licensed under the GNU LGPL Version 2.1.
+//
+// Imported from licorne.
+//
+// First added:  2017-10-10
+// Last changed: 2017-10-10
+
+#ifndef __DOLFIN_MESH_VALUES_H
+#define __DOLFIN_MESH_VALUES_H
+
+#include <dolfin/mesh/MeshFunction.h>
+
+#include <dolfin/mesh/Vertex.h>
+#include <dolfin/mesh/Edge.h>
+#include <dolfin/mesh/Face.h>
+#include <dolfin/mesh/Facet.h>
+#include <dolfin/mesh/Cell.h>
+
+namespace dolfin
+{
+
+/**
+ *  @class  MeshValues
+ *
+ *  @brief  A MeshFunction defined given an entity (what it should have been).
+ *
+ */
+
+template<class T, class E, uint N = 1>
+struct MeshValues : public MeshFunction<T>
+{
+
+  /// Return value size
+  inline uint value_size() { return N; }
+
+  /// Assignment operator
+  inline  MeshValues<T, E>& operator=(MeshValues<T, E> const& other)
+  {
+    if(this != &other)
+    {
+      if(this->mesh_ != other.mesh_)
+      {
+        init(other.mesh_, other.dim_, other.size_);
+      }
+      std::copy(other.values_, other.values_ + other.size_, this->values_);
+    }
+    return *this;
+  }
+
+  /// Assignment operator
+  template <class V>
+  MeshValues<T, E>& operator=(MeshValues<V, E, N> const& other)
+  {
+    if (this != &other)
+    {
+      if(this->mesh_ != other.mesh_)
+      {
+        init(other.mesh_, other.dim_, other.size_);
+      }
+      std::transform(other.values_, other.values_ + other.size_, this->values_,
+                     MeshFunction<T>::cast );
+    }
+    return *this;
+  }
+
+  /// Set all values to given value
+  inline MeshValues<T, E, N>& operator=(T const& value)
+  {
+    MeshFunction<T>::operator =(value);
+    return *this;
+  }
+
+  /// Return value at given entity
+  inline T& operator()(E& entity, uint i = 0)
+  {
+    dolfin_assert(this->values_);
+    dolfin_assert(&entity.mesh() == this->mesh_);
+    return this->values_[entity.index() * N + i];
+  }
+
+  /// Return value at given entity
+  inline T const& operator()(E& entity, uint i = 0) const
+  {
+    dolfin_assert(this->values_);
+    dolfin_assert(&entity.mesh() == this->mesh_);
+    return this->values_[entity.index() * N + i];
+  }
+
+  /// Return point to value at given entity
+  inline T * operator[](E& entity)
+  {
+    dolfin_assert(this->values_);
+    dolfin_assert(&entity.mesh() == this->mesh_);
+    return this->values_ + entity.index() * N;
+  }
+
+  /// Return pointer to value at given entity
+  inline T const * operator[](E& entity) const
+  {
+    dolfin_assert(this->values_);
+    dolfin_assert(&entity.mesh() == this->mesh_);
+    return this->values_ + entity.index() * N;
+  }
+
+};
+
+//--- TEMPLATE SPECIALIZATIONS ------------------------------------------------
+
+template<class T>
+struct MeshValues<T, Vertex, 1> : public MeshFunction<T>
+{
+  MeshValues(Mesh& mesh) : MeshFunction<T>(mesh, 0) {}
+
+  /// Set all values to given value
+  inline MeshValues<T, Vertex, 1>& operator=(T const& value)
+  {
+    MeshFunction<T>::operator =(value);
+    return *this;
+  }
+
+};
+
+template<class T>
+struct MeshValues<T, Edge, 1> : public MeshFunction<T>
+{
+  MeshValues(Mesh& mesh) : MeshFunction<T>(mesh, 1) {}
+
+  /// Set all values to given value
+  inline MeshValues<T, Edge, 1>& operator=(T const& value)
+  {
+    MeshFunction<T>::operator =(value);
+    return *this;
+  }
+
+};
+
+template<class T>
+struct MeshValues<T, Face, 1> : public MeshFunction<T>
+{
+  MeshValues(Mesh& mesh) : MeshFunction<T>(mesh, 2) {}
+
+  /// Set all values to given value
+  inline MeshValues<T, Face, 1>& operator=(T const& value)
+  {
+    MeshFunction<T>::operator =(value);
+    return *this;
+  }
+
+};
+
+template<class T>
+struct MeshValues<T, Facet, 1> : public MeshFunction<T>
+{
+  MeshValues(Mesh& mesh) : MeshFunction<T>(mesh, mesh.type().facet_dim()) {}
+
+  /// Set all values to given value
+  inline MeshValues<T, Facet, 1>& operator=(T const& value)
+  {
+    MeshFunction<T>::operator =(value);
+    return *this;
+  }
+
+};
+
+template<class T>
+struct MeshValues<T, Cell, 1> : public MeshFunction<T>
+{
+  MeshValues(Mesh& mesh) : MeshFunction<T>(mesh, mesh.type().dim()) {}
+
+  /// Set all values to given value
+  inline MeshValues<T, Cell, 1>& operator=(T const& value)
+  {
+    MeshFunction<T>::operator =(value);
+    return *this;
+  }
+
+};
+
+//-----------------------------------------------------------------------------
+
+} /* namespace dolfin */
+
+#endif /* __DOLFIN_MESH_VALUES_H */
+

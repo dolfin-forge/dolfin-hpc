eecfbfb074c44c7a26529d58eab0d391b781d3a0
Niclas Jansson <njansson@csc.kth.se>
2017-10-17 04:04:10 +0900

    Nuke redundant interface

--- include/dolfin/insitu/dolfin_insitu.h
+++ include/dolfin/insitu/dolfin_insitu.h
@@ -1,7 +1,6 @@
 #ifndef __DOLFIN_HEADER_INSITU_H
 #define __DOLFIN_HEADER_INSITU_H
 
-#include <dolfin/insitu/InsituPipeLine.h>
 #include <dolfin/insitu/libsimInterface.h>
 #include <dolfin/insitu/libsimPipeLine.h>
 

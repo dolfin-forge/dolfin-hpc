f3893837253bcf2bb4c8bc734146cb0c22a99ba1
Niclas Jansson <njansson@csc.kth.se>
2017-10-17 02:50:04 +0900

    Add missing headers

--- include/dolfin/insitu/dolfin_insitu.h
+++ include/dolfin/insitu/dolfin_insitu.h
@@ -1,6 +1,7 @@
 #ifndef __DOLFIN_HEADER_INSITU_H
 #define __DOLFIN_HEADER_INSITU_H
 
+#include <dolfin/insitu/InsituPipeLine.h>
 #include <dolfin/insitu/libsimInterface.h>
 #include <dolfin/insitu/libsimPipeLine.h>
 

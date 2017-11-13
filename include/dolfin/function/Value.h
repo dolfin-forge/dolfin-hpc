b4042b0c31c55386489976ac63f9481c935103d9
Aurelien Larcher <aurelien.larcher@gmail.com>
2017-11-13 01:23:55 +0100

    Add Zero for convenience

--- include/dolfin/function/Value.h
+++ include/dolfin/function/Value.h
@@ -81,6 +81,76 @@ public:
 
 };
 
+//-----------------------------------------------------------------------------
+
+template<uint I = 1, uint J = 1>
+class Zero : public Expression, public TimeDependent
+{
+
+public:
+
+  ///
+  Zero() :
+      Expression(),
+      TimeDependent()
+  {
+  }
+
+  /// Evaluate expression at given point
+  void eval(real* values, real const* x) const
+  {
+    std::fill(values, values + I * J, 0.0);
+  }
+
+  /// Return the rank of the value space
+  inline uint rank() const
+  {
+    return ValueSpace<I, J>::rank();
+  }
+
+  /// Return the dimension of the value space for axis i
+  inline uint dim(uint i) const
+  {
+    return ValueSpace<I, J>::dim(i);
+  }
+
+  /// Return value size (allow overloading to avoid recomputation)
+  inline uint value_size() const
+  {
+    return ValueSpace<I, J>::value_size();
+  }
+
+  //---------------------------------------------------------------------------
+
+  /// Value implements the time dependency
+  inline Zero<I,J> const& operator()(Time const& t) const
+  {
+    TimeDependent::operator ()(t);
+    return *this;
+  }
+
+  //---------------------------------------------------------------------------
+
+  ///
+  virtual void disp() const
+  {
+    section("Zero");
+    message("rank       : %d", this->rank());
+    std::stringstream ss;
+    ss << "(";
+    for (uint i = 0; i <= this->rank(); ++i)
+    {
+      ss << this->dim(i) << ", ";
+    }
+    ss << ")";
+    message("dimensions : %s", ss.str().c_str());
+    message("value size : %d", this->value_size());
+    end();
+    skip();
+  }
+
+};
+
 } /* namespace licorne */
 
 #endif /* __LICORNE_FUNCTION_VALUE_H_ */

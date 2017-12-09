3e82c383326bf3c2a61ff305cae83e398b8ada58
Aurelien Larcher <aurelien.larcher@gmail.com>
2017-12-09 15:54:06 +0100

    MeshData: add container size()

--- include/dolfin/mesh/MeshData.h
+++ include/dolfin/mesh/MeshData.h
@@ -37,12 +37,13 @@ class MeshData
   template <class V, class E>
   struct type : public key { type() : key(values<V>(), entity<E>()) {} };
 
-  struct data_array { };
+  struct data_array { virtual uint size() const = 0; };
 
   template <class V, class E>
   struct array : public data_array
   {
     typedef Array<MeshValues<V, E>*> type; type data;
+    uint size() const { return data.size(); };
   };
 
   //---------------------------------------------------------------------------
@@ -87,6 +88,17 @@ public:
   template <class V, class E> uint size()
   {
     return lookup<V, E>().size();
+  }
+
+  // Return total number of functions
+  uint size() const
+  {
+    uint s = 0;
+    for (Store::const_iterator it = S_.begin(); it != S_.end(); ++it)
+    {
+      s += it->second->size();
+    }
+    return s;
   }
 
   // Count number of function instances in data

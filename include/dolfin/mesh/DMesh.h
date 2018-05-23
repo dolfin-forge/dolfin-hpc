42c3f5860671b8c10f576248afe0683a3d04b320
Aurélien Larcher <aurelien.larcher@gmail.com>
2018-05-23 18:10:37 +0200

    DMesh: remove unused expKeepNumbering

--- include/dolfin/mesh/DMesh.h
+++ include/dolfin/mesh/DMesh.h
@@ -96,19 +96,11 @@ private:
   /// Entity is just marked as deleted, but not yet erased
   void removeCell(DCell* c);
 
-  /// Export to a regular mesh but keep numbering in the DMesh
-  ///
-  /// An optional mapping between old and new indices is generated. The Arrays
-  /// have to have at least the size of the old numbering (as it was at the
-  /// time of the import).
-  void expKeepNumbering(Mesh& mesh, Array<int> * old2new_cells = 0,
-                        Array<int> * old2new_vertices = 0);
-
   /// Renumber mesh entities locally
   ///
   /// An optional mapping between old and new indices is generated. The Arrays
   /// have to have the size of the old numbering
-  void number(Array<int> *old2new_cells = 0, Array<int> *old2new_vertices = 0);
+  void number(Array<int> *old2new_cells = NULL, Array<int> *old2new_vertices = NULL);
 
   /// Bisect cell dcell
   ///

e6aeb1f4c5708db13d47d25befdf6ed9a6f8d9f2
Niclas Jansson <njansson@kth.se>
2019-10-27 21:48:04 +0100

    Fix flaky test-wrapper causing check to segv on some systems

--- src/common/Test.cpp
+++ src/common/Test.cpp
@@ -48,7 +48,7 @@ void Test::init(int argc, char *argv[])
 {
   int flag;
   int i = 0;
-  while (-1 != (flag = getopt(argc, argv, ":d:m:b")))
+  while (argc > 1 && -1 != (flag = getopt(argc, argv, "d:m:b")))
   {
     switch (flag)
       {
@@ -63,13 +63,11 @@ void Test::init(int argc, char *argv[])
         args.mesh_file = optarg;
         break;
       default:
-        if (i < argc && (getopt(argc, argv, ":d:m:b") == -1))
-        {
-          ++optind;
-        }
         break;
       }
   }
+  // Reset optind if applications are using getopt through the TEST interface
+  optind = 1;
 }
 
 //-----------------------------------------------------------------------------

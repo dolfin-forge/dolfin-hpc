f09780b029d9b20f06427afc7e7ba1d13fc60c87
Julian Hornich <julian@ingridcloud.com>
2020-01-07 11:31:46 +0100

    fix tests compilation with -std=c++98

--- tests/io/Binary/Binary.h
+++ tests/io/Binary/Binary.h
@@ -107,45 +107,45 @@ DOLFIN_START_TEST( test_BinaryFile_Mesh )
 DOLFIN_END_TEST
     //--------------------------------------------------------------------------
     // example from demo/pde/poisson
+
+    // Source term
+    struct Source : public Value<Source>
+    {
+      void eval(real * value, const real* x) const
+      {
+        real dx = x[0] - 0.5;
+        real dy = x[1] - 0.5;
+        value[0] = 500.0*std::exp(-(dx*dx + dy*dy)/0.02);
+      }
+    };
+
+    // Neumann boundary condition
+    struct Flux : public Value<Flux>
+    {
+      void eval(real * value, const real* x) const
+      {
+        if (x[0] > DOLFIN_EPS)
+          value[0] = 25.0*std::sin(5.0*DOLFIN_PI*x[1]);
+        else
+          value[0] = 0.0;
+      }
+    };
+
+    //------------------------------------------------------------------------
+
+    // Sub domain for Dirichlet boundary condition
+    struct DirichletBoundary : public SubDomain
+    {
+      bool inside(const real* x, bool on_boundary) const
+      {
+        return x[0] < DOLFIN_EPS && on_boundary;
+      }
+    };
+
+    //------------------------------------------------------------------------
 DOLFIN_START_TEST( test_BinaryFile_Poisson )
   {
     {
-      //------------------------------------------------------------------------
-
-      // Source term
-      struct Source : public Value<Source>
-      {
-        void eval(real * value, const real* x) const
-        {
-          real dx = x[0] - 0.5;
-          real dy = x[1] - 0.5;
-          value[0] = 500.0*std::exp(-(dx*dx + dy*dy)/0.02);
-        }
-      };
-
-      // Neumann boundary condition
-      struct Flux : public Value<Flux>
-      {
-        void eval(real * value, const real* x) const
-        {
-          if (x[0] > DOLFIN_EPS)
-            value[0] = 25.0*std::sin(5.0*DOLFIN_PI*x[1]);
-          else
-            value[0] = 0.0;
-        }
-      };
-
-      //------------------------------------------------------------------------
-
-      // Sub domain for Dirichlet boundary condition
-      struct DirichletBoundary : public SubDomain
-      {
-        bool inside(const real* x, bool on_boundary) const
-        {
-          return x[0] < DOLFIN_EPS && on_boundary;
-        }
-      };
-
       //------------------------------------------------------------------------
 
       // Create mesh

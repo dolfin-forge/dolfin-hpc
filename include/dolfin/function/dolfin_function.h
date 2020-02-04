2b2f33021b227fad710f76f5fc78dae21f8223b5
Niclas Jansson <leifniclas.jansson@riken.jp>
2020-02-04 15:47:01 +0100

    Cleanup

--- include/dolfin/function/dolfin_function.h
+++ include/dolfin/function/dolfin_function.h
@@ -6,7 +6,6 @@
 #include <dolfin/function/Analytic.h>
 #include <dolfin/function/Constant.h>
 #include <dolfin/function/Expression.h>
-#include <dolfin/function/Field.h>
 #include <dolfin/function/Function.h>
 #include <dolfin/function/FunctionDecomposition.h>
 #include <dolfin/function/FunctionInterpolation.h>

80cd17a436cd5e69b32188b57c626ec0da3e2ef0
Julian Hornich <julian@ingridcloud.com>
2020-02-11 17:06:07 +0100

    remove meaningless const qualifier

--- include/dolfin/fem/CoefficientMap.h
+++ include/dolfin/fem/CoefficientMap.h
@@ -36,13 +36,7 @@ public:
   bool has(std::string const& label) const;
 
   /// Accessor
-  inline Coefficient *& operator[](std::string const& label)
-  {
-    return map_[label];
-  }
-
-  /// Accessor (const)
-  inline Coefficient * const operator[](std::string const& label) const
+  inline Coefficient * operator[](std::string const& label) const
   {
     Container::const_iterator it = map_.find(label);
     if (it == map_.end()) return NULL;

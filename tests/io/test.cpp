75e58eb08def7398a99f8f3176c052bb8107fe7f
Julian Hornich <julian@ingridcloud.com>
2020-03-23 21:17:46 +0100

    adapt tests to recent changes

--- tests/io/test.cpp
+++ tests/io/test.cpp
@@ -4,9 +4,6 @@
 #ifdef HAVE_CHECK
 
 #include "File/File.h"
-#ifdef HAVE_XML
-#include "XML/XML.h"
-#endif
 #include "STL/STL.h"
 #include "VTK/VTK.h"
 #include "Binary/Binary.h"
@@ -17,12 +14,6 @@ DOLFIN_SUITE_BEGIN(suite, "io")
   DOLFIN_TCASE_CREATE("File");
   DOLFIN_TCASE_ADD(test_File);
 
-#ifdef HAVE_XML
-  DOLFIN_TCASE_CREATE("XML");
-  DOLFIN_TCASE_ADD(test_XMLFile);
-  DOLFIN_TCASE_ADD(test_XMLMesh);
-#endif
-  
   DOLFIN_TCASE_CREATE("STL");
   DOLFIN_TCASE_ADD(test_STLMesh);
 
--- tests/parameter/test.cpp
+++ tests/parameter/test.cpp
@@ -2,21 +2,13 @@
 
 #ifdef HAVE_CHECK
 
-#include "ParameterValue/ParameterValue.h"
 #include "Parameter/Parameter.h"
-#include "Parametrized/Parametrized.h"
 
 //-----------------------------------------------------------------------------
 DOLFIN_SUITE_BEGIN(suite, "main")
 {
-  DOLFIN_TCASE_CREATE("ParameterValue");
-  DOLFIN_TCASE_ADD(test_ParameterValue);
-
-  DOLFIN_TCASE_CREATE("Parameter");
-  DOLFIN_TCASE_ADD(test_Parameter);
-
-  DOLFIN_TCASE_CREATE("Parametrized");
-  DOLFIN_TCASE_ADD(test_Parametrized);
+  DOLFIN_TCASE_CREATE("parameter");
+  DOLFIN_TCASE_ADD(test_parameter);
 }
 DOLFIN_SUITE_END
 //-----------------------------------------------------------------------------
